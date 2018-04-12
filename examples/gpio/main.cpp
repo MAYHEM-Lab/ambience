ab4d7f1a4bbae1da7ff395ab8c158b6e88ed64c9
Fatih BAKIR <mfatihbakir@gmail.com>
2018-04-11 18:57:03 -0700

    usart interface

--- examples/gpio/main.cpp
+++ examples/gpio/main.cpp
@@ -44,13 +44,13 @@ void write_task()
     {
         have_write.down();
 
-        UCSR0B |= (1<<UDRIE0);
-        /*while (*data)
+        //UCSR0B |= (1<<UDRIE0);
+        while (*data)
         {
             tx_comp.down();
             UDR0 = *data;
             ++data;
-        }*/
+        }
     }
 }
 
@@ -99,27 +99,92 @@ void timer1_init()
     sei();
 }
 
+
+namespace tos
+{
+    enum class usart_modes : uint8_t
+    {
+        async = 0,
+        sync = 0b01,
+        reserved = 0b10,
+        spi_master = 0b11
+    };
+
+    enum class usart_parity : uint8_t
+    {
+        disabled = 0,
+        reserved = 0b01,
+        even = 0b10,
+        odd = 0b11
+    };
+
+    enum class usart_stop_bit : uint8_t
+    {
+        one = 0b0,
+        two = 0b1
+    };
+
+    constexpr uint8_t usart_control(usart_modes mode, usart_parity parity, usart_stop_bit stop)
+    {
+        return ((uint8_t)mode << 6) | ((uint8_t)parity << 4) | ((uint8_t)stop << 3) | (0b11 << 1);
+    }
+
+/**
+ * This class manages the AVR USART0 device
+ */
+class avr_usart0
+{
+public:
+    void set_baud_rate(uint16_t);
+    void set_2x_rate(bool);
+    void enable();
+    void disable();
+
+    void set_control(usart_modes, usart_parity, usart_stop_bit);
+};
+}
+
+namespace tos
+{
+    void avr_usart0::set_baud_rate(uint16_t baud)
+    {
+        const auto prescale = F_CPU / (baud * 16UL) - 1;
+        UBRR0L = (uint8_t)(prescale & 0xff);
+        UBRR0H = (uint8_t)(prescale >> 8);
+    }
+
+    void avr_usart0::set_2x_rate(bool)
+    {
+        UCSR0A |= (1 << U2X0);
+    }
+
+    void avr_usart0::enable()
+    {
+        UCSR0B |= (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0) | (1 << TXCIE0);
+    }
+
+    void avr_usart0::disable()
+    {
+        UCSR0B &= ~((1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0) | (1 << TXCIE0));
+    }
+
+    void avr_usart0::set_control(usart_modes m, usart_parity p, usart_stop_bit s)
+    {
+        UCSR0B = usart_control(m, p, s);
+    }
+}
+
+tos::avr_usart0 usart;
+
 void initUSART()
 {
-#define USART_BAUDRATE 9600
-#define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1)
-    /* 9600 baud */
-    UBRR0L = (uint8_t)(BAUD_PRESCALE & 0xff);
-    UBRR0H = (uint8_t)(BAUD_PRESCALE >> 8);
-    UCSR0B = (1 << RXCIE0) | (1 << RXEN0) | (1 << TXEN0);
-    UCSR0C =
-            /* no parity bit */
-            (0 << UPM01) |
-                    (0 << UPM00) |
-                    /* asyncrounous USART */
-                    (0 << UMSEL01) |
-                    (0 << UMSEL00) |
-                    /* one stop bit */
-                    (0 << USBS0) |
-                    /* 8-bits of data */
-                    (1 << UCSZ01) |
-                    (1 << UCSZ00);
-    UCSR0A = (1 << U2X0);
+    using namespace tos;
+
+    usart.set_baud_rate(9600);
+    usart.set_2x_rate(true);
+    usart.set_control(usart_modes::async, usart_parity::disabled, usart_stop_bit::one);
+
+    usart.enable();
 }
 
 int main()
