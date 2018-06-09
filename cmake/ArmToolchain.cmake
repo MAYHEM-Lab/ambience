5fd87eda66854329a596388381e948999327aa5c
Fatih BAKIR <fatih@linux.com>
2018-06-08 18:19:38 -0700

    fixed the toolchains

--- cmake/ArmToolchain.cmake
+++ cmake/ArmToolchain.cmake
@@ -6,13 +6,15 @@ set(CMAKE_SYSTEM_PROCESSOR arm)
 set(tools /opt/x-tools/gcc-arm-none-eabi-7-2017-q4-major)
 set(CMAKE_SYSROOT "sysroot")
 
-CMAKE_FORCE_C_COMPILER(${tools}/bin/arm-none-eabi-gcc GNU)
-CMAKE_FORCE_CXX_COMPILER(${tools}/bin/arm-none-eabi-g++ GNU)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-eabi-g++)
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 set(TOS_NRF52_FLAGS "-DNRF52 -DFLOAT_ABI_HARD -DCONFIG_GPIO_AS_PINRESET -DNRF52832_XXAA -DNRF52_PAN_74 -mthumb -mabi=aapcs -mfloat-abi=hard -mfpu=fpv4-sp-d16 -u _printf_float -fno-builtin -fshort-enums")
 set(TOS_COMMON_FLAGS "-mcpu=cortex-m4 -ffunction-sections -fdata-sections -Wall -Wextra -ffreestanding ${TOS_NRF52_FLAGS}")
@@ -20,6 +22,10 @@ set(TOS_COMMON_FLAGS "-mcpu=cortex-m4 -ffunction-sections -fdata-sections -Wall 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${TOS_COMMON_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TOS_COMMON_FLAGS} -std=c++14 -pedantic -fno-unwind-tables -fno-exceptions -fno-rtti -fno-threadsafe-statics")
 set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -fno-threadsafe-statics ${TOS_NRF52_FLAGS}")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "CFLAGS")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "CXXFLAGS")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}" CACHE STRING "")
 
 set(SDK_ROOT /home/fatih/Downloads/nRF5_SDK_15.0.0_a53641a)
 
--- cmake/AvrToolchain.cmake
+++ cmake/AvrToolchain.cmake
@@ -11,6 +11,12 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
 set(CMAKE_C_FLAGS "-mmcu=${AVR_CPU} ${CMAKE_C_FLAGS} -Wall -Wextra -ffreestanding")
 set(CMAKE_CXX_FLAGS "-mmcu=${AVR_CPU} ${CMAKE_CXX_FLAGS} -ffunction-sections -fdata-sections -std=c++14 -Wall -Wextra -pedantic -fno-unwind-tables -ffreestanding -fno-exceptions -fno-rtti -fno-threadsafe-statics")
 set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -fno-threadsafe-statics")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "CFLAGS")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "CXXFLAGS")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS}" CACHE STRING "")
