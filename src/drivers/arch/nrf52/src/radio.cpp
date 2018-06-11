57b06e806455f37d662197f7c2a017e9e3b70076
Fatih BAKIR <fatih@linux.com>
2018-06-10 22:02:15 -0700

    moved initialization code to the driver

--- src/drivers/arch/nrf52/src/radio.cpp
+++ src/drivers/arch/nrf52/src/radio.cpp
@@ -3,11 +3,69 @@
 //
 
 #include <radio.hpp>
-#include <radio_config.h>
 #include <stdint.h>
 #include <nrf52.h>
 #include <mdk/nrf.h>
 #include <tos/semaphore.hpp>
+
+#define PACKET_BASE_ADDRESS_LENGTH  (4UL)                   //!< Packet base address length field size in bytes
+#define PACKET_STATIC_LENGTH        (4UL)                   //!< Packet static length in bytes
+#define PACKET_PAYLOAD_MAXSIZE      (PACKET_STATIC_LENGTH)  //!< Packet payload maximum size in bytes
+
+#define PACKET_S1_FIELD_SIZE      (0UL)  /**< Packet S1 field size in bits. */
+#define PACKET_S0_FIELD_SIZE      (0UL)  /**< Packet S0 field size in bits. */
+#define PACKET_LENGTH_FIELD_SIZE  (0UL)  /**< Packet length field size in bits. */
+
+static void radio_configure()
+{
+    // Radio config
+    NRF_RADIO->TXPOWER   = (RADIO_TXPOWER_TXPOWER_0dBm << RADIO_TXPOWER_TXPOWER_Pos);
+    NRF_RADIO->FREQUENCY = 7UL;  // Frequency bin 7, 2407MHz
+    NRF_RADIO->MODE      = (RADIO_MODE_MODE_Nrf_1Mbit << RADIO_MODE_MODE_Pos);
+
+    // Radio address config
+    NRF_RADIO->PREFIX0 =
+        ((uint32_t)(0xC3) << 24) // Prefix byte of address 3 converted to nRF24L series format
+      | ((uint32_t)(0xC2) << 16) // Prefix byte of address 2 converted to nRF24L series format
+      | ((uint32_t)(0xC1) << 8)  // Prefix byte of address 1 converted to nRF24L series format
+      | ((uint32_t)(0xC0) << 0); // Prefix byte of address 0 converted to nRF24L series format
+
+    NRF_RADIO->PREFIX1 =
+        ((uint32_t)(0xC7) << 24) // Prefix byte of address 7 converted to nRF24L series format
+      | ((uint32_t)(0xC6) << 16) // Prefix byte of address 6 converted to nRF24L series format
+      | ((uint32_t)(0xC4) << 0); // Prefix byte of address 4 converted to nRF24L series format
+
+    NRF_RADIO->BASE0 = 0x01234567UL;  // Base address for prefix 0 converted to nRF24L series format
+    NRF_RADIO->BASE1 = 0x89ABCDEFUL;  // Base address for prefix 1-7 converted to nRF24L series format
+
+    NRF_RADIO->TXADDRESS   = 0x00UL;  // Set device address 0 to use when transmitting
+    NRF_RADIO->RXADDRESSES = RADIO_RXADDRESSES_ADDR0_Msk;  // Enable device address 0 to use to select which addresses to receive
+
+    // Packet configuration
+    NRF_RADIO->PCNF0 = (PACKET_S1_FIELD_SIZE     << RADIO_PCNF0_S1LEN_Pos) |
+                       (PACKET_S0_FIELD_SIZE     << RADIO_PCNF0_S0LEN_Pos) |
+                       (PACKET_LENGTH_FIELD_SIZE << RADIO_PCNF0_LFLEN_Pos);
+
+    // Packet configuration
+    NRF_RADIO->PCNF1 = (RADIO_PCNF1_WHITEEN_Disabled << RADIO_PCNF1_WHITEEN_Pos) |
+                       (RADIO_PCNF1_ENDIAN_Big       << RADIO_PCNF1_ENDIAN_Pos)  |
+                       (PACKET_BASE_ADDRESS_LENGTH   << RADIO_PCNF1_BALEN_Pos)   |
+                       (PACKET_STATIC_LENGTH         << RADIO_PCNF1_STATLEN_Pos) |
+                       (PACKET_PAYLOAD_MAXSIZE       << RADIO_PCNF1_MAXLEN_Pos);
+
+    // CRC Config
+    NRF_RADIO->CRCCNF = (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos); // Number of checksum bits
+    if ((NRF_RADIO->CRCCNF & RADIO_CRCCNF_LEN_Msk) == (RADIO_CRCCNF_LEN_Two << RADIO_CRCCNF_LEN_Pos))
+    {
+        NRF_RADIO->CRCINIT = 0xFFFFUL;   // Initial value
+        NRF_RADIO->CRCPOLY = 0x11021UL;  // CRC poly: x^16 + x^12^x^5 + 1
+    }
+    else if ((NRF_RADIO->CRCCNF & RADIO_CRCCNF_LEN_Msk) == (RADIO_CRCCNF_LEN_One << RADIO_CRCCNF_LEN_Pos))
+    {
+        NRF_RADIO->CRCINIT = 0xFFUL;   // Initial value
+        NRF_RADIO->CRCPOLY = 0x107UL;  // CRC poly: x^8 + x^2^x^1 + 1
+    }
+}
 
 namespace tos
 {
--- src/drivers/arch/nrf52/src/sdk_config.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Mehmet Fatih BAKIR on 09/06/2018.
-//
-
-#ifndef TOS_SDK_CONFIG_H
-#define TOS_SDK_CONFIG_H
-
-#endif //TOS_SDK_CONFIG_H
