78e52cada1ab03457a19ae44a52ba31cad68aeb7
Fatih BAKIR <mfatihbakir@gmail.com>
2018-10-24 16:54:46 -0700

    even better libc++ integration

---
+++ libs/toscxx/src/compile.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Mehmet Fatih BAKIR on 24/10/2018.
+//
+
+#include <tuple>
+#include <functional>
--- src/util/include/tos/utility.hpp
+++ src/util/include/tos/utility.hpp
@@ -9,6 +9,20 @@
 #include <utility>
 
 namespace tos {
+    template<class T> using invoke_t = typename T::type;
+
+    template<class S1, class S2>
+    struct concat;
+
+    template<class T, T... I1, T... I2>
+    struct concat<std::integer_sequence<T, I1...>, std::integer_sequence<T, I2...>>
+    : std::integer_sequence<T, I1..., (sizeof...(I1)+I2)...>
+    {
+    };
+
+    template<class S1, class S2>
+    using concat_t = invoke_t<concat<S1, S2>>;
+
     template <class AlarmT, class FunT>
     void forever(AlarmT& alarm, milliseconds ms, FunT&& fun)
     {
