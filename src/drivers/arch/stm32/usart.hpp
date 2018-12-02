cf10e3a6b0a5fda9b58396757ea8e7479789e0d7
Fatih BAKIR <fatih@linux.com>
2018-12-01 22:26:40 -0800

    stm32 usart header

---
+++ src/drivers/arch/stm32/usart.hpp
@@ -0,0 +1,20 @@
+//
+// Created by fatih on 11/29/18.
+//
+
+#pragma once
+
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/usart.h>
+
+namespace tos
+{
+namespace stm32
+{
+
+    class usart
+    {
+
+    };
+} // namespace stm32
+} // namespace tos
