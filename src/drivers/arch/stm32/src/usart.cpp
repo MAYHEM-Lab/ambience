a3ee360b88b1012b8fa4f3dbe97bc0410a957f36
Fatih BAKIR <fatih@linux.com>
2018-12-05 14:50:05 -0800

    proper stm32 usart driver

---
+++ src/drivers/arch/stm32/src/usart.cpp
@@ -0,0 +1,27 @@
+//
+// Created by fatih on 12/5/18.
+//
+
+#include <usart.hpp>
+
+void usart2_isr()
+{
+    auto& ins = *tos::stm32::usart::get(1);
+
+    if ((USART_CR1(USART2) & USART_CR1_RXNEIE) &&
+        (USART_SR(USART2) & USART_SR_RXNE)) {
+        ins.rx_buf.push(usart_recv(USART2));
+        ins.rx_s.up_isr();
+    }
+
+    if ((USART_CR1(USART2) & USART_CR1_TXEIE) &&
+        (USART_SR(USART2) & USART_SR_TXE))
+    {
+        usart_send(USART2, *ins.tx_it++);
+        if (ins.tx_it == ins.tx_end)
+        {
+            usart_disable_tx_interrupt(USART2);
+            ins.tx_s.up_isr();
+        }
+    }
+}
