6b0228288eccca792f0f925d4339b36e447210e3
Fatih BAKIR <mfatihbakir@gmail.com>
2018-12-11 18:09:33 -0800

    span comments

--- src/util/include/tos/span.hpp
+++ src/util/include/tos/span.hpp
@@ -6,34 +6,104 @@
 
 #include <stddef.h>
 #include <array>
+#include <cstddef>
 
 namespace tos
 {
+    /**
+     * Span is a class template that represents a view to a
+     * contiguous range of values.
+     *
+     * Span must be used in place of pointer-size pairs in
+     * APIs.
+     *
+     * As this is a view, spans do not own the memory they
+     * are pointing to. Care must be taken not to have
+     * dangling spans.
+     *
+     * @tparam T type of the values in the span
+     */
     template <class T>
     class span
     {
     public:
         using iterator = T*;
 
-        constexpr span(nullptr_t, size_t) = delete;
+        /**
+         * Spans cannot be null, thus this constructor
+         * is deleted.
+         */
+        constexpr span(std::nullptr_t, size_t) = delete;
 
+        /**
+         * Constructs a span from the given pointer-length pair.
+         *
+         * Use of this constructor should be avoided in favor of
+         * direct conversions from containers.
+         *
+         * @param base pointer to the beginning of the range
+         * @param len size of the range
+         */
         constexpr span(T* base, size_t len) : m_base(base), m_len(len) {}
 
+        /**
+         * Constructs a span from the given iterator pair.
+         *
+         * As usual, the iterators are interpreted as [begin, end),
+         * ie. *end is not included.
+         *
+         * @param base begin iterator
+         * @param end end iterator
+         */
         constexpr span(T* base, T* end) : m_base{base}, m_len{end - base} {}
 
+        /**
+         * Constructs a span from the given C-style array object
+         *
+         * When passed string literals, this constructor is used,
+         * thus, it will have a zero terminator at the end.
+         *
+         * @tparam Sz size of the array
+         * @param arr the array
+         */
         template <size_t Sz>
         constexpr span(T (&arr)[Sz]) : m_base(arr), m_len(Sz) {}
 
+        /**
+         * Constructs a span from the given C++ array object
+         *
+         * @tparam Sz size of the array
+         * @param arr the array
+         */
         template <size_t Sz>
         constexpr span(std::array<T, Sz>& arr) : m_base(arr.data()), m_len(arr.size()) {}
 
+        /**
+         * Constructs a const span from the given C++ array to const objects
+         *
+         * @tparam Sz size of the array
+         * @param arr the array
+         */
         template <size_t Sz>
         constexpr span(const std::array<std::remove_const_t<T>, Sz>& arr)
         : m_base(arr.data()), m_len(arr.size()) {}
 
+        /**
+         * Returns the number of elements in the span
+         * @return tne number of elements
+         */
         constexpr size_t size() const { return m_len; }
 
+        /**
+         * Returns the pointer to the beginning of the range of the span
+         * @return pointer to the beginning of the range
+         */
         constexpr T* data() { return m_base; }
+
+        /**
+         * Returns the pointer to the beginning of the range of the span
+         * @return pointer to the beginning of the range
+         */
         constexpr const T* data() const { return m_base; }
 
         constexpr T& operator[](size_t ind) {
@@ -50,10 +120,22 @@ namespace tos
         constexpr const T* begin() const { return m_base; }
         constexpr const T* end() const { return m_base + m_len; }
 
+        /**
+         * Spans convert to a const version of them automatically
+         *
+         * @return constant version of this span
+         */
         constexpr operator span<const T>() const {
             return { m_base, size_t(m_len) };
         }
 
+        /**
+         * Takes a slice from this span.
+         *
+         * @param begin beginning index of the slice
+         * @param len length of the slice
+         * @return a new span
+         */
         constexpr span slice(size_t begin, size_t len){
             return { m_base + begin, len };
         }
