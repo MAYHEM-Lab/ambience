9bb7b5e72ba2cdd23ae540f6d18a902ab721cdae
Fatih BAKIR <mfatihbakir@gmail.com>
2019-01-09 22:05:06 +0300

    avr core splitted from the boot, allowing it to be installed

--- src/arch/avr/avr_impl.cpp
+++ src/arch/avr/avr_impl.cpp
@@ -5,7 +5,7 @@
 #include <avr/io.h>
 #include <avr/wdt.h>
 
-#include <arch/avr/include/tos_arch.hpp>
+#include <tos_arch.hpp>
 #include <tos/arch.hpp>
 
 #include <avr/power.h>
@@ -29,48 +29,6 @@ namespace tos
     }
 } // namespace tos
 
-void NORETURN reset_cpu()
-{
-    tos_disable_interrupts();
-    wdt_enable(WDTO_15MS);
-    while (true){}
-}
-
-extern "C"
-{
-    /**
-     * Doing a software reset through watchdog timer leaves the wdt
-     * enabled after the reset which causes an infinite reset loop.
-     *
-     * Disabling the watchdog timer at reset solves it.
-     *
-     * .init3 is simply an unused section that's reserved for user code,
-     * so we stick it there.
-     */
-    void __attribute__((naked, used, section(".init3"))) wdt_init(void)
-    {
-        MCUSR = 0;
-        wdt_disable();
-    }
-
-    void NORETURN tos_force_reset()
-    {
-        reset_cpu();
-    }
-
-    alignas(alignof(std::max_align_t)) char stack[256*2];
-    int stack_index = 0;
-    void* tos_stack_alloc(size_t)
-    {
-        return stack+256*stack_index++;
-    }
-
-    void tos_stack_free(void*)
-    {
-    }
-}
-
-extern void tos_main();
 
 static void power_down(int mode)
 {
@@ -80,6 +38,8 @@ static void power_down(int mode)
     sleep_cpu();
     sleep_disable();
 }
+
+extern void tos_main();
 
 int TOS_EXPORT TOS_MAIN NORETURN main()
 {
