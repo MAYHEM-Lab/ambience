c107a37ed8ebe08f98f1a9178e91b7faa640303a
Fatih BAKIR <mfatihbakir@gmail.com>
2019-01-09 22:05:28 +0300

    stm32 core separated from the boot code, successfully installs

--- src/arch/stm32/main.cpp
+++ src/arch/stm32/main.cpp
@@ -2,55 +2,13 @@
 // Created by fatih on 10/25/18.
 //
 
-#include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <tos/compiler.hpp>
 #include <tos/scheduler.hpp>
 #include <tos/ft.hpp>
-#include "include/tos_arch.hpp"
-#include <libopencmsis/core_cm3.h>
-
-#define NVIC_AIRCR_VECTKEY    (0x5FA << 16)   /*!< AIRCR Key for write access   */
-#define NVIC_SYSRESETREQ            2         /*!< System Reset Request         */
-
-#define dsb() __asm__ __volatile__ ("dsb" : : : "memory")
-
-static __INLINE NORETURN void NVIC_SystemReset()
-{
-    SCB->AIRCR  = (NVIC_AIRCR_VECTKEY | (SCB->AIRCR & (0x700)) | (1<<NVIC_SYSRESETREQ)); /* Keep priority group unchanged */
-    dsb();                                                                                 /* Ensure completion of memory access */
-    while(1);                                                                                /* wait until reset */
-}
-
-extern "C"
-{
-    void NORETURN tos_force_reset()
-    {
-        NVIC_SystemReset();
-    }
-
-extern "C" void *__dso_handle;
-void *__dso_handle = 0;
-
-void* tos_stack_alloc(size_t sz)
-{
-    return malloc(sz);
-}
-
-void tos_stack_free(void* ptr)
-{
-    return free(ptr);
-}
-}
+#include <tos_arch.hpp>
 
 void tos_main();
-
-void hard_fault_handler()
-{
-    while(true)
-    {
-    }
-}
 
 int main()
 {
---
+++ src/arch/stm32/stm32_core.cpp
@@ -0,0 +1,50 @@
+//
+// Created by fatih on 1/9/19.
+//
+
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencmsis/core_cm3.h>
+#include <tos/compiler.hpp>
+#include <cstddef>
+#include <cstdlib>
+
+#define NVIC_AIRCR_VECTKEY    (0x5FA << 16)   /*!< AIRCR Key for write access   */
+#define NVIC_SYSRESETREQ            2         /*!< System Reset Request         */
+
+#define dsb() __asm__ __volatile__ ("dsb" : : : "memory")
+
+static __INLINE NORETURN void NVIC_SystemReset()
+{
+    SCB->AIRCR  = (NVIC_AIRCR_VECTKEY | (SCB->AIRCR & (0x700)) | (1<<NVIC_SYSRESETREQ)); /* Keep priority group unchanged */
+    dsb();                                                                                 /* Ensure completion of memory access */
+    while(1);                                                                                /* wait until reset */
+}
+
+extern "C"
+{
+void NORETURN tos_force_reset()
+{
+    NVIC_SystemReset();
+}
+
+extern "C" void *__dso_handle;
+void *__dso_handle = 0;
+
+void* tos_stack_alloc(size_t sz)
+{
+    return malloc(sz);
+}
+
+void tos_stack_free(void* ptr)
+{
+    return free(ptr);
+}
+}
+
+void hard_fault_handler()
+{
+    while(true)
+    {
+    }
+}
