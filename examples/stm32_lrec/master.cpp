c0fc8b5d308bd2bea9b3c47c31584dd0f65f13ce
Fatih BAKIR <fatih@linux.com>
2019-02-22 21:18:14 -0800

    lrec updates

--- examples/stm32_lrec/master.cpp
+++ examples/stm32_lrec/master.cpp
@@ -44,39 +44,66 @@ auto setup_usart2 = [](auto& g){
                   GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART3_PR_TX);
 };
 
-auto setup_usart0 = [](auto&){
+auto setup_usart0 = [](auto& g){
+    using namespace tos::tos_literals;
+
+    auto tx_pin = 9_pin;
+    auto rx_pin = 10_pin;
+
+    g.set_pin_mode(rx_pin, tos::pin_mode::in);
 };
 
-void master_task()
+auto xbee_task = [](auto& g, auto& log)
 {
     using namespace tos::tos_literals;
     using namespace std::chrono_literals;
-
-    auto g = tos::open(tos::devs::gpio);
-
-    setup_usart1(g);
-    auto log = tos::open(tos::devs::usart<1>, tos::uart::default_9600);
-    setup_usart2(g);
-    auto xbee_ser = tos::open(tos::devs::usart<2>, tos::uart::default_9600);
-    setup_usart0(g);
-    auto slave = tos::open(tos::devs::usart<0>, tos::uart::default_9600);
+    namespace xbee = tos::xbee;
 
     auto tmr = tos::open(tos::devs::timer<2>);
     auto alarm = tos::open(tos::devs::alarm, tmr);
 
-    auto dht = tos::make_dht(g, delay);
+    setup_usart0(g);
+    auto xbee_ser = tos::open(tos::devs::usart<0>, tos::uart::default_9600);
+    gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+                  GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
 
-    while (true) {
-        auto res = dht.read(8_pin);
-        if (res != tos::dht_res::ok) {
-            tos::println(log, "dht failed");
-            goto sleep;
+    auto dht_pin = 1_pin;
+    g.set_pin_mode(dht_pin, tos::pin_mode::in_pullup);
+
+    auto d = tos::make_dht(g, delay);
+
+    g.set_pin_mode(11_pin, tos::pin_mode::out);
+    g.write(11_pin, tos::digital::low);
+    alarm.sleep_for(100ms); // let it die
+
+    while (true)
+    {
+        int tries = 1;
+        auto res = d.read(dht_pin);
+
+        while (res != tos::dht_res::ok && tries < 5) {
+            tos::println(log, "dht non-responsive");
+            alarm.sleep_for(2s);
+            res = d.read(dht_pin);
+            ++tries;
         }
 
-        tos::println(log, "dht", int(dht.temperature), int(dht.humidity));
+        if (tries == 5) {
+            d.temperature = -1;
+            d.humidity = -1;
+        }
 
-        namespace xbee = tos::xbee;
-        /*g.write(7_pin, tos::digital::high);
+        std::array<temp::sample, 2> samples = {
+                temp::sample{d.temperature, d.humidity, 0},
+                {}
+        };
+
+        char buf[50];
+        tos::omemory_stream str{buf};
+
+        temp::print(str, 102, samples[0]);
+
+        g.write(11_pin, tos::digital::high);
 
         alarm.sleep_for(100ms); // let it wake up
         auto r = xbee::read_modem_status(xbee_ser, alarm);
@@ -84,43 +111,68 @@ void master_task()
         if (!r)
         {
             tos::println(log, "xbee non responsive");
-            goto sleep;
-        }*/
+        }
 
-        { // inner block to limit visibility of buff
-            auto sample = temp::sample{dht.temperature, dht.humidity, -1};
+        constexpr xbee::addr_16 base_addr{0x0010};
 
-            static std::array<char, 100> msg_buf;
-            tos::omemory_stream buff{msg_buf};
+        xbee::tx16_req req{base_addr, tos::raw_cast<const uint8_t>(str.get()), xbee::frame_id_t{0xAB}};
+        xbee::write_to(xbee_ser, req);
 
-            temp::print(buff, temp::master_id, sample);
+        alarm.sleep_for(100ms);
+        auto tx_r = xbee::read_tx_status(xbee_ser, alarm);
 
-            constexpr xbee::addr_16 base_addr{0x0010};
-            xbee::tx16_req req{base_addr, tos::raw_cast<const uint8_t>(buff.get()), xbee::frame_id_t{1}};
-            xbee::write_to(xbee_ser, req);
-
-            alarm.sleep_for(100ms);
-            auto tx_r = xbee::read_tx_status(xbee_ser, alarm);
-
-            if (tx_r) {
-                auto &res = tos::force_get(tx_r);
-                if (res.status == xbee::tx_status::statuses::success) {
-                    tos::println(log, "xbee sent!");
-                } else {
-                    tos::println(log, "xbee send failed");
-                }
+        if (tx_r) {
+            auto &res = tos::force_get(tx_r);
+            if (res.status == xbee::tx_status::statuses::success) {
+                tos::println(log, "xbee sent!");
             } else {
-                tos::println(log, "xbee non responsive");
+                tos::println(log, "xbee send failed");
             }
-        } // inner block
+        } else {
+            tos::println(log, "xbee non responsive");
+        }
 
-        //g.write(7_pin, tos::digital::low);
-    sleep:
-        alarm.sleep_for(15s);
+        g.write(11_pin, tos::digital::low);
+        alarm.sleep_for(5s);
     }
+
+    tos::this_thread::block_forever();
+};
+
+tos::kern::tcb* tcb;
+static std::array<char, 1024> sstack alignas(std::max_align_t);
+void master_task()
+{
+    using namespace tos::tos_literals;
+    using namespace std::chrono_literals;
+
+    namespace xbee = tos::xbee;
+
+    auto g = tos::open(tos::devs::gpio);
+
+    //setup_usart1(g);
+    //auto log = tos::open(tos::devs::usart<1>, tos::uart::default_9600);
+    //setup_usart2(g);
+    //auto xbee_ser = tos::open(tos::devs::usart<2>, tos::uart::default_9600);
+    //setup_usart0(g);
+    //auto slave = tos::open(tos::devs::usart<0>, tos::uart::default_9600);
+
+    struct x : tos::self_pointing<x> {
+        int write(tos::span<const char> x) { return x.size(); }
+    } l;
+
+    //xbee_task(g, log);
+    //return;
+    tos::semaphore s{0};
+    auto& t = tos::launch(tos::span<char>(sstack), [&]{
+        xbee_task(g, l);
+       s.up();
+    });
+    tcb = &t;
+    s.down();
 }
 
-static std::array<char, 2048> stack;
+static std::array<char, 1024> stack;
 void tos_main()
 {
     tos::launch(tos::span<char>(stack), master_task);
