3792707789b5abfc176ba31866d17a075a1ebf7b
Fatih BAKIR <fatih@linux.com>
2019-04-02 01:53:33 -0700

    nrf52 twim driver update

--- src/drivers/arch/nrf52/include/arch/nrf52/twim.hpp
+++ src/drivers/arch/nrf52/include/arch/nrf52/twim.hpp
@@ -7,20 +7,19 @@
 #include <tos/span.hpp>
 #include "gpio.hpp"
 #include <common/i2c.hpp>
+#include <common/driver_base.hpp>
 
 namespace tos
 {
     namespace nrf52
     {
-        class twim
+        class twim : public self_pointing<twim>
         {
         public:
             twim(gpio::pin_type clock_pin, gpio::pin_type data_pin);
 
             twi_tx_res transmit(twi_addr_t to, span<const char> buf) noexcept;
             twi_tx_res receive(twi_addr_t from, span<char> buf) noexcept;
-
-        private:
         };
     }
 }
