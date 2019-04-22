b1837946c289dedc52bbbdd8ecb273999efc4de7
Fatih Bakir <mfatihbakir@gmail.com>
2019-04-21 20:51:51 -0700

    update lrec code to buffer packets

--- examples/stm32_lrec/app.hpp
+++ examples/stm32_lrec/app.hpp
@@ -12,32 +12,4 @@ namespace temp
 {
     static constexpr auto master_id = 101;
     static constexpr auto slave_id = 102;
-
-    struct sample
-    {
-        float temp;
-        float humid;
-        float cpu;
-    };
-
-    float to_fahrenheits(float c)
-    {
-        if (c == -1) return -1;
-        return c * 9 / 5 + 32;
-    }
-
-    template <class StreamT>
-    void print(StreamT& str, uint32_t id, const sample& s)
-    {
-        std::array<char, 40> buf;
-        tos::msgpack::packer p{buf};
-        auto arr = p.insert_arr(4);
-
-        arr.insert(id);
-        arr.insert(s.temp);
-        arr.insert(s.humid);
-        arr.insert(s.cpu);
-
-        str->write(p.get());
-    }
 }
