5b45d9aca4f05e79021f3081722b812276de2287
Fatih BAKIR <fatih@linux.com>
2019-05-21 12:27:53 -0700

    minor utility changes

--- src/util/include/tos/expected.hpp
+++ src/util/include/tos/expected.hpp
@@ -23,7 +23,7 @@
  * The implementation must never return to the caller, otherwise
  * the behaviour is undefined.
  */
-[[noreturn]] void tos_force_get_failed(void*);
+[[noreturn]] void tos_force_get_failed(void*) NO_INLINE;
 
 namespace tos
 {
