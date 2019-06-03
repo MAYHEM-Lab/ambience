9ec9a36c2e620d971a2bb0505c318c9dfdaac300
Fatih Bakir <mfatihbakir@gmail.com>
2019-06-02 18:58:07 -0700

    refactor nrf52 example to make use of the updated ble stack

--- examples/nrf52/main.cpp
+++ examples/nrf52/main.cpp
@@ -2,6 +2,8 @@
 // Created by Mehmet Fatih BAKIR on 29/03/2018.
 //
 
+#include <arch/drivers.hpp>
+
 #include <tos/ft.hpp>
 #include <tos/semaphore.hpp>
 #include <tos/track_ptr.hpp>
@@ -13,7 +15,6 @@
 #include <drivers/include/nrfx_uarte.h>
 #include <tos/compiler.hpp>
 
-#include <arch/drivers.hpp>
 #include <algorithm>
 #include <tos/print.hpp>
 #include <tos/fixed_fifo.hpp>
@@ -24,94 +25,21 @@
 #include <common/epd/waveshare/bw29.hpp>
 
 #include <ble.h>
+#include <common/ble_srv_common.h>
 #include "nordic_common.h"
 #include "nrf.h"
 #include "ble_hci.h"
 #include "ble_advdata.h"
 #include "ble_advertising.h"
-#include "ble_conn_params.h"
-#include "nrf_sdh.h"
-#include "nrf_sdh_soc.h"
-#include "nrf_sdh_ble.h"
 #include "nrf_ble_gatt.h"
-#include "nrf_ble_qwr.h"
-#include "ble_nus.h"
-#include "app_timer.h"
-
-static uint32_t m_nus_link_ctx_storage_ctx_data_pool[((20)) * (((sizeof(ble_nus_client_context_t)) + 3) >> 2)];
-static blcm_link_ctx_storage_t m_nus_link_ctx_storage = {m_nus_link_ctx_storage_ctx_data_pool, 20,
-                                                         sizeof(m_nus_link_ctx_storage_ctx_data_pool) / 20};
-static ble_nus_t m_nus = []{
-    ble_nus_t res {
-        0,
-        0,
-        {},
-        {},
-        &m_nus_link_ctx_storage,
-        {}
-    };
-    return res;
-}();
-
-static nrf_sdh_ble_evt_observer_t m_nus_obs __attribute__((section(".sdh_ble_observers2"))) __attribute__((used)) = {
-    ble_nus_on_ble_evt,
-    &m_nus
-};
+#include "bakir_ble.hpp"
 
 static nrf_ble_gatt_t m_gatt;
 static nrf_sdh_ble_evt_observer_t m_gatt_obs __attribute__((section(".sdh_ble_observers1")))__attribute__((used)) = {nrf_ble_gatt_on_ble_evt, &m_gatt};
 
-static nrf_ble_qwr_t m_qwr;
-static nrf_sdh_ble_evt_observer_t m_qwr_obs __attribute__((section(".sdh_ble_observers2")))__attribute__((used)) = {nrf_ble_qwr_on_ble_evt, &m_qwr};
-
-struct soc_observer : tos::list_node<soc_observer>
-{
-    // nrf sdk defines the observer_ts to be const by default -_-
-    // so remove the const here
-    std::remove_const_t <nrf_sdh_soc_evt_observer_t> obs;
-};
-
-struct ble_observer : tos::list_node<ble_observer>
-{
-    // nrf sdk defines the observer_ts to be const by default -_-
-    // so remove the const here
-    std::remove_const_t <nrf_sdh_ble_evt_observer_t> obs;
-};
-
-tos::intrusive_list<ble_observer> ble_observers;
-tos::intrusive_list<soc_observer> soc_observers;
-
-static nrf_sdh_soc_evt_observer_t tos_soc_observer
-    __attribute__((section(".sdh_soc_observers1"))) __attribute__((used))
-    { [](uint32_t ev, void*) {
-        for (auto& obs : soc_observers)
-        {
-            obs.obs.handler(ev, obs.obs.p_context);
-        }
-    }, nullptr };
-
-static nrf_sdh_ble_evt_observer_t tos_ble_observer
-    __attribute__((section(".sdh_ble_observers1"))) __attribute__((used))
-    { [](ble_evt_t const* ev, void*) {
-        for (auto& obs : ble_observers)
-        {
-            obs.obs.handler(ev, obs.obs.p_context);
-        }
-    }, nullptr };
-
 static uint16_t   m_conn_handle          = BLE_CONN_HANDLE_INVALID;
-static ble_uuid_t m_adv_uuids[]          =
-{
-    {0xDDDD, BLE_UUID_TYPE_VENDOR_BEGIN}
-};
 
-#define FIRST_CONN_PARAMS_UPDATE_DELAY  APP_TIMER_TICKS(5000)                       /**< Time from initiating event (connect or start of notification) to first time sd_ble_gap_conn_param_update is called (5 seconds). */
-#define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000)                      /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
-#define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
-
-tos::fixed_fifo<char, 1024> buf;
-
-static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
+static void ble_evt_handler(ble_evt_t const * p_ble_evt, void *)
 {
     uint32_t err_code;
 
@@ -120,8 +48,6 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
         case BLE_GAP_EVT_CONNECTED:
             //APP_ERROR_CHECK(err_code);
             m_conn_handle = p_ble_evt->evt.gap_evt.conn_handle;
-            err_code = nrf_ble_qwr_conn_handle_assign(&m_qwr, m_conn_handle);
-            APP_ERROR_CHECK(err_code);
             break;
 
         case BLE_GAP_EVT_DISCONNECTED:
@@ -139,15 +65,6 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
             err_code = sd_ble_gap_phy_update(p_ble_evt->evt.gap_evt.conn_handle, &phys);
             APP_ERROR_CHECK(err_code);
         } break;
-
-        case BLE_GAP_EVT_TIMEOUT:
-        {
-            buf.push('\n');
-            buf.push('T');
-            buf.push('O');
-            buf.push('\n');
-            break;
-        }
 
         case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
             // Pairing not supported
@@ -176,7 +93,6 @@ static void ble_evt_handler(ble_evt_t const * p_ble_evt, void * p_context)
             break;
 
         default:
-            // No implementation needed.
             break;
     }
 }
@@ -185,7 +101,7 @@ static nrf_sdh_ble_evt_observer_t m_ble_observer __attribute__((section(".sdh_bl
     ble_evt_handler, nullptr};
 
 /**@brief Function for handling events from the GATT library. */
-void gatt_evt_handler(nrf_ble_gatt_t * p_gatt, nrf_ble_gatt_evt_t const * p_evt)
+void gatt_evt_handler(nrf_ble_gatt_t *, nrf_ble_gatt_evt_t const * p_evt)
 {
     if ((m_conn_handle == p_evt->conn_handle) && (p_evt->evt_id == NRF_BLE_GATT_EVT_ATT_MTU_UPDATED))
     {
@@ -205,169 +121,8 @@ void gatt_init()
     APP_ERROR_CHECK(err_code);
 }
 
-static void on_adv_evt(ble_adv_evt_t ble_adv_evt)
+static void gap_params_init()
 {
-    uint32_t err_code;
-
-    switch (ble_adv_evt)
-    {
-        case BLE_ADV_EVT_FAST:
-            //err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
-            //APP_ERROR_CHECK(err_code);
-            break;
-        case BLE_ADV_EVT_IDLE:
-            buf.push('\n');
-            buf.push('T');
-            buf.push('O');
-            buf.push('O');
-            buf.push('\n');
-            break;
-        default:
-            break;
-    }
-}
-
-namespace tos
-{
-namespace nrf52
-{
-enum class setname_errors
-{
-    invalid_addr = NRF_ERROR_INVALID_ADDR,
-    invalid_param = NRF_ERROR_INVALID_PARAM,
-    bad_size = NRF_ERROR_DATA_SIZE,
-    forbidden = NRF_ERROR_FORBIDDEN
-};
-
-enum class advertising_errors
-{
-    module_not_initialized = NRF_ERROR_INVALID_STATE
-};
-
-class softdev;
-
-class advertising
-{
-public:
-    advertising() {
-        ble_advertising_init_t init{};
-
-        init.advdata.name_type          = BLE_ADVDATA_FULL_NAME;
-        init.advdata.include_appearance = false;
-        init.advdata.flags              = BLE_GAP_ADV_FLAGS_LE_ONLY_LIMITED_DISC_MODE;
-
-        init.srdata.uuids_complete.uuid_cnt = std::size(m_adv_uuids);
-        init.srdata.uuids_complete.p_uuids  = m_adv_uuids;
-
-        init.config.ble_adv_fast_enabled  = true;
-        init.config.ble_adv_fast_interval = 64; /**< The advertising interval (in units of 0.625 ms. This value corresponds to 40 ms). */
-        init.config.ble_adv_fast_timeout  = 1800; /**< The advertising duration (18 seconds) in units of 10 milliseconds. */
-        init.evt_handler = on_adv_evt;
-
-        auto err_code = ble_advertising_init(&m_advertising, &init);
-        APP_ERROR_CHECK(err_code);
-
-        m_ble_obs.obs.handler = ble_advertising_on_ble_evt;
-        m_ble_obs.obs.p_context = &m_advertising;
-
-        m_soc_obs.obs.handler = ble_advertising_on_sys_evt;
-        m_soc_obs.obs.p_context = &m_advertising;
-
-        ble_observers.push_back(m_ble_obs);
-        soc_observers.push_back(m_soc_obs);
-
-        // https://devzone.nordicsemi.com/f/nordic-q-a/33504/what-does-app_ble_conn_cfg_tag-do
-        constexpr auto tag = 1;
-
-        ble_advertising_conn_cfg_tag_set(&m_advertising, tag);
-    }
-
-    advertising(const advertising&) = delete;
-    advertising(advertising&&) = delete;
-
-    expected<void,advertising_errors>
-    start() {
-        uint32_t err_code = ble_advertising_start(&m_advertising, BLE_ADV_MODE_FAST);
-        if (err_code == NRF_SUCCESS) return {};
-        return unexpected((advertising_errors)err_code);
-    }
-
-    ~advertising()
-    {
-        auto ble_it = std::find_if(ble_observers.begin(), ble_observers.end(), [&](auto& a) {
-            return &a == &m_ble_obs;
-        });
-        if (ble_it != ble_observers.end())
-            ble_observers.erase(ble_it);
-
-        auto soc_it = std::find_if(soc_observers.begin(), soc_observers.end(), [&](auto& a) {
-            return &a == &m_soc_obs;
-        });
-        if (soc_it != soc_observers.end())
-            soc_observers.erase(soc_it);
-    }
-
-private:
-    ble_observer m_ble_obs;
-    soc_observer m_soc_obs;
-    ble_advertising_t m_advertising;
-};
-
-class softdev
-{
-public:
-    softdev()
-    {
-        ret_code_t err_code;
-
-        err_code = nrf_sdh_enable_request();
-        //APP_ERROR_CHECK(err_code);
-        if (err_code != NRF_SUCCESS)
-        {
-            tos::this_thread::block_forever();
-        }
-
-        // Configure the BLE stack using the default settings.
-        // Fetch the start address of the application RAM.
-        uint32_t ram_start = 0;
-        err_code = nrf_sdh_ble_default_cfg_set(1, &ram_start);
-        APP_ERROR_CHECK(err_code);
-
-        // Enable BLE stack.
-        err_code = nrf_sdh_ble_enable(&ram_start);
-        APP_ERROR_CHECK(err_code);
-    }
-
-    expected<void, setname_errors>
-    set_device_name(std::string_view name)
-    {
-        ble_gap_conn_sec_mode_t sec_mode;
-
-        BLE_GAP_CONN_SEC_MODE_SET_OPEN(&sec_mode);
-
-        auto err_code = sd_ble_gap_device_name_set(&sec_mode,
-                                              (const uint8_t *) name.data(),
-                                              name.size());
-
-        if (err_code != NRF_SUCCESS)
-        {
-            return unexpected(setname_errors(err_code));
-        }
-
-        return {};
-    }
-
-private:
-};
-}
-}
-
-static void gap_params_init(tos::nrf52::softdev& sd)
-{
-    auto r = sd.set_device_name("Tos BLE");
-
-    if (!r) return;
-
     ble_gap_conn_params_t gap_conn_params {};
 
     gap_conn_params.min_conn_interval = MSEC_TO_UNITS(40, UNIT_1_25_MS);
@@ -379,113 +134,9 @@ static void gap_params_init(tos::nrf52::softdev& sd)
     APP_ERROR_CHECK(err_code);
 }
 
-auto g = tos::open(tos::devs::gpio);
-static void nus_data_handler(ble_nus_evt_t * p_evt)
-{
-    if (p_evt->type == BLE_NUS_EVT_RX_DATA)
-    {
-        auto str = tos::itoa(p_evt->params.rx_data.length, 10);
-        while (*str)
-        {
-            buf.push(*str++);
-        }
-        buf.push('\n');
-        for (uint32_t i = 0; i < p_evt->params.rx_data.length; i++)
-        {
-            using namespace tos;
-            g->write(14_pin, tos::digital::low);
-            buf.push(p_evt->params.rx_data.p_data[i]);
-        }
-        if (p_evt->params.rx_data.p_data[p_evt->params.rx_data.length - 1] == '\r')
-        {
-            buf.push('\n');
-        }
-    }
+extern const unsigned char IMAGE_DATA[4736];
 
-}
-/**@snippet [Handling the data received over BLE] */
-
-
-/**@brief Function for initializing services that will be used by the application.
- */
-static void services_init(void)
-{
-    uint32_t           err_code;
-    ble_nus_init_t     nus_init;
-    nrf_ble_qwr_init_t qwr_init;
-
-    // Initialize Queued Write Module.
-    qwr_init.error_handler = [](uint32_t nrf_error) {
-        APP_ERROR_HANDLER(nrf_error);
-    };
-
-    err_code = nrf_ble_qwr_init(&m_qwr, &qwr_init);
-    APP_ERROR_CHECK(err_code);
-
-    // Initialize NUS.
-    memset(&nus_init, 0, sizeof(nus_init));
-
-    nus_init.data_handler = nus_data_handler;
-
-    err_code = ble_nus_init(&m_nus, &nus_init);
-    APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for handling an event from the Connection Parameters Module.
- *
- * @details This function will be called for all events in the Connection Parameters Module
- *          which are passed to the application.
- *
- * @note All this function does is to disconnect. This could have been done by simply setting
- *       the disconnect_on_fail config parameter, but instead we use the event handler
- *       mechanism to demonstrate its use.
- *
- * @param[in] p_evt  Event received from the Connection Parameters Module.
- */
-static void on_conn_params_evt(ble_conn_params_evt_t * p_evt)
-{
-    uint32_t err_code;
-
-    if (p_evt->evt_type == BLE_CONN_PARAMS_EVT_FAILED)
-    {
-        err_code = sd_ble_gap_disconnect(m_conn_handle, BLE_HCI_CONN_INTERVAL_UNACCEPTABLE);
-        APP_ERROR_CHECK(err_code);
-    }
-}
-
-
-/**@brief Function for handling errors from the Connection Parameters module.
- *
- * @param[in] nrf_error  Error code containing information about what went wrong.
- */
-static void conn_params_error_handler(uint32_t nrf_error)
-{
-    APP_ERROR_HANDLER(nrf_error);
-}
-
-
-/**@brief Function for initializing the Connection Parameters module.
- */
-static void conn_params_init()
-{
-    uint32_t               err_code;
-    ble_conn_params_init_t cp_init{};
-
-    cp_init.p_conn_params                  = nullptr;
-    cp_init.first_conn_params_update_delay = FIRST_CONN_PARAMS_UPDATE_DELAY;
-    cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
-    cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
-    cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
-    cp_init.disconnect_on_fail             = false;
-    cp_init.evt_handler                    = on_conn_params_evt;
-    cp_init.error_handler                  = conn_params_error_handler;
-
-    err_code = ble_conn_params_init(&cp_init);
-    APP_ERROR_CHECK(err_code);
-}
-
-auto ble_task = [](auto& g)
+auto ble_task = []()
 {
     using namespace tos;
     using namespace tos_literals;
@@ -494,6 +145,7 @@ auto ble_task = [](auto& g)
         .add(usart_parity::disabled)
         .add(usart_stop_bit::one);
 
+    auto g = open(tos::devs::gpio);
     auto usart = open(tos::devs::usart<0>, usconf);
 
     auto mosi = 33_pin;
@@ -506,41 +158,21 @@ auto ble_task = [](auto& g)
     g->set_pin_mode(cs, tos::pin_mode::out);
     g->write(cs, tos::digital::high);
 
-    tos::nrf52::spi s(clk, 39_pin, mosi);
-
-    epd<decltype(&s)> epd(&s, cs, dc, reset, busy);
-    epd.initialize([](std::chrono::milliseconds ms) {
-        nrf_delay_ms(ms.count());
-    });
-
-    epd.ClearFrameMemory(0x00);
-    epd.DisplayFrame();
-    epd.ClearFrameMemory(0x00);
-    epd.DisplayFrame();
-
-    epd.ClearFrameMemory(0xFF);
-    epd.DisplayFrame();
-    epd.ClearFrameMemory(0xFF);
-    epd.DisplayFrame();
-
-    g->write(13_pin, digital::low);
-
     tos::println(usart, "hello");
 
     tos::nrf52::softdev sd;
-    tos::println(usart, "sd initd");
-    gap_params_init(sd);
+    auto setname_res = sd.set_device_name("Tos BLE");
+    auto tx_pow_res = sd.set_tx_power();
+    tos::println(usart, "sd initd", bool(setname_res));
+    gap_params_init();
     tos::println(usart, "gap initd");
     gatt_init();
     tos::println(usart, "gatt initd");
-    services_init();
+    auto serv = bakir::make_ble_service(usart);
     tos::println(usart, "services initd");
     tos::nrf52::advertising adv;
     tos::println(usart, "adv initd");
-    ret_code_t err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
-    conn_params_init();
-    tos::println(usart, "conn initd");
+
     auto started = adv.start();
     if (started)
     {
@@ -553,8 +185,35 @@ auto ble_task = [](auto& g)
 
     while (true)
     {
-        auto c = buf.pop();
-        usart->write({&c, 1});
+        std::array<char, 1> c;
+        serv->read(c);
+
+        if (c[0] == 'a')
+        {
+            tos::nrf52::spi s(clk, 39_pin, mosi);
+
+            epd<decltype(&s)> epd(&s, cs, dc, reset, busy);
+            epd.initialize([](std::chrono::milliseconds ms) {
+                nrf_delay_ms(ms.count());
+            });
+
+            epd.SetFrameMemory(IMAGE_DATA, 0, 0, epd.width, epd.height);
+            epd.DisplayFrame();
+        }
+        else if (c[0] == 'c')
+        {
+            tos::nrf52::spi s(clk, 39_pin, mosi);
+
+            epd<decltype(&s)> epd(&s, cs, dc, reset, busy);
+            epd.initialize([](std::chrono::milliseconds ms) {
+                nrf_delay_ms(ms.count());
+            });
+
+            epd.ClearFrameMemory(0xFF);
+            epd.DisplayFrame();
+        }
+
+        usart->write(c);
     }
 
     tos::this_thread::block_forever();
@@ -562,11 +221,5 @@ auto ble_task = [](auto& g)
 
 void TOS_EXPORT tos_main()
 {
-    using namespace tos;
-    g->set_pin_mode(13_pin, pin_mode::out);
-    g->set_pin_mode(14_pin, pin_mode::out);
-
-    tos::launch(tos::alloc_stack, ble_task, g);
-    //tos::launch(i2c_task);
-    //tos::launch(tos::alloc_stack, led2_task);
+    tos::launch(tos::alloc_stack, ble_task);
 }
