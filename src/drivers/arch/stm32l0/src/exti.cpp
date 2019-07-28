7c0a80ed24e681fca7b7527f78bfc3caac8541c3
Fatih BAKIR <mfatihbakir@gmail.com>
2019-07-27 23:04:53 -0700

    initial exti driver for stm32l

---
+++ src/drivers/arch/stm32l0/src/exti.cpp
@@ -0,0 +1,46 @@
+//
+// Created by fatih on 7/24/19.
+//
+
+#include <arch/interrupt.hpp>
+#include <libopencm3/stm32/exti.h>
+
+extern "C" {
+    void exti0_isr()
+    {
+
+    }
+    void exti1_isr()
+    {
+
+    }
+    void exti2_isr()
+    {
+
+    }
+    void exti3_isr()
+    {
+
+    }
+    void exti4_isr()
+    {
+
+    }
+    void exti9_5_isr()
+    {
+        if ((EXTI_PR & EXTI6) == EXTI6)
+        {
+            EXTI_PR = EXTI6;
+            auto it = tos::stm32::exti::get(0);
+            if (!it)
+            {
+                tos::kern::fatal("No EXTI driver instance!");
+            }
+            it->isr(EXTI6);
+        }
+    }
+    void exti15_10_isr()
+    {
+
+    }
+}
