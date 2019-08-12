a4954d92d38c0bf7c60825d274e83ca120f647cd
Fatih Bakir <mfatihbakir@gmail.com>
2019-08-11 22:29:15 -0700

    refactor esp i2c driver a bit

--- src/drivers/arch/esp8266/src/si2c.cpp
+++ src/drivers/arch/esp8266/src/si2c.cpp
@@ -4,6 +4,7 @@
 extern "C" {
 #include <user_interface.h>
 }
+namespace {
 using namespace tos::esp82;
 
 unsigned int preferred_si2c_clock = 100000;
@@ -12,11 +13,15 @@ static pin_t pin_sda, pin_scl;
 static uint32_t twi_clockStretchLimit;
 
 inline void SDA_LOW() { gpio::write(pin_sda, tos::digital::low); }
+
 inline void SDA_HIGH() { gpio::write(pin_sda, tos::digital::high); }
+
 inline auto SDA_READ() { return gpio::read(pin_sda); }
 
 inline void SCL_LOW() { gpio::write(pin_scl, tos::digital::low); }
+
 inline void SCL_HIGH() { gpio::write(pin_scl, tos::digital::high); }
+
 inline auto SCL_READ() { return gpio::read(pin_scl); }
 
 #ifndef FCPU80
@@ -30,71 +35,6 @@ inline auto SCL_READ() { return gpio::read(pin_scl); }
 #define TWI_CLOCK_STRETCH_MULTIPLIER 6
 #endif
 
-void twi_setClock(unsigned int freq) {
-    preferred_si2c_clock = freq;
-#if F_CPU == FCPU80
-    if (freq <= 50000)
-        twi_dcount = 38; // about 50KHz
-    else if (freq <= 100000)
-        twi_dcount = 19; // about 100KHz
-    else if (freq <= 200000)
-        twi_dcount = 8; // about 200KHz
-    else if (freq <= 300000)
-        twi_dcount = 3; // about 300KHz
-    else if (freq <= 400000)
-        twi_dcount = 1; // about 400KHz
-    else
-        twi_dcount = 1; // about 400KHz
-#else
-    if (freq <= 50000)
-        twi_dcount = 64; // about 50KHz
-    else if (freq <= 100000)
-        twi_dcount = 32; // about 100KHz
-    else if (freq <= 200000)
-        twi_dcount = 14; // about 200KHz
-    else if (freq <= 300000)
-        twi_dcount = 8; // about 300KHz
-    else if (freq <= 400000)
-        twi_dcount = 5; // about 400KHz
-    else if (freq <= 500000)
-        twi_dcount = 3; // about 500KHz
-    else if (freq <= 600000)
-        twi_dcount = 2; // about 600KHz
-    else
-        twi_dcount = 1; // about 700KHz
-#endif
-}
-
-void twi_setClockStretchLimit(uint32_t limit) {
-    twi_clockStretchLimit = limit * TWI_CLOCK_STRETCH_MULTIPLIER;
-}
-
-void twi_init(unsigned char sda, unsigned char scl) {
-    using namespace tos::esp82;
-    pin_sda = pin_t{sda};
-    pin_scl = pin_t{scl};
-
-    gpio::set_pin_mode(pin_sda, tos::pin_mode::in_pullup);
-    gpio::set_pin_mode(pin_scl, tos::pin_mode::in_pullup);
-
-    GPIO_REG_WRITE(GPIO_PIN_ADDR(GPIO_ID_PIN(sda)),
-                   GPIO_REG_READ(GPIO_PIN_ADDR(GPIO_ID_PIN(sda))) |
-                       GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_ENABLE)); // open drain;
-    GPIO_REG_WRITE(GPIO_ENABLE_ADDRESS, GPIO_REG_READ(GPIO_ENABLE_ADDRESS) | (1 << sda));
-    GPIO_REG_WRITE(GPIO_PIN_ADDR(GPIO_ID_PIN(scl)),
-                   GPIO_REG_READ(GPIO_PIN_ADDR(GPIO_ID_PIN(scl))) |
-                       GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_ENABLE)); // open drain;
-    GPIO_REG_WRITE(GPIO_ENABLE_ADDRESS, GPIO_REG_READ(GPIO_ENABLE_ADDRESS) | (1 << scl));
-
-    twi_setClock(preferred_si2c_clock);
-    twi_setClockStretchLimit(230); // default value is 230 uS
-}
-
-void twi_stop() {
-    gpio::set_pin_mode(pin_sda, tos::pin_mode::in);
-    gpio::set_pin_mode(pin_scl, tos::pin_mode::in);
-}
-
 #define ESP8266_REG(addr) *((volatile uint32_t*)(0x60000000 + (addr)))
 #define GPI ESP8266_REG(0x318) // GPIO_IN RO (Read Input Level)
 static void twi_delay(unsigned char v) {
@@ -105,7 +45,6 @@ static void twi_delay(unsigned char v) {
     for (i = 0; i < v; i++) reg = GPI;
 #pragma GCC diagnostic pop
 }
-
 #undef GPI
 
 static bool twi_write_start(void) {
@@ -177,6 +116,72 @@ static unsigned char twi_read_byte(bool nack) {
     for (bit = 0; bit < 8; bit++) byte = (byte << 1) | twi_read_bit();
     twi_write_bit(nack);
     return byte;
+}
+} // namespace
+
+void twi_setClock(unsigned int freq) {
+    preferred_si2c_clock = freq;
+#if F_CPU == FCPU80
+    if (freq <= 50000)
+        twi_dcount = 38; // about 50KHz
+    else if (freq <= 100000)
+        twi_dcount = 19; // about 100KHz
+    else if (freq <= 200000)
+        twi_dcount = 8; // about 200KHz
+    else if (freq <= 300000)
+        twi_dcount = 3; // about 300KHz
+    else if (freq <= 400000)
+        twi_dcount = 1; // about 400KHz
+    else
+        twi_dcount = 1; // about 400KHz
+#else
+    if (freq <= 50000)
+        twi_dcount = 64; // about 50KHz
+    else if (freq <= 100000)
+        twi_dcount = 32; // about 100KHz
+    else if (freq <= 200000)
+        twi_dcount = 14; // about 200KHz
+    else if (freq <= 300000)
+        twi_dcount = 8; // about 300KHz
+    else if (freq <= 400000)
+        twi_dcount = 5; // about 400KHz
+    else if (freq <= 500000)
+        twi_dcount = 3; // about 500KHz
+    else if (freq <= 600000)
+        twi_dcount = 2; // about 600KHz
+    else
+        twi_dcount = 1; // about 700KHz
+#endif
+}
+
+void twi_setClockStretchLimit(uint32_t limit) {
+    twi_clockStretchLimit = limit * TWI_CLOCK_STRETCH_MULTIPLIER;
+}
+
+void twi_init(unsigned char sda, unsigned char scl) {
+    using namespace tos::esp82;
+    pin_sda = pin_t{sda};
+    pin_scl = pin_t{scl};
+
+    gpio::set_pin_mode(pin_sda, tos::pin_mode::in_pullup);
+    gpio::set_pin_mode(pin_scl, tos::pin_mode::in_pullup);
+
+    GPIO_REG_WRITE(GPIO_PIN_ADDR(GPIO_ID_PIN(sda)),
+                   GPIO_REG_READ(GPIO_PIN_ADDR(GPIO_ID_PIN(sda))) |
+                       GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_ENABLE)); // open drain;
+    GPIO_REG_WRITE(GPIO_ENABLE_ADDRESS, GPIO_REG_READ(GPIO_ENABLE_ADDRESS) | (1 << sda));
+    GPIO_REG_WRITE(GPIO_PIN_ADDR(GPIO_ID_PIN(scl)),
+                   GPIO_REG_READ(GPIO_PIN_ADDR(GPIO_ID_PIN(scl))) |
+                       GPIO_PIN_PAD_DRIVER_SET(GPIO_PAD_DRIVER_ENABLE)); // open drain;
+    GPIO_REG_WRITE(GPIO_ENABLE_ADDRESS, GPIO_REG_READ(GPIO_ENABLE_ADDRESS) | (1 << scl));
+
+    twi_setClock(preferred_si2c_clock);
+    twi_setClockStretchLimit(230); // default value is 230 uS
+}
+
+void twi_stop() {
+    gpio::set_pin_mode(pin_sda, tos::pin_mode::in);
+    gpio::set_pin_mode(pin_scl, tos::pin_mode::in);
 }
 
 unsigned char twi_writeTo(unsigned char address,
