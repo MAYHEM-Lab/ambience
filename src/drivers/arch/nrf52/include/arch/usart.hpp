ee8c8ed58aac9feb100f93dc4a51ef4a300c4119
Fatih Bakir <mfatihbakir@gmail.com>
2019-08-18 16:46:14 -0700

    formatted nrf52 usart driver

--- src/drivers/arch/nrf52/include/arch/usart.hpp
+++ src/drivers/arch/nrf52/include/arch/usart.hpp
@@ -5,48 +5,45 @@
 #pragma once
 
 #include "gpio.hpp"
+
+#include <common/driver_base.hpp>
 #include <common/usart.hpp>
 #include <tos/mutex.hpp>
 #include <tos/span.hpp>
-#include <common/driver_base.hpp>
 
-namespace tos
-{
-    namespace nrf52
-    {
-        using usart_constraint =
-            ct_map<usart_key_policy,
-                    el_t<usart_baud_rate, const usart_baud_rate&>,
-                    el_t<usart_parity, const usart_parity&>,
-                    el_t<usart_stop_bit, const usart_stop_bit&>>;
+namespace tos {
+namespace nrf52 {
+using usart_constraint = ct_map<usart_key_policy,
+                                el_t<usart_baud_rate, const usart_baud_rate&>,
+                                el_t<usart_parity, const usart_parity&>,
+                                el_t<usart_stop_bit, const usart_stop_bit&>>;
 
-        class uart : public self_pointing<uart>
-        {
-        public:
-            explicit uart(usart_constraint&& config, gpio::pin_type rx = 8_pin, gpio::pin_type tx = 6_pin) noexcept;
+class uart : public self_pointing<uart> {
+public:
+    explicit uart(usart_constraint&& config,
+                  gpio::pin_type rx = 8_pin,
+                  gpio::pin_type tx = 6_pin) noexcept;
 
-            uart(const uart&) = delete;
-            uart(uart&&);
+    uart(const uart&) = delete;
+    uart(uart&&);
 
-            void write(span<const char> buf);
-            span<char> read(span<char> buf);
+    void write(span<const char> buf);
+    span<char> read(span<char> buf);
 
-            ~uart();
+    ~uart();
 
-        private:
+private:
+    void handle_callback(const void* p_event);
 
-            void handle_callback(const void *p_event);
+    tos::mutex m_write_busy;
+    tos::semaphore m_write_sync;
 
-            tos::mutex m_write_busy;
-            tos::semaphore m_write_sync;
+    tos::mutex m_read_busy;
+    tos::semaphore m_read_sync;
+};
+} // namespace nrf52
 
-            tos::mutex m_read_busy;
-            tos::semaphore m_read_sync;
-        };
-    }
-
-    inline nrf52::uart open_impl(devs::usart_t<0>, nrf52::usart_constraint&& c)
-    {
-        return nrf52::uart{ std::move(c) };
-    }
+inline nrf52::uart open_impl(devs::usart_t<0>, nrf52::usart_constraint&& c) {
+    return nrf52::uart{std::move(c)};
 }
+} // namespace tos
