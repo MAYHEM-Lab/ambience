d8c487feae571ea19b2bf0541678d227e042c82d
Fatih Bakir <mfatihbakir@gmail.com>
2019-08-23 21:30:39 -0700

    formatted and updated the adxl345 example a little

--- examples/esp_adxl345/main.cpp
+++ examples/esp_adxl345/main.cpp
@@ -2,140 +2,142 @@
 // Created by fatih on 12/23/18.
 //
 
-#include <tos/ft.hpp>
-#include <common/adxl345.hpp>
 #include <arch/drivers.hpp>
-#include <tos/print.hpp>
+#include <boost/pfr/detail/core17.hpp>
+#include <boost/pfr/detail/for_each_field_impl.hpp>
+#include <boost/pfr/flat/core.hpp>
+#include <common/adxl345.hpp>
 #include <common/gpio.hpp>
 #include <common/inet/tcp_stream.hpp>
 #include <cwpack.hpp>
-#include <boost/pfr/flat/core.hpp>
-#include <boost/pfr/detail/for_each_field_impl.hpp>
-#include <boost/pfr/detail/core17.hpp>
+#include <tos/ft.hpp>
+#include <tos/print.hpp>
 
 struct vec3i {
-  int32_t x, y, z;
+    int32_t x, y, z;
 };
 
 tos::fixed_fifo<vec3i, 20> fifo;
 
 tos::stack_storage<1024> accel_stack;
 auto task = [] {
-  tos::esp82::twim twim{ {5}, {4} };
+    using namespace tos::tos_literals;
+    auto i2c = tos::open(tos::devs::i2c<0>, tos::i2c_type::master, 5_pin, 4_pin);
 
-  tos::adxl345 sensor{twim};
-  sensor.powerOn();
-  sensor.setRangeSetting(2);
+    tos::adxl345 sensor{i2c};
+    sensor.powerOn();
+    sensor.setRangeSetting(2);
 
-  auto tmr = tos::open(tos::devs::timer<0>);
-  auto alarm = tos::open(tos::devs::alarm, tmr);
+    auto tmr = tos::open(tos::devs::timer<0>);
+    auto alarm = tos::open(tos::devs::alarm, tmr);
 
-  while (true)
-  {
-      auto [x, y, z] = sensor.read();
+    while (true) {
+        auto [x, y, z] = sensor.read();
 
-      fifo.push({x, y, z});
+        fifo.push({x, y, z});
 
-      using namespace std::chrono_literals;
-      alarm->sleep_for(200ms);
-  }
+        using namespace std::chrono_literals;
+        alarm->sleep_for(200ms);
+    }
 };
 
-auto wifi_connect()
-{
-  tos::esp82::wifi w;
+auto wifi_connect() {
+    tos::esp82::wifi w;
 
-  conn_:
-  auto res = w.connect("cs190b", "cs190bcs190b");
-  if (!res) goto conn_;
+conn_:
+    auto res = w.connect("cs190b", "cs190bcs190b");
+    if (!res)
+        goto conn_;
 
-  auto& wconn = force_get(res);
+    auto& wconn = force_get(res);
 
-  wconn.wait_for_dhcp();
-  lwip_init();
+    wconn.wait_for_dhcp();
+    lwip_init();
 
-  return std::make_pair(w, std::move(wconn));
+    return std::make_pair(w, std::move(wconn));
 }
-namespace tos
-{
-  namespace msgpack
-  {
-    template <class PackerT, class T>
-    void pack(PackerT& p, const T& value);
+namespace tos {
+namespace msgpack {
+template<class PackerT, class T>
+void pack(PackerT& p, const T& value);
 
-    template <class PackerT>
-    void pack_one(PackerT& p, int32_t i) { p.insert(i); }
+template<class PackerT>
+void pack_one(PackerT& p, int32_t i) {
+    p.insert(i);
+}
 
-    template <class PackerT>
-    void pack_one(PackerT& p, const std::string& i) { p.insert(i); }
+template<class PackerT>
+void pack_one(PackerT& p, const std::string& i) {
+    p.insert(i);
+}
 
-    template <class PackerT>
-    void pack_one(PackerT& p, float i) { p.insert(i); }
+template<class PackerT>
+void pack_one(PackerT& p, float i) {
+    p.insert(i);
+}
 
-    template <class PackerT>
-    void pack_one(PackerT& p, double i) { p.insert(i); }
+template<class PackerT>
+void pack_one(PackerT& p, double i) {
+    p.insert(i);
+}
 
-    template <class PackerT, class T>
-    void pack_one(PackerT& p, const T& t)
-    {
-      pack(p, t);
-    }
+template<class PackerT, class T>
+void pack_one(PackerT& p, const T& t) {
+    pack(p, t);
+}
 
-    template <template <class...> class TupleT, class... Ts, size_t... Is>
-    void pack_tup(arr_packer& ap, const TupleT<Ts...>& tup, std::index_sequence<Is...>)
-    {
-      using std::get;
-      (pack_one(ap, get<Is>(tup)), ...);
-    }
+template<template<class...> class TupleT, class... Ts, size_t... Is>
+void pack_tup(arr_packer& ap, const TupleT<Ts...>& tup, std::index_sequence<Is...>) {
+    using std::get;
+    (pack_one(ap, get<Is>(tup)), ...);
+}
 
-    template <class PackerT, class T>
-    void pack(PackerT& p, const T& value) {
-      constexpr std::size_t fields_count_val = boost::pfr::detail::fields_count<std::remove_reference_t<T>>();
+template<class PackerT, class T>
+void pack(PackerT& p, const T& value) {
+    constexpr std::size_t fields_count_val =
+        boost::pfr::detail::fields_count<std::remove_reference_t<T>>();
 
-      auto arr = p.insert_arr(fields_count_val);
+    auto arr = p.insert_arr(fields_count_val);
 
-      boost::pfr::detail::for_each_field_dispatcher(
-          value,
-          [&arr](const auto& val) {
+    boost::pfr::detail::for_each_field_dispatcher(
+        value,
+        [&arr](const auto& val) {
             pack_tup(arr, val, std::make_index_sequence<fields_count_val>{});
-          },
-          boost::pfr::detail::make_index_sequence<fields_count_val>{}
-      );
+        },
+        boost::pfr::detail::make_index_sequence<fields_count_val>{});
+}
+} // namespace msgpack
+} // namespace tos
+
+template<class StreamT, class ElemT>
+void push(StreamT& stream, const char* schema, const ElemT& elem) {
+    std::array<char, 32> buf;
+    tos::msgpack::packer p{buf};
+    pack(p, elem);
+    auto sp = p.get();
+    std::strncpy(buf.begin() + sp.size(), schema, buf.size() - sp.size());
+    buf[sp.size() + strlen(schema)] = strlen(schema);
+    stream->write(tos::span(buf).slice(0, sp.size() + strlen(schema) + 1));
+    tos::this_thread::yield();
+}
+
+auto wifi_task = [] {
+    auto [w, conn] = wifi_connect();
+
+    while (true) {
+        auto try_conn = tos::esp82::connect(conn, {{169, 231, 9, 60}}, {9993});
+        if (!try_conn)
+            continue;
+
+        auto& conn = force_get(try_conn);
+        tos::tcp_stream<tos::esp82::tcp_endpoint> stream{std::move(conn)};
+
+        auto elem = fifo.pop();
+        push(stream, "bakir/adxl", elem);
     }
-  }
-}
-
-template <class StreamT, class ElemT>
-void push(StreamT& stream, const char* schema, const ElemT& elem)
-{
-  std::array<char, 32> buf;
-  tos::msgpack::packer p{buf};
-  pack(p, elem);
-  auto sp = p.get();
-  std::strncpy(buf.begin() + sp.size(), schema, buf.size() - sp.size());
-  buf[sp.size() + strlen(schema)] = strlen(schema);
-  stream->write(tos::span(buf).slice(0, sp.size() + strlen(schema) + 1));
-  tos::this_thread::yield();
-}
-
-auto wifi_task = []{
-  auto [w, conn] = wifi_connect();
-
-  while (true)
-  {
-    auto try_conn = tos::esp82::connect(conn, {{169, 231, 9, 60}}, {9993});
-    if (!try_conn) continue;
-
-    auto& conn = force_get(try_conn);
-    tos::tcp_stream<tos::esp82::tcp_endpoint> stream{std::move(conn)};
-
-    auto elem = fifo.pop();
-    push(stream, "bakir/adxl", elem);
-  }
 };
 
-void tos_main()
-{
-  tos::launch(accel_stack, task);
-  tos::launch(tos::alloc_stack, wifi_task);
+void tos_main() {
+    tos::launch(accel_stack, task);
+    tos::launch(tos::alloc_stack, wifi_task);
 }
