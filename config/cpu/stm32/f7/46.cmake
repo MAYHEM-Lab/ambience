6dfb35b74e510d0d39379bd9d1086bd8af21352f
Fatih Bakir <mfatihbakir@gmail.com>
2019-08-31 16:15:20 -0700

    stm32_hal target configs

--- config/cpu/stm32/f7/46.cmake
+++ config/cpu/stm32/f7/46.cmake
@@ -1,2 +1,3 @@
 set(TOS_LD_FILE "f7/46.ld")
+set(CPU_NAME STM32F746xx)
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
--- config/cpu/stm32/l4/75.cmake
+++ config/cpu/stm32/l4/75.cmake
@@ -1,2 +1,3 @@
 set(TOS_LD_FILE "l4/75e.ld")
+set(CPU_NAME STM32L475xx)
 include(${CMAKE_CURRENT_LIST_DIR}/common.cmake)
