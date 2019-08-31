779a268ad4ef359607f8217aceb7c34385436fff
Fatih Bakir <mfatihbakir@gmail.com>
2019-08-31 16:14:33 -0700

    stm32_hal portability headers

---
+++ src/arch/stm32_hal/include/stm32_hal/rcc_ex.hpp
@@ -0,0 +1,11 @@
+/**
+ * This file includes the correct stm32xxxx_rcc_ex header
+ * depending on the tos arch configuration.
+ */
+
+#pragma once
+
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/stringize.hpp>
+
+#include BOOST_PP_STRINGIZE(BOOST_PP_CAT(STM32_HAL_NAME, _hal_rcc_ex.h))
