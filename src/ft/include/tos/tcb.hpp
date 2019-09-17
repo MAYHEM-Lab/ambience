e1219027900e253eb24bc6eede21963d41f3760d
Fatih Bakir <mfatihbakir@gmail.com>
2019-09-17 13:26:37 +0300

    formatted tcb header

--- src/ft/include/tos/tcb.hpp
+++ src/ft/include/tos/tcb.hpp
@@ -1,101 +1,91 @@
 #pragma once
 
-#include <tos/intrusive_list.hpp>
-#include <setjmp.h>
-#include <utility>
-#include <tos/utility.hpp>
-#include <tos/arch.hpp>
-#include <cstddef>
 #include <algorithm>
+#include <cstddef>
+#include <setjmp.h>
+#include <tos/arch.hpp>
+#include <tos/intrusive_list.hpp>
+#include <tos/utility.hpp>
+#include <utility>
 
 namespace tos {
-    namespace kern
-    {
-        struct ctx;
+namespace kern {
+struct ctx;
 
-        /**
-         * This type represents an execution context in the system.
-         *
-         * It's expected that a concrete task descriptor type will
-         * extend this class to implement required functionality such
-         * as starting threads or passing arguments.
-         */
-        struct alignas(alignof(std::max_align_t)) tcb
-            : public list_node<tcb>
-        {
-            /**
-             * Returns a reference to the context of the task.
-             *
-             * The function can either be called to store the current
-             * context, or to resume execution from the context.
-             *
-             * @return execution context of the task
-             */
-            ctx& get_ctx()
-            {
-                return *m_ctx;
-            }
+/**
+ * This type represents an execution context in the system.
+ *
+ * It's expected that a concrete task descriptor type will
+ * extend this class to implement required functionality such
+ * as starting threads or passing arguments.
+ */
+struct alignas(alignof(std::max_align_t)) tcb : public list_node<tcb> {
+    /**
+     * Returns a reference to the context of the task.
+     *
+     * The function can either be called to store the current
+     * context, or to resume execution from the context.
+     *
+     * @return execution context of the task
+     */
+    ctx& get_ctx() {
+        return *m_ctx;
+    }
 
-            void set_ctx(ctx& buf) {
-                m_ctx = &buf;
-            }
+    void set_ctx(ctx& buf) {
+        m_ctx = &buf;
+    }
 
-            /**
-             * The threading subsystem does not know about the concrete time
-             * at the destruction of a task, so the destructor must be virtual
-             * in order to properly destroy the concrete task descriptor.
-             */
-            virtual ~tcb() = 0;
-        private:
+    /**
+     * The threading subsystem does not know about the concrete time
+     * at the destruction of a task, so the destructor must be virtual
+     * in order to properly destroy the concrete task descriptor.
+     */
+    virtual ~tcb() = 0;
 
-            ctx *m_ctx;
-        };
+private:
+    ctx* m_ctx;
+};
 
-        inline tcb::~tcb() = default;
-    } // namespace kern
-} //namespace tos
+inline tcb::~tcb() = default;
+} // namespace kern
+} // namespace tos
 
-namespace tos
+namespace tos {
+enum class return_codes : uint8_t
 {
-  enum class return_codes : uint8_t
-  {
     saved = 0,
     /**
      * the running thread yielded
      */
-        yield,
+    yield,
     /**
      * the running thread has been suspended
      */
-        suspend,
+    suspend,
     /**
      * a thread exited
      */
-        do_exit,
+    do_exit,
     /**
      * this thread was assigned the cpu
      */
-        scheduled
-  };
+    scheduled
+};
 
-  namespace kern
-  {
-    struct ctx
-    {
-      jmp_buf buf;
-    };
+namespace kern {
+struct ctx {
+    jmp_buf buf;
+};
 
-    [[noreturn]] inline void switch_context(kern::ctx& j, return_codes rc)
-    {
-      longjmp(j.buf, static_cast<int>(rc));
+[[noreturn]] inline void switch_context(kern::ctx& j, return_codes rc) {
+    longjmp(j.buf, static_cast<int>(rc));
 
-      __builtin_unreachable();
-    }
-  }
+    __builtin_unreachable();
+}
+} // namespace kern
 } // namespace tos
 
-#define save_ctx(ctx) \
-  (return_codes)setjmp((ctx).buf)
+#define save_ctx(ctx) (return_codes) setjmp((ctx).buf)
 
-#define save_context(tcb, ctx) \
-    (tcb).set_ctx((ctx)), save_ctx(ctx)
+#define save_context(tcb, ctx) (tcb).set_ctx((ctx)), save_ctx(ctx)
