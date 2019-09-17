de9de53e93ce7b26a1d3617736ded9135c2c7991
Fatih Bakir <mfatihbakir@gmail.com>
2019-09-17 13:26:15 +0300

    tos::debug::panic interface

--- src/core/include/tos/debug.hpp
+++ src/core/include/tos/debug.hpp
@@ -5,42 +5,43 @@
 #pragma once
 
 #include <tos/arch.hpp>
+#include <tos/span.hpp>
 
-namespace tos
-{
-    template <class ErrT>
-    [[noreturn]]
-    void raise(ErrT&&);
-
-    namespace kern
-    {
-        /**
-         * This function is used to signal a non-recoverable fault in
-         * the system.
-         *
-         * The execution of the whole operating system will be suspended.
-         *
-         * In a debug environment, the OS will idle forever.
-         *
-         * In a release environment, if possible, the incident will be
-         * logged and the system will be rebooted.
-         *
-         * Should be used only for fatal errors such as broken invariants.
-         *
-         * @tparam ErrT an explanation for the crash
-         */
-        template <class ErrT>
-        [[noreturn]]
-        void fatal(ErrT&&) noexcept {
-            // or directly reset?
-            tos_force_reset();
-            //tos::this_thread::block_forever();
-        }
-    }
+namespace tos {
+namespace kern {
+/**
+ * This function is used to signal a non-recoverable fault in
+ * the system.
+ *
+ * The execution of the whole operating system will be suspended.
+ *
+ * In a debug environment, the OS will idle forever.
+ *
+ * In a release environment, if possible, the incident will be
+ * logged and the system will be rebooted.
+ *
+ * Should be used only for fatal errors such as broken invariants.
+ *
+ * @tparam ErrT an explanation for the crash
+ */
+template<class ErrT>
+[[noreturn]] void fatal(ErrT&&) noexcept {
+    // or directly reset?
+    tos_force_reset();
+    // tos::this_thread::block_forever();
 }
+} // namespace kern
+
+namespace debug {
+template<class ErrorTagType>
+void panic(ErrorTagType&& error_tag) {
+    kern::fatal(error_tag);
+}
+} // namespace debug
+} // namespace tos
 
 #if defined(TOS_ARCH_esp8266)
-extern "C" int ets_printf(const char *format, ...)  __attribute__ ((format (printf, 1, 2)));
+extern "C" int ets_printf(const char* format, ...) __attribute__((format(printf, 1, 2)));
 #define tos_debug_print ets_printf
 #endif
 
