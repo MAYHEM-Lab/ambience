f03f311394d00572b69129899dd12eebed36abc3
Fatih Bakir <mfatihbakir@gmail.com>
2019-09-18 19:08:50 +0300

    moved_flag moved to it's own header, any_spi interface

--- src/drivers/include/common/spi.hpp
+++ src/drivers/include/common/spi.hpp
@@ -4,92 +4,85 @@
 
 #pragma once
 
-#include <stdint.h>
-#include <tos/devices.hpp>
 #include <common/gpio.hpp>
+#include <cstdint>
+#include <tos/devices.hpp>
 #include <tos/expected.hpp>
+#include <tos/moved_flag.hpp>
 #include <tos/span.hpp>
 
-namespace tos
-{
-    struct moved_flag
-    {
-        moved_flag() = default;
-        moved_flag(moved_flag&& rhs) noexcept { rhs.m_moved = true; }
-
-        explicit operator bool() const { return m_moved; }
-
-        moved_flag& operator=(moved_flag&& rhs) = delete;
-
-    private:
-        bool m_moved = false;
-    };
-
-    namespace spi {
-    template <class SpiT>
-    auto exchange(SpiT& spi, uint8_t val)
-    {
-        uint8_t rx{0xFF};
-        using SpiRetT = decltype(spi->exchange(tos::monospan(rx), tos::monospan(val)));
-        using ErrT = typename SpiRetT::error_type;
-        using RetT = expected<uint8_t, ErrT>;
-        auto res = spi->exchange(tos::monospan(rx), tos::monospan(val));
-        if (!res)
-        {
-            return RetT(unexpected(force_error(res)));
-        }
-        return RetT(rx);
+namespace tos {
+namespace spi {
+template<class SpiT>
+auto exchange(SpiT& spi, uint8_t val) {
+    uint8_t rx{0xFF};
+    using SpiRetT = decltype(spi->exchange(tos::monospan(rx), tos::monospan(val)));
+    using ErrT = typename SpiRetT::error_type;
+    using RetT = expected<uint8_t, ErrT>;
+    auto res = spi->exchange(tos::monospan(rx), tos::monospan(val));
+    if (!res) {
+        return RetT(unexpected(force_error(res)));
     }
-    }
-
-
-    template<class T>
-    struct spi_transaction
-    {
-    public:
-        explicit spi_transaction(T& spi, typename T::gpio_type& gpio, typename T::gpio_type::pin_type pin)
-                : m_pin{pin}, m_g{gpio}, m_spi{spi}
-        {
-            m_g->write(m_pin, tos::digital::low);
-        }
-
-        ~spi_transaction()
-        {
-            if (!m_omit) {
-                m_g->write(m_pin, tos::digital::high);
-            }
-        }
-
-        T* operator->() {
-            return &m_spi;
-        }
-
-        spi_transaction(spi_transaction&& rhs) noexcept = default;
-
-        spi_transaction(const spi_transaction&) = delete;
-
-        spi_transaction& operator=(const spi_transaction&) = delete;
-
-        spi_transaction& operator=(spi_transaction&&) = delete;
-
-    private:
-        moved_flag m_omit;
-        T& m_spi;
-        typename T::gpio_type& m_g;
-        typename T::gpio_type::pin_type m_pin;
-    };
-
-    struct spi_mode
-    {
-        struct slave_t {};
-        struct master_t {};
-
-        static constexpr slave_t slave{};
-        static constexpr master_t master{};
-    };
-
-    namespace devs {
-        template<int N> using spi_t = dev<struct _spi_t, N>;
-        template<int N> static constexpr spi_t<N> spi{};
-    }
+    return RetT(rx);
 }
+} // namespace spi
+
+
+template<class T>
+struct spi_transaction {
+public:
+    explicit spi_transaction(T& spi,
+                             typename T::gpio_type& gpio,
+                             typename T::gpio_type::pin_type pin)
+        : m_pin{pin}
+        , m_g{gpio}
+        , m_spi{spi} {
+        m_g->write(m_pin, tos::digital::low);
+    }
+
+    ~spi_transaction() {
+        if (!m_omit) {
+            m_g->write(m_pin, tos::digital::high);
+        }
+    }
+
+    T* operator->() {
+        return &m_spi;
+    }
+
+    spi_transaction(spi_transaction&& rhs) noexcept = default;
+
+    spi_transaction(const spi_transaction&) = delete;
+
+    spi_transaction& operator=(const spi_transaction&) = delete;
+
+    spi_transaction& operator=(spi_transaction&&) = delete;
+
+private:
+    moved_flag m_omit;
+    T& m_spi;
+    typename T::gpio_type& m_g;
+    typename T::gpio_type::pin_type m_pin;
+};
+
+struct spi_mode {
+    struct slave_t {};
+    struct master_t {};
+
+    static constexpr slave_t slave{};
+    static constexpr master_t master{};
+};
+
+namespace devs {
+template<int N>
+using spi_t = dev<struct _spi_t, N>;
+template<int N>
+static constexpr spi_t<N> spi{};
+} // namespace devs
+
+struct any_spi : public self_pointing<any_spi> {
+    virtual void write(span<const char>) = 0;
+    virtual void exchange(span<char>, span<const char>) = 0;
+    virtual ~any_spi() = default;
+};
+} // namespace tos
