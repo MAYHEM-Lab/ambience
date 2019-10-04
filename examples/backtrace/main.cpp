0a1954f2a94bd1bbcca4f07d7a0d5be171db37b3
Fatih Bakir <mfatihbakir@gmail.com>
2019-10-04 16:55:47 -0700

    An example showing how to dump the current stack to uart

---
+++ examples/backtrace/main.cpp
@@ -0,0 +1,37 @@
+//
+// Created by fatih on 10/4/19.
+//
+
+#include <arch/drivers.hpp>
+#include <tos/ft.hpp>
+#include <tos/print.hpp>
+#include <tos/compiler.hpp>
+#include <tos/debug.hpp>
+
+template <class LogT>
+void NO_INLINE leaf(LogT& log)
+{
+    tos::debug::dump_stack(log);
+}
+
+template <class LogT>
+void NO_INLINE middle(LogT& log)
+{
+    leaf(log);
+}
+
+void backtrace_main() {
+    auto usart = open(tos::devs::usart<0>, tos::uart::default_9600);
+
+    middle(usart);
+
+    tos::this_thread::block_forever();
+}
+
+namespace {
+tos::stack_storage<2048> stack;
+}
+
+void tos_main() {
+    tos::launch(stack, backtrace_main);
+}
