93efac1924fc66f5def02a4b4dd6b8fbd5bfa26b
Fatih Bakir <mfatihbakir@gmail.com>
2019-10-11 13:38:32 -0700

    Formatted the capabilities examples

--- examples/capabilities/x86_main.cpp
+++ examples/capabilities/x86_main.cpp
@@ -2,39 +2,35 @@
 // Created by fatih on 3/9/19.
 //
 
-#include <arch/x86/drivers.hpp>
+#include "apps.hpp"
+#include "common.hpp"
 
-#include <tos/ft.hpp>
-#include <tos/devices.hpp>
-#include <tos/print.hpp>
-#include <tos/version.hpp>
-#include <tos/streams.hpp>
+#include <arch/x86/drivers.hpp>
 #include <fstream>
+#include <tos/devices.hpp>
+#include <tos/ft.hpp>
+#include <tos/print.hpp>
+#include <tos/streams.hpp>
+#include <tos/version.hpp>
 #include <vector>
 
-
-#include "common.hpp"
-#include "apps.hpp"
-
-std::vector<char> read_file(const std::string& path)
-{
+std::vector<char> read_file(const std::string& path) {
     std::ifstream file(path);
-    return std::vector<char>(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>{});
+    return std::vector<char>(std::istreambuf_iterator<char>(file),
+                             std::istreambuf_iterator<char>{});
 }
 
-static void x86_main()
-{
+static void x86_main() {
     auto tty = tos::open(tos::devs::tty<0>);
     tos::println(tty, "hello");
 
-//    auto f = read_file("/home/fatih/req.bin");
-//
-//    tos::imemory_stream is{f};
+    //    auto f = read_file("/home/fatih/req.bin");
+    //
+    //    tos::imemory_stream is{f};
 
     sink_task(tty);
 }
 
-void tos_main()
-{
+void tos_main() {
     tos::launch(tos::alloc_stack, x86_main);
 }
