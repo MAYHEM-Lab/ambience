f6f47c8db146e77623ee7f8f85e549ab826722c8
Fatih Bakir <mfatihbakir@gmail.com>
2019-10-25 19:05:39 -0700

    Formatted IP header and added print support for ip addresses

--- src/drivers/include/common/inet/tcp_ip.hpp
+++ src/drivers/include/common/inet/tcp_ip.hpp
@@ -4,86 +4,94 @@
 
 #pragma once
 
+#include <algorithm>
 #include <stdint.h>
 #include <string.h>
+#include <tos/print.hpp>
 #include <tos/span.hpp>
-#include <algorithm>
 
 namespace tos {
-    /**
-     * This class represents an IP port number
-     */
-    struct port_num_t {
-        uint16_t port;
-    };
+/**
+ * This class represents an IP port number
+ */
+struct port_num_t {
+    uint16_t port;
+};
 
-    struct alignas(std::uint32_t) ipv4_addr_t {
-        uint8_t addr[4];
-    };
+struct alignas(std::uint32_t) ipv4_addr_t {
+    uint8_t addr[4];
+};
 
-    struct mac_addr_t {
-        uint8_t addr[6];
-    };
+struct mac_addr_t {
+    uint8_t addr[6];
+};
 
-    inline bool operator==(const port_num_t &a, const port_num_t &b) {
-        return a.port == b.port;
-    }
-
-    inline bool operator!=(const port_num_t &a, const port_num_t &b) {
-        return a.port != b.port;
-    }
-
-    inline bool operator==(const ipv4_addr_t &a, const ipv4_addr_t &b) {
-        return memcmp(a.addr, b.addr, 4) == 0;
-    }
-
-    inline bool operator!=(const ipv4_addr_t &a, const ipv4_addr_t &b) {
-        return memcmp(a.addr, b.addr, 4) != 0;
-    }
-
-    inline ipv4_addr_t parse_ip(tos::span<const char> addr);
-
-    struct udp_endpoint_t
-    {
-        ipv4_addr_t addr;
-        port_num_t port;
-    };
+inline bool operator==(const port_num_t& a, const port_num_t& b) {
+    return a.port == b.port;
 }
 
-namespace tos{
-    inline int atoi(tos::span<const char> chars)
-    {
-        int res = 0;
-        for (auto p = chars.begin(); p != chars.end() && *p != 0; ++p)
-        {
-            res = res * 10 + *p - '0';
-        }
-        return res;
-    }
-
-    inline ipv4_addr_t parse_ip(tos::span<const char> addr) {
-        ipv4_addr_t res;
-
-        auto it = addr.begin();
-        auto end = it;
-        while (end != addr.end() && *end != '.') ++end;
-        res.addr[0] = atoi({it, end});
-
-        it = ++end;
-        end = it;
-        while (end != addr.end() && *end != '.') ++end;
-        res.addr[1] = atoi({it, end});
-
-        it = ++end;
-        end = it;
-        while (end != addr.end() && *end != '.') ++end;
-        res.addr[2] = atoi({it, end});
-
-        it = ++end;
-        end = it;
-        while (end != addr.end() && *end != '.') ++end;
-        res.addr[3] = atoi({it, end});
-
-        return res;
-    }
+inline bool operator!=(const port_num_t& a, const port_num_t& b) {
+    return a.port != b.port;
 }
+
+inline bool operator==(const ipv4_addr_t& a, const ipv4_addr_t& b) {
+    return memcmp(a.addr, b.addr, 4) == 0;
+}
+
+inline bool operator!=(const ipv4_addr_t& a, const ipv4_addr_t& b) {
+    return memcmp(a.addr, b.addr, 4) != 0;
+}
+
+inline ipv4_addr_t parse_ip(tos::span<const char> addr);
+
+struct udp_endpoint_t {
+    ipv4_addr_t addr;
+    port_num_t port;
+};
+
+template<class StreamT>
+void print(StreamT& stream, const ipv4_addr_t& addr) {
+    tos::print(stream,
+               int(addr.addr[0]),
+               int(addr.addr[1]),
+               int(addr.addr[2]),
+               int(addr.addr[3]),
+               tos::separator('.'));
+}
+} // namespace tos
+
+namespace tos {
+inline int atoi(tos::span<const char> chars) {
+    int res = 0;
+    for (auto p = chars.begin(); p != chars.end() && *p != 0; ++p) {
+        res = res * 10 + *p - '0';
+    }
+    return res;
+}
+
+inline ipv4_addr_t parse_ip(tos::span<const char> addr) {
+    ipv4_addr_t res;
+
+    auto it = addr.begin();
+    auto end = it;
+    while (end != addr.end() && *end != '.') ++end;
+    res.addr[0] = atoi({it, end});
+
+    it = ++end;
+    end = it;
+    while (end != addr.end() && *end != '.') ++end;
+    res.addr[1] = atoi({it, end});
+
+    it = ++end;
+    end = it;
+    while (end != addr.end() && *end != '.') ++end;
+    res.addr[2] = atoi({it, end});
+
+    it = ++end;
+    end = it;
+    while (end != addr.end() && *end != '.') ++end;
+    res.addr[3] = atoi({it, end});
+
+    return res;
+}
+} // namespace tos
