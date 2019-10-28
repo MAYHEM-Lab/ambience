6580674c8b1535757c9cdb852f3a7188bb796fdf
Fatih Bakir <mfatihbakir@gmail.com>
2019-10-28 16:49:54 -0700

    Formatted and slightly improved ring_buf

--- src/util/include/tos/ring_buf.hpp
+++ src/util/include/tos/ring_buf.hpp
@@ -8,84 +8,88 @@
 
 namespace tos
 {
-    template <class T>
-    class base_ring_buf
+template <class T>
+class base_ring_buf
+{
+public:
+    explicit base_ring_buf(size_t sz)
+        : m_cap{sz} {}
+
+    explicit base_ring_buf(size_t sz, size_t begin)
+        : m_begin{begin}, m_cap{sz} {}
+
+    size_t capacity() const
     {
-    public:
-        explicit base_ring_buf(size_t sz)
-                : m_cap{sz} {}
+        return m_cap;
+    }
 
-        explicit base_ring_buf(size_t sz, size_t begin)
-                : m_begin{begin}, m_cap{sz} {}
-
-        size_t capacity() const
-        {
-            return m_cap;
-        }
-
-        size_t translate(size_t index) const
-        {
-            return (m_begin + index) % m_cap;
-        }
-
-    protected:
-        size_t push_base()
-        {
-            return (m_begin + get_size()) % m_cap;
-        }
-
-        size_t pop_base()
-        {
-            auto res = m_begin;
-            m_begin = (m_begin + 1) % m_cap;
-            return res;
-        }
-
-    private:
-        size_t get_size() const
-        {
-            return static_cast<const T*>(this)->size();
-        }
-
-        size_t m_begin = 0;
-        size_t m_cap;
-    };
-
-
-    class ring_buf
-            : public base_ring_buf<ring_buf>
+    size_t translate(size_t index) const
     {
-    public:
-        /**
-         * Constructs a new ring buffer with the given capacity
-         * @param cap capacity
-         */
-        explicit ring_buf(size_t cap) : base_ring_buf{cap}, m_sz{0} {}
+        return (m_begin + index) % m_cap;
+    }
 
-        ring_buf(size_t cap, size_t sz, size_t begin) : base_ring_buf{cap, begin}, m_sz{sz} {}
+protected:
+    size_t push_base()
+    {
+        return (m_begin + get_size()) % m_cap;
+    }
 
-        size_t size() const { return m_sz; }
+    size_t pop_base()
+    {
+        auto res = m_begin;
+        m_begin = (m_begin + 1) % m_cap;
+        return res;
+    }
 
-        using base_ring_buf::capacity;
-        using base_ring_buf::translate;
+private:
+    size_t get_size() const
+    {
+        return static_cast<const T*>(this)->size();
+    }
 
-        size_t push()
-        {
-            auto res = push_base();
-            m_sz++;
-            return res;
-        }
+    size_t m_begin = 0;
+    size_t m_cap;
+};
 
-        size_t pop()
-        {
-            auto res = pop_base();
-            m_sz--;
-            return res;
-        }
 
-    private:
-        size_t m_sz;
-    };
+class ring_buf
+    : public base_ring_buf<ring_buf>
+{
+public:
+    /**
+     * Constructs a new ring buffer with the given capacity
+     * @param cap capacity
+     */
+    explicit ring_buf(size_t cap) : base_ring_buf{cap}, m_sz{0} {}
 
-    class sync_ring_buf;
+    ring_buf(size_t cap, size_t sz, size_t begin) : base_ring_buf{cap, begin}, m_sz{sz} {}
+
+    size_t size() const { return m_sz; }
+
+    using base_ring_buf::capacity;
+    using base_ring_buf::translate;
+
+    size_t push()
+    {
+        auto res = push_base();
+        m_sz++;
+        return res;
+    }
+
+    size_t pop()
+    {
+        auto res = pop_base();
+        m_sz--;
+        return res;
+    }
+
+    bool empty() const {
+        return size() == 0;
+    }
+
+private:
+    size_t m_sz;
+};
+
+class sync_ring_buf;
 }
