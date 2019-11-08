7ed5ed4eeba51345f90a795fd2b0865bfef4af26
Fatih Bakir <mfatihbakir@gmail.com>
2019-11-07 16:26:32 -0800

    Fix span equality comparisons

--- src/util/include/tos/span.hpp
+++ src/util/include/tos/span.hpp
@@ -45,7 +45,8 @@ public:
      */
     constexpr span(T* base, size_t len)
         : m_base(base)
-        , m_len(len) {}
+        , m_len(len) {
+    }
 
     /**
      * Constructs a span from the given iterator pair.
@@ -58,7 +59,8 @@ public:
      */
     constexpr span(T* base, T* end)
         : m_base{base}
-        , m_len{end - base} {}
+        , m_len{end - base} {
+    }
 
     /**
      * Constructs a span from the given C-style array object
@@ -72,7 +74,8 @@ public:
     template<size_t Sz>
     constexpr span(T (&arr)[Sz])
         : m_base(arr)
-        , m_len(Sz) {}
+        , m_len(Sz) {
+    }
 
     /**
      * Constructs a span from the given C++ array object
@@ -83,7 +86,8 @@ public:
     template<size_t Sz>
     constexpr span(std::array<T, Sz>& arr)
         : m_base(arr.data())
-        , m_len(arr.size()) {}
+        , m_len(arr.size()) {
+    }
 
     /**
      * Constructs a const span from the given C++ array to const objects
@@ -94,7 +98,8 @@ public:
     template<size_t Sz>
     constexpr span(const std::array<std::remove_const_t<T>, Sz>& arr)
         : m_base(arr.data())
-        , m_len(arr.size()) {}
+        , m_len(arr.size()) {
+    }
 
     /**
      * Constructs a span from the given C++ vector object
@@ -103,7 +108,8 @@ public:
      */
     constexpr span(std::vector<T>& arr)
         : m_base(arr.data())
-        , m_len(arr.size()) {}
+        , m_len(arr.size()) {
+    }
     /**
      * Constructs a const span from the given C++ vector to const objects
      *
@@ -111,57 +117,91 @@ public:
      */
     constexpr span(const std::vector<std::remove_const_t<T>>& arr)
         : m_base(arr.data())
-        , m_len(arr.size()) {}
+        , m_len(arr.size()) {
+    }
 
     /**
      * Returns the number of elements in the span
      * @return tne number of elements
      */
-    constexpr size_t size() const { return m_len; }
+    constexpr size_t size() const {
+        return m_len;
+    }
 
     /**
      * Returns the total number bytes occupied by the elements in the spen
      * @return number of bytes
      */
-    constexpr size_t size_bytes() const { return size() * sizeof(T); }
-
-    /**
-     * Returns the pointer to the beginning of the range of the span
-     * @return pointer to the beginning of the range
-     */
-    constexpr T* data() { return m_base; }
-
-    /**
-     * Returns the pointer to the beginning of the range of the span
-     * @return pointer to the beginning of the range
-     */
-    constexpr const T* data() const { return m_base; }
-
-    constexpr T& operator[](size_t ind) { return m_base[ind]; }
-
-    constexpr const T& operator[](size_t ind) const { return m_base[ind]; }
-
-    constexpr T* begin() { return m_base; }
-    constexpr T* end() { return m_base + m_len; }
-
-    constexpr const T* begin() const { return m_base; }
-    constexpr const T* end() const { return m_base + m_len; }
-
-    constexpr bool empty() const { return m_len == 0; }
-
-    span<const uint8_t> as_bytes() const {
-        return span<const uint8_t>{reinterpret_cast<const uint8_t*>(data()), size() * sizeof(T)};
+    constexpr size_t size_bytes() const {
+        return size() * sizeof(T);
     }
 
-    T& front() { return *begin(); }
-    const T& front() const { return *begin(); }
+    /**
+     * Returns the pointer to the beginning of the range of the span
+     * @return pointer to the beginning of the range
+     */
+    constexpr T* data() {
+        return m_base;
+    }
+
+    /**
+     * Returns the pointer to the beginning of the range of the span
+     * @return pointer to the beginning of the range
+     */
+    constexpr const T* data() const {
+        return m_base;
+    }
+
+    constexpr T& operator[](size_t ind) {
+        return m_base[ind];
+    }
+
+    constexpr const T& operator[](size_t ind) const {
+        return m_base[ind];
+    }
+
+    constexpr T* begin() {
+        return m_base;
+    }
+    constexpr T* end() {
+        return m_base + m_len;
+    }
+
+    constexpr const T* begin() const {
+        return m_base;
+    }
+    constexpr const T* end() const {
+        return m_base + m_len;
+    }
+
+    constexpr bool empty() const {
+        return m_len == 0;
+    }
+
+    /*std::enable_if_t<!std::is_const_v<T>, span<uint8_t>> as_bytes() {
+        return span<uint8_t>{reinterpret_cast<uint8_t*>(data()), size() * sizeof(T)};
+    }*/
+
+    span<const uint8_t> as_bytes() const {
+        return span<const uint8_t>{reinterpret_cast<const uint8_t*>(data()),
+                                   size() * sizeof(T)};
+    }
+
+    T& front() {
+        return *begin();
+    }
+    const T& front() const {
+        return *begin();
+    }
 
     /**
      * Spans convert to a const version of them automatically
      *
      * @return constant version of this span
      */
-    constexpr operator span<const T>() const { return {m_base, size_t(m_len)}; }
+    constexpr operator span<const T>() const {
+        return {m_base, size_t(m_len)};
+    }
 
     /**
      * Takes a slice from this span.
@@ -170,7 +210,9 @@ public:
      * @param len length of the slice
      * @return a new span
      */
-    constexpr span slice(size_t begin, size_t len) { return {m_base + begin, len}; }
+    constexpr span slice(size_t begin, size_t len) {
+        return {m_base + begin, len};
+    }
 
     /**
      * Takes a slice from this span.
@@ -178,18 +220,14 @@ public:
      * @param begin beginning index of the slice
      * @return a new span
      */
-    constexpr span slice(size_t begin) { return {m_base + begin, size() - begin}; }
+    constexpr span slice(size_t begin) {
+        return {m_base + begin, size() - begin};
+    }
 
 private:
     T* m_base;
     ptrdiff_t m_len;
 };
-
-template <class T>
-constexpr bool operator==(span<T> l, span<T> r)
-{
-    return l.size() == r.size() && std::equal(l.begin(), l.end(), r.begin(), r.end());
-}
 
 template<class T>
 span<T> empty_span() {
@@ -218,13 +256,28 @@ span<T> raw_cast(span<U> sp) {
     return {reinterpret_cast<T*>(sp.data()), sp.size() * sizeof(U)};
 }
 
-template <class T>
-constexpr bool operator==(tos::span<const T> left, span<const T> right)
-{
+template<class T>
+constexpr bool operator==(tos::span<const T> left, span<const T> right) {
     if (left.size() != right.size()) {
         return false;
     }
 
     return std::equal(left.begin(), left.end(), right.begin());
+}
+
+template<class T>
+constexpr bool operator==(tos::span<T> left, span<const T> right) {
+    return static_cast<tos::span<const T>>(left) == right;
+}
+
+template<class T>
+constexpr bool operator==(tos::span<const T> left, span<T> right) {
+    return left == static_cast<tos::span<const T>>(right);
+}
+
+template<class T>
+constexpr bool operator==(tos::span<T> left, span<T> right) {
+    return static_cast<tos::span<const T>>(left) ==
+           static_cast<tos::span<const T>>(right);
 }
 } // namespace tos
