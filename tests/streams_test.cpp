7c7dcb0fa039e192b6675088965cdc81404aac05
Fatih Bakir <mfatihbakir@gmail.com>
2019-11-07 17:02:52 -0800

    More tests for streams

--- tests/streams_test.cpp
+++ tests/streams_test.cpp
@@ -33,9 +33,9 @@ TEST_CASE("read_until works with correct input of size 1") {
     std::array<char, 8> buf;
     auto sep = '\n';
     auto read = tos::read_until<char>(mock, tos::monospan(sep), buf);
-        REQUIRE_EQ(2, read.size());
-        REQUIRE_EQ('f', read[0]);
-        REQUIRE_EQ('\n', read[1]);
+    REQUIRE_EQ(2, read.size());
+    REQUIRE_EQ('f', read[0]);
+    REQUIRE_EQ('\n', read[1]);
 }
 
 TEST_CASE("read_until works with CR/LF") {
@@ -43,12 +43,34 @@ TEST_CASE("read_until works with CR/LF") {
     mock.write("f\r\ny");
 
     std::array<char, 8> buf;
-    char sep[] = { '\r', '\n' };
+    char sep[] = {'\r', '\n'};
     auto read = tos::read_until<char>(mock, sep, buf);
-        REQUIRE_EQ(3, read.size());
-        REQUIRE_EQ('f', read[0]);
-        REQUIRE_EQ('\r', read[1]);
-        REQUIRE_EQ('\n', read[2]);
+    REQUIRE_EQ(3, read.size());
+    REQUIRE_EQ('f', read[0]);
+    REQUIRE_EQ('\r', read[1]);
+    REQUIRE_EQ('\n', read[2]);
+}
+
+TEST_CASE("read_until returns correctly on exact fit") {
+    mock_uart mock;
+    const char msg[] = "hellow\r\n";
+    mock.write(msg);
+    std::array<char, 8> buf;
+    char sep[] = {'\r', '\n'};
+    auto read = tos::read_until<char>(mock, sep, buf);
+    REQUIRE_EQ(buf.size(), read.size());
+    REQUIRE_EQ(tos::span(msg).slice(0, read.size()), read);
+}
+
+TEST_CASE("read_until returns if buffer is full") {
+    mock_uart mock;
+    const char msg[] = "hello world foo bar\r\n";
+    mock.write(msg);
+    std::array<char, 8> buf;
+    char sep[] = {'\r', '\n'};
+    auto read = tos::read_until<char>(mock, sep, buf);
+    REQUIRE_EQ(buf.size(), read.size());
+    REQUIRE_EQ(tos::span(msg).slice(0, read.size()), read);
 }
 } // namespace
 } // namespace tos
