057fe0341f122b1c53355b23f0aac4eebf7a9a90
Fatih Bakir <mfatihbakir@gmail.com>
2019-12-03 15:14:05 -0800

    format atmega spi examples and driver

--- examples/spi/slave.cpp
+++ examples/spi/slave.cpp
@@ -2,34 +2,31 @@
 // Created by fatih on 4/15/18.
 //
 
-#include <common/spi.hpp>
 #include <arch/drivers.hpp>
+#include <common/spi.hpp>
 #include <tos/ft.hpp>
 #include <tos/print.hpp>
 
-void slave_task()
-{
+void slave_task() {
     using namespace tos::tos_literals;
     auto spi = open(tos::devs::spi<0>, tos::spi_mode::slave);
     spi.enable();
 
     constexpr auto usconf = tos::usart_config()
-            .add(9600_baud_rate)
-            .add(tos::usart_parity::disabled)
-            .add(tos::usart_stop_bit::one);
+                                .add(9600_baud_rate)
+                                .add(tos::usart_parity::disabled)
+                                .add(tos::usart_stop_bit::one);
 
     auto usart = open(tos::devs::usart<0>, usconf);
 
     tos::println(usart, "Hi from slave!");
 
-    while (true)
-    {
+    while (true) {
         char c = spi.exchange(42);
         tos::println(usart, c);
     }
 }
 
-void tos_main()
-{
+void tos_main() {
     tos::launch(tos::alloc_stack, slave_task);
 }
--- src/drivers/arch/atmega328p/include/arch/spi.hpp
+++ src/drivers/arch/atmega328p/include/arch/spi.hpp
@@ -4,58 +4,56 @@
 
 #pragma once
 
-#include <cstdint>
-#include <tos/devices.hpp>
-#include <common/spi.hpp>
-#include <tos/span.hpp>
-#include <common/driver_base.hpp>
 #include "gpio.hpp"
 
+#include <common/driver_base.hpp>
+#include <common/spi.hpp>
+#include <cstdint>
+#include <tos/devices.hpp>
+#include <tos/span.hpp>
+
 namespace tos {
-    namespace avr {
-        /**
-         * This class implements the AVR spi hardware.
-         *
-         * It's one of the first drivers implemented in tos, so it's
-         * not as nice as I'd like it to be and is probably in need
-         * for a rehaul, specifically, it has both master and slave
-         * interfaces for SPI, only one of which can be used at one
-         * time.
-         *
-         * Also, it does not support setting the clock speed for now.
-         */
-        class spi0 : public self_pointing<spi0>
-        {
-        public:
-            using gpio_type = avr::gpio;
+namespace avr {
+/**
+ * This class implements the AVR spi hardware.
+ *
+ * It's one of the first drivers implemented in tos, so it's
+ * not as nice as I'd like it to be and is probably in need
+ * for a rehaul, specifically, it has both master and slave
+ * interfaces for SPI, only one of which can be used at one
+ * time.
+ *
+ * Also, it does not support setting the clock speed for now.
+ */
+class spi0 : public self_pointing<spi0> {
+public:
+    using gpio_type = avr::gpio;
 
-            static void init_master();
+    static void init_master();
 
-            static void init_slave();
+    static void init_slave();
 
-            static void enable();
+    static void enable();
 
-            static void disable();
+    static void disable();
 
-            static expected<void, int> exchange(tos::span<uint8_t> buffer);
+    static expected<void, int> exchange(tos::span<uint8_t> buffer);
 
-            static void select_slave(pin_t pin);
+    static void select_slave(pin_t pin);
 
-            static void deselect_slave(pin_t pin);
+    static void deselect_slave(pin_t pin);
 
-            spi0() = default;
-        };
-    }
+    spi0() = default;
+};
+} // namespace avr
 
-    inline avr::spi0 open_impl(tos::devs::spi_t<0> /*tag*/, spi_mode::master_t /*type*/)
-    {
-        avr::spi0::init_master();
-        return {};
-    }
+inline avr::spi0 open_impl(tos::devs::spi_t<0> /*tag*/, spi_mode::master_t /*type*/) {
+    avr::spi0::init_master();
+    return {};
+}
 
-    inline avr::spi0 open_impl(tos::devs::spi_t<0> /*tag*/, spi_mode::slave_t /*type*/)
-    {
-        avr::spi0::init_slave();
-        return {};
-    } // namespace avr
+inline avr::spi0 open_impl(tos::devs::spi_t<0> /*tag*/, spi_mode::slave_t /*type*/) {
+    avr::spi0::init_slave();
+    return {};
+} // namespace avr
 } // namespace tos
