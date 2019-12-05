4474cb599c056b4e68cd5e1415faec66ba09560f
Fatih Bakir <mfatihbakir@gmail.com>
2019-12-04 17:34:38 -0800

    suspend_self takes a no_interrupts rather than int_guard reference

--- src/ft/include/tos/scheduler.hpp
+++ src/ft/include/tos/scheduler.hpp
@@ -54,7 +54,7 @@ void make_runnable(struct tcb& t);
  * If the interrupts are not disabled when this function
  * is called, the behaviour is undefined
  */
-void suspend_self(const int_guard&);
+void suspend_self(const no_interrupts&);
 // pre-condition: interrupts must be disabled
 
 /**
