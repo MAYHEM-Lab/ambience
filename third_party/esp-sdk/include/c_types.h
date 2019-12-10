2ce35243d3c3c22164d2487ea895540830036c01
Fatih Bakir <mfatihbakir@gmail.com>
2019-12-10 15:45:01 +0300

    Patch c_types.h to remove custom uintX_t types

--- third_party/esp-sdk/include/c_types.h
+++ third_party/esp-sdk/include/c_types.h
@@ -25,20 +25,7 @@
 #ifndef _C_TYPES_H_
 #define _C_TYPES_H_
 
-typedef unsigned char       uint8_t;
-typedef signed char         sint8_t;
-typedef signed char         int8_t;
-typedef unsigned short      uint16_t;
-typedef signed short        sint16_t;
-typedef signed short        int16_t;
-typedef unsigned int        uint32_t;
-typedef signed int          sint32_t;
-typedef signed int          int32_t;
-typedef signed long long    sint64_t;
-typedef unsigned long long  uint64_t;
-typedef unsigned long long  u_int64_t;
-typedef float               real32_t;
-typedef double              real64_t;
+#include <stdint.h>
 
 typedef unsigned char       uint8;
 typedef unsigned char       u8;
