a752107a55f335aefd617fd59c80fc26dec0b931
Fatih Bakir <mfatihbakir@gmail.com>
2019-12-18 10:57:49 +0300

    Add a new test for omemory_stream

---
+++ src/util/src/mem_stream_test.cpp
@@ -0,0 +1,17 @@
+#include <array>
+#include <doctest.h>
+#include <tos/mem_stream.hpp>
+
+
+namespace tos {
+namespace {
+TEST_CASE("Output memory streams work") {
+    std::array<uint8_t, 128> buf;
+    omemory_stream str(buf);
+    const char data[] = {'h', 'e', 'l', 'l', 'o'};
+    str.write(raw_cast<const uint8_t>(span(data)));
+    REQUIRE_EQ(5, str.get().size());
+    REQUIRE_EQ(span<const char>(data), str.get());
+}
+} // namespace
+} // namespace tos
