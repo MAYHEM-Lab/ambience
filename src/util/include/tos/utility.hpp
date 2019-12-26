8866bd9b870949fa7c99d357fb9f275567dbe4e7
Fatih Bakir <mfatihbakir@gmail.com>
2019-12-26 06:05:49 -0800

    Format utility header, add make_overload to generate visitors on the fly

--- src/util/include/tos/utility.hpp
+++ src/util/include/tos/utility.hpp
@@ -3,47 +3,60 @@
 //
 #pragma once
 
-#include <stdint.h>
-#include <stddef.h>
 #include <chrono>
+#include <stddef.h>
+#include <stdint.h>
+#include <type_traits>
 #include <utility>
 
 namespace tos {
-    template<class T> using invoke_t = typename T::type;
+template<class T>
+using invoke_t = typename T::type;
 
-    template<class S1, class S2>
-    struct concat;
+template<class S1, class S2>
+struct concat;
 
-    template<class T, T... I1, T... I2>
-    struct concat<std::integer_sequence<T, I1...>, std::integer_sequence<T, I2...>>
-    : std::integer_sequence<T, I1..., (sizeof...(I1)+I2)...>
-    {
-    };
+template<class T, T... I1, T... I2>
+struct concat<std::integer_sequence<T, I1...>, std::integer_sequence<T, I2...>>
+    : std::integer_sequence<T, I1..., (sizeof...(I1) + I2)...> {};
 
-    template<class S1, class S2>
-    using concat_t = invoke_t<concat<S1, S2>>;
+template<class S1, class S2>
+using concat_t = invoke_t<concat<S1, S2>>;
 
-    struct non_copyable
-    {
-        non_copyable() = default;
-        non_copyable(const non_copyable&) = delete;
-        non_copyable(non_copyable&&) = default;
-        non_copyable& operator=(const non_copyable&) = delete;
-        non_copyable& operator=(non_copyable&&) = default;
-        ~non_copyable() = default;
-    };
+struct non_copyable {
+    non_copyable() = default;
+    non_copyable(const non_copyable&) = delete;
+    non_copyable(non_copyable&&) = default;
+    non_copyable& operator=(const non_copyable&) = delete;
+    non_copyable& operator=(non_copyable&&) = default;
+    ~non_copyable() = default;
+};
 
-    struct non_movable
-    {
-        non_movable() = default;
-        non_movable(const non_movable&) = default;
-        non_movable(non_movable&&) = delete;
-        non_movable& operator=(const non_movable&) = default;
-        non_movable& operator=(non_movable&&) = delete;
-        ~non_movable() = default;
-    };
+struct non_movable {
+    non_movable() = default;
+    non_movable(const non_movable&) = default;
+    non_movable(non_movable&&) = delete;
+    non_movable& operator=(const non_movable&) = default;
+    non_movable& operator=(non_movable&&) = delete;
+    ~non_movable() = default;
+};
 
-    struct non_copy_movable : non_copyable, non_movable {};
+struct non_copy_movable
+    : non_copyable
+    , non_movable {};
 
+template<class... Fs>
+struct overload : Fs... {
+    template<class... Ts>
+    overload(Ts&&... ts)
+        : Fs{std::forward<Ts>(ts)}... {
+    }
+
+    using Fs::operator()...;
+};
+
+template<class... Ts>
+auto make_overload(Ts&&... ts) {
+    return overload<std::remove_reference_t<Ts>...>(std::forward<Ts>(ts)...);
+}
 } // namespace tos
-
