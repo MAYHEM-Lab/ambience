c74bb6870b06bcea6dd1d5fa3743ccf5b1a73afb
Fatih BAKIR <bakir@ucsb.edu>
2020-02-15 17:11:52 -0800

    WIP flash driver for nrf52

---
+++ src/drivers/arch/nrf52/src/flash.cpp
@@ -0,0 +1,20 @@
+#include <arch/flash.hpp>
+#include <nrfx_nvmc.h>
+
+namespace tos::nrf52 {
+flash::flash() {
+}
+
+size_t flash::sector_count() const {
+    return nrfx_nvmc_flash_page_count_get();
+}
+
+size_t flash::sector_size_bytes() const {
+    return nrfx_nvmc_flash_page_size_get();
+}
+
+expected<void, flash_errors> flash::erase(sector_id_t sector_id) {
+    auto res = nrfx_nvmc_page_erase(translate_address(sector_id, 0));
+    return {};
+}
+} // namespace tos::nrf52
