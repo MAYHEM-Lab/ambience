07608fbbc2fd09e45721a478b7e3963fddd27f60
Fatih BAKIR <bakir@ucsb.edu>
2020-02-15 22:45:36 -0800

    Compare milliseconds directly for printable output

--- tests/alarm_tests.cpp
+++ tests/alarm_tests.cpp
@@ -2,27 +2,28 @@
 // Created by fatih on 12/10/18.
 //
 
-#include <tos/ft.hpp>
 #include "doctest.h"
+
 #include <arch/drivers.hpp>
 #include <common/alarm.hpp>
+#include <tos/ft.hpp>
 #include <tos/semaphore.hpp>
 
-TEST_CASE("alarm"){
+TEST_CASE("alarm") {
     tos::semaphore s{0};
-    tos::launch(tos::alloc_stack, [&]{
+    tos::launch(tos::alloc_stack, [&] {
         tos::x86::timer tmr;
         auto alarm = tos::open(tos::devs::alarm, tmr);
 
-        for (int i = 0; i < 50; ++i)
-        {
+        for (int i = 0; i < 50; ++i) {
             auto tm = std::chrono::system_clock::now();
             using namespace std::chrono_literals;
             tos::this_thread::sleep_for(alarm, 100ms);
-            auto diff = std::chrono::system_clock::now() - tm;
+            auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(
+                std::chrono::system_clock::now() - tm);
 
-            CHECK_LE(100ms, diff);
-            CHECK_GE(130ms, diff);
+            CHECK_LE(100, diff.count());
+            CHECK_GE(130, diff.count());
         }
 
         s.up();
