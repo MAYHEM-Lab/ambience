8d858dc980a716ba4f892e39b1a938a9ff4b82ae
Fatih Bakir <mfatihbakir@gmail.com>
2020-02-28 17:16:09 -0800

    Move bss zeroing to C++

--- src/arch/aarch64/impl.cpp
+++ src/arch/aarch64/impl.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
 #include <tos/ft.hpp>
@@ -7,10 +8,14 @@ extern void tos_main();
 extern "C" {
 extern void (*start_ctors[])(void);
 extern void (*end_ctors[])(void);
+extern uint32_t* __bss_start;
+extern uint32_t __bss_size;
 }
 
-extern "C"
-void kernel_main([[maybe_unused]]uint32_t r0, [[maybe_unused]]uint32_t r1, [[maybe_unused]]uint32_t atags) {
+extern "C" void kernel_main([[maybe_unused]] uint32_t r0,
+                            [[maybe_unused]] uint32_t r1,
+                            [[maybe_unused]] uint32_t atags) {
+    std::fill_n(__bss_start, __bss_size, 0);
     std::for_each(start_ctors, end_ctors, [](auto ctor) { ctor(); });
 
     tos_main();
