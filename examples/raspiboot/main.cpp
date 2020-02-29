8fb11a1ef4c4ace28baf15115b3c7500a37839b7
Fatih Bakir <mfatihbakir@gmail.com>
2020-02-28 17:16:46 -0800

    Print execution level upon reaching the thread

--- examples/raspiboot/main.cpp
+++ examples/raspiboot/main.cpp
@@ -8,6 +8,10 @@ void tos_main() {
         tos::raspi3::uart0 uart;
         tos::println(uart, "Hello from tos!");
 
+        uint32_t el;
+        asm volatile("mrs %0, CurrentEL" : "=r"(el));
+        tos::println(uart, "Execution Level:", int((el >> 2) & 3));
+
         while (true) {
             uint8_t c;
             uart->write(uart->read(tos::monospan(c)));
