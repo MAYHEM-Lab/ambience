c593411a5a86fce9d9776ed4c27a2a59da616cef
Fatih Bakir <mfatihbakir@gmail.com>
2020-03-19 01:32:53 -0700

    A few missing c++ stuff from standard library

--- src/util/src/cxxrt.cpp
+++ src/util/src/cxxrt.cpp
@@ -2,9 +2,21 @@
 // Created by fatih on 5/17/18.
 //
 
+#include <ios>
 extern "C"
 {
+    [[gnu::weak]]
     void __cxa_pure_virtual()
     {
     }
+
+    void __cxa_atexit(void (*destructor) (void *), void *arg, void *__dso_handle) {
+
+    }
+}
+
+namespace std {
+ios_base::Init::Init() {}
+ios_base::Init::~Init() {
+}
 }
