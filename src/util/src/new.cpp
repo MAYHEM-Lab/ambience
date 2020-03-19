9d8fb4653cfa9e332f41c0d124d846bf1b77da1c
Fatih Bakir <mfatihbakir@gmail.com>
2020-03-19 01:31:53 -0700

    align free list allocations to 16 bytes, use a 1M fixed heap for now

--- src/util/src/new.cpp
+++ src/util/src/new.cpp
@@ -2,34 +2,52 @@
 // Created by Mehmet Fatih BAKIR on 14/05/2018.
 //
 
-#include <stdlib.h>
+#include "../../core/include/tos/memory/free_list.hpp"
+
 #include <new>
 
+alignas(16) uint8_t heap[1024*1024];
+tos::memory::free_list alloc(heap);
+
 void operator delete (void* pt, size_t){
-    free(pt);
+    alloc.free(pt);
 }
 
 void operator delete (void* pt)
 {
-    free(pt);
+    alloc.free(pt);
 }
 
 void operator delete[] (void* pt)
 {
-    free(pt);
+    alloc.free(pt);
 }
 
 void operator delete[] (void* pt, size_t)
 {
-    free(pt);
+    alloc.free(pt);
 }
 
 void* operator new(size_t sz)
 {
-    return malloc(sz);
+    auto ptr = alloc.allocate(sz);
+    return ptr;
 }
 
 void* operator new[](size_t sz)
 {
-    return malloc(sz);
+    auto ptr = alloc.allocate(sz);
+    return ptr;
+}
+
+void* operator new(size_t sz, const std::nothrow_t&) noexcept
+{
+    auto ptr = alloc.allocate(sz);
+    return ptr;
+}
+
+void* operator new[](size_t sz, const std::nothrow_t&) noexcept
+{
+    auto ptr = alloc.allocate(sz);
+    return ptr;
 }
