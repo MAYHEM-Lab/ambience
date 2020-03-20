ba8dc813911671ab8f217f7c2e1671ea47ccfb72
Fatih Bakir <mfatihbakir@gmail.com>
2020-03-20 09:23:06 -0700

    raspi3 framebuffer double buffering

--- src/drivers/arch/raspi3/include/arch/framebuffer.hpp
+++ src/drivers/arch/raspi3/include/arch/framebuffer.hpp
@@ -10,13 +10,24 @@ class framebuffer : public self_pointing<framebuffer> {
 public:
     explicit framebuffer(gfx::dimensions dims);
 
+    gfx::dimensions virtual_dims() const {
+        return m_virtual;
+    }
+
     gfx::dimensions dims() const {
         return m_physical;
     }
 
-    span<uint8_t> get_buffer() const {
-        return m_buffer;
+    span<uint8_t> get_buffer() {
+        auto len = m_buffer.size() / 2;
+        if (m_swapped) {
+            return m_buffer.slice(0, len);
+        } else {
+            return m_buffer.slice(len, len);
+        }
     }
+
+    void swap_buffers();
 
     void set_pixel(gfx::point pt, bool val) {
         auto pos = (pt.y * m_physical.width + pt.x) * 3;
@@ -46,6 +57,8 @@ private:
     bool m_rgb;
     int32_t m_pitch;
     gfx::dimensions m_physical;
+    gfx::dimensions m_virtual;
     span<uint8_t> m_buffer{nullptr};
+    bool m_swapped = false;
 };
 }
--- src/drivers/arch/raspi3/src/framebuffer.cpp
+++ src/drivers/arch/raspi3/src/framebuffer.cpp
@@ -10,8 +10,8 @@ namespace {
 enum class fb_tags
 {
     allocate_buffer = 0x00040001,
-    screen_size = 0x00048003,
-    virtual_screen_size = 0x00048004,
+    set_physical_screen_size = 0x00048003,
+    set_virtual_screen_size = 0x00048004,
     set_bitdepth = 0x00048005,
     set_pixel_order = 0x00048006,
     get_pitch = 0x00040008,
@@ -22,41 +22,59 @@ enum class fb_tags
 raspi3::framebuffer::framebuffer(gfx::dimensions dims) {
     property_channel props;
 
-    {
-        property_channel_tags_builder builder;
-        auto buf = builder
-                       .add(static_cast<uint32_t>(fb_tags::screen_size),
-                            {dims.width, dims.height})
-                       .add(static_cast<uint32_t>(fb_tags::virtual_screen_size),
-                            {dims.width, dims.height})
-                       .add(static_cast<uint32_t>(fb_tags::set_virtual_offset), {0, 0})
-                       .add(static_cast<uint32_t>(fb_tags::set_bitdepth), {24})
-                       .add(static_cast<uint32_t>(fb_tags::set_pixel_order), {1})
-            .add(static_cast<uint32_t>(fb_tags::allocate_buffer), {4096, 0})
-                       .add(static_cast<uint32_t>(fb_tags::get_pitch), {0})
-                       .end();
+    property_channel_tags_builder builder;
+    auto buf = builder
+                   .add(static_cast<uint32_t>(fb_tags::set_physical_screen_size),
+                        {dims.width, dims.height})
+                   .add(static_cast<uint32_t>(fb_tags::set_virtual_screen_size),
+                        {dims.width, (uint32_t)dims.height * 2})
+                   .add(static_cast<uint32_t>(fb_tags::set_virtual_offset), {0, 0})
+                   .add(static_cast<uint32_t>(fb_tags::set_bitdepth), {24})
+                   .add(static_cast<uint32_t>(fb_tags::set_pixel_order), {1})
+                   .add(static_cast<uint32_t>(fb_tags::allocate_buffer), {4096, 0})
+                   .add(static_cast<uint32_t>(fb_tags::get_pitch), {0})
+                   .end();
 
-        auto res = props.transaction(buf);
-        if (!res) {
-            // noo
-            tos::debug::panic("can't initialize framebuffer");
-        }
+    auto res = props.transaction(buf);
+    if (!res) {
+        // noo
+        tos::debug::panic("can't initialize framebuffer");
+    }
 
-        if (buf[1] == 0) {
-            tos::debug::panic("bad response");
-        }
+    if (buf[1] == 0) {
+        tos::debug::panic("bad response");
+    }
 
-        auto code = buf[1] - 0x80000000;
-        if (code != 0) {
-            tos::debug::panic("error");
-        }
+    auto code = buf[1] - 0x80000000;
+    if (code != 0) {
+        tos::debug::panic("error");
+    }
 
-        m_physical.width = buf[5];
-        m_physical.height = buf[6];
-        m_bit_depth = buf[20];
-        m_rgb = buf[24];
-        m_pitch = buf[33];
-        m_buffer = span<uint8_t>(reinterpret_cast<uint8_t*>(buf[28] & 0x3FFFFFFF), buf[29]);
+    m_physical.width = buf[5];
+    m_physical.height = buf[6];
+    m_virtual.width = buf[10];
+    m_virtual.height = buf[11];
+    m_bit_depth = buf[20];
+    m_rgb = buf[24];
+    m_pitch = buf[33];
+    m_buffer = span<uint8_t>(reinterpret_cast<uint8_t*>(buf[28] & 0x3FFFFFFF), buf[29]);
+}
+
+void framebuffer::swap_buffers() {
+    m_swapped ^= true;
+
+    property_channel props;
+
+    property_channel_tags_builder builder;
+    auto buf = builder
+                   .add(static_cast<uint32_t>(fb_tags::set_virtual_offset),
+                        {0, static_cast<uint32_t>(m_swapped * dims().height)})
+                   .end();
+
+    auto res = props.transaction(buf);
+    if (!res) {
+        // noo
+        tos::debug::panic("can't initialize framebuffer");
     }
 }
 } // namespace tos::raspi3
