11f9d60991815ce7bdbfdc62bd31a38830ea7342
Fatih Bakir <mfatihbakir@gmail.com>
2020-04-13 16:32:18 -0700

    improve mux, allow creating channels over a shell

--- examples/mux/mux.cpp
+++ examples/mux/mux.cpp
@@ -9,7 +9,9 @@
 #include <tos/debug/sinks/clock_adapter.hpp>
 #include <tos/debug/sinks/serial_sink.hpp>
 #include <tos/io/serial_multiplexer.hpp>
+#include <tos/shell/shell.hpp>
 #include <tos/streams.hpp>
+#include <unordered_map>
 
 namespace asio = boost::asio;
 
@@ -20,7 +22,7 @@ public:
     explicit channel_owner(StreamT str)
         : m_str{std::move(str)} {
         m_read_thread = &tos::launch(tos::alloc_stack, [this] { read_run(); });
-        m_listen_thread = &tos::launch(tos::alloc_stack, [this] { listen_run(); });
+        m_listen_thread = &tos::launch(tos::alloc_stack, [this] { listen_unix_run(); });
     }
 
     void cancel() {
@@ -40,7 +42,7 @@ private:
     }
 
     template<class ClientStreamT>
-    void write_run(ClientStreamT& sock) {
+    void unix_write_run(ClientStreamT& sock) {
         auto i = m_str->get_stream_id();
         while (!m_cancel) {
             std::array<uint8_t, 1> buffer;
@@ -60,7 +62,8 @@ private:
         }
     }
 
-    void listen_run() {
+    void listen_unix_run() {
+        ::mkdir("/tmp/tdb", 0777);
         auto i = m_str->get_stream_id();
         using namespace std::string_literals;
         auto path = "/tmp/tdb/channel"s + std::to_string(i);
@@ -69,12 +72,13 @@ private:
         sock.listen();
 
         while (!m_cancel) {
-            LOG_INFO("Accepting for", i);
+            LOG_INFO("Accepting for", i, "at", path);
             auto res = sock.accept();
             LOG_INFO("Accepted for", i);
             tos::lock_guard g{m_socks_lock};
             m_unix_socks.emplace_back(std::move(force_get(res)));
-            tos::launch(tos::alloc_stack, [this] { write_run(*m_unix_socks.back()); });
+            tos::launch(tos::alloc_stack,
+                        [this] { unix_write_run(*m_unix_socks.back()); });
         }
     }
 
@@ -94,33 +98,30 @@ public:
     void operator()() {
         LOG_INFO("tos tdb");
         LOG_INFO("Version 0.1");
-        LOG_INFO("Connecting to device over /dev/ttyACM2");
+        LOG_INFO("Connecting to device over /dev/ttyACM0");
         LOG_INFO("Opening device");
         m_mux =
             std::make_unique<tos::serial_multiplexer<std::unique_ptr<tos::x86::usart>>>(
                 std::make_unique<tos::x86::usart>(
-                    get_io(), "/dev/ttyACM2", tos::uart::default_115200),
-                std::initializer_list<uint16_t>{0});
+                    get_io(), "/dev/ttyACM0", tos::uart::default_115200),
+                std::initializer_list<uint16_t>{0, 1});
         LOG_INFO("Launching channel owner threads");
 
-        for (int i : {0}) {
-            m_channels.emplace_back(*m_mux->get_stream(i));
+        for (int i : {0, 1}) {
+            m_channels.emplace(i, *m_mux->get_stream(i));
         }
+
+        tos::launch(tos::alloc_stack, [this] { repl(tos::x86::stdio{}); });
 
         static tos::semaphore int_sem{0};
 
-        signal(SIGINT, [](int signal) {
-             int_sem.up();
-        });
-
-        signal(SIGTERM, [](int signal) {
-          int_sem.up();
-        });
+        signal(SIGINT, [](int) { int_sem.up(); });
+        signal(SIGTERM, [](int) { int_sem.up(); });
 
         int_sem.down();
         LOG_INFO("Received termination request, cancelling channels");
 
-        for (auto& channel : m_channels) {
+        for (auto& [id, channel] : m_channels) {
             channel.cancel();
         }
 
@@ -130,8 +131,33 @@ public:
     }
 
 private:
-    std::deque<channel_owner<
-        multiplexed_stream<serial_multiplexer<std::unique_ptr<x86::usart>>>>>
+    template<class StreamT>
+    void repl(StreamT str) {
+        shell::shell<decltype(&str), false> sh(&str);
+        auto& cmds = sh.get_command_store();
+
+        auto add_channel = [&](span<std::string_view> args) {
+            auto id = std::stoi(std::string(args[1]));
+            if (id == 0) {
+                // Either the number entered was 0, or the conversion failed. Sigh...
+                tos::println(str, "Usage: add [stream id : (base 10 number)]");
+                return;
+            }
+            if (m_channels.find(id) != m_channels.end()) {
+                LOG_WARN("Given channel", id, "already exists");
+                return;
+            }
+            m_channels.emplace(id, m_mux->create_stream(id));
+            LOG_INFO("Created channel", id);
+        };
+        cmds.add_command("add", function_ref<void(span<std::string_view>)>(add_channel));
+
+        sh.run();
+    }
+
+    std::unordered_map<int,
+                       channel_owner<multiplexed_stream<
+                           serial_multiplexer<std::unique_ptr<x86::usart>>>>>
         m_channels;
     std::unique_ptr<tos::serial_multiplexer<std::unique_ptr<x86::usart>>> m_mux;
 };
@@ -140,7 +166,7 @@ private:
 void tos_main() {
     tos::debug::set_default_log(
         new tos::debug::detail::any_logger(new tos::debug::clock_sink_adapter{
-            tos::debug::serial_sink(tos::x86::stdio{}),
+            tos::debug::serial_sink(tos::x86::stderr_adapter{}),
             tos::x86::clock<std::chrono::system_clock>{}}));
 
     tos::launch(tos::alloc_stack, tos::tdb::multiplexer{});
