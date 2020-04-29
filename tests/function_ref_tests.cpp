71d4162381c8fb22caf56594270ecd5c95c5c435
Fatih Bakir <mfatihbakir@gmail.com>
2020-04-28 21:01:36 -0700

    [util] Function refs support casting between types with reinterpret cast semantics

--- tests/function_ref_tests.cpp
+++ tests/function_ref_tests.cpp
@@ -3,20 +3,20 @@
 //
 
 #include "doctest.h"
+
 #include <tos/function_ref.hpp>
 
-TEST_CASE("simple")
-{
-    tos::function_ref<bool()> fr {[](void*){ return true; }};
+namespace tos {
+namespace {
+TEST_CASE("simple") {
+    tos::function_ref<bool()> fr{[](void*) { return true; }};
     REQUIRE(fr());
 
     fr = tos::function_ref<bool()>([](void*) { return false; });
     REQUIRE(!fr());
 
     bool b = false;
-    auto f = [&]{
-        return b;
-    };
+    auto f = [&] { return b; };
 
     fr = tos::function_ref<bool()>(f);
 
@@ -26,3 +26,20 @@ TEST_CASE("simple")
 
     REQUIRE(fr());
 }
+
+TEST_CASE("unsafe function cast works") {
+    bool b = false;
+    auto f = [&] { return b; };
+
+    auto fr = tos::function_ref<bool()>(f);
+
+    REQUIRE_EQ(false, fr());
+
+    auto unsafe = unsafe_function_ref_cast<function_ref<void()>>(fr);
+
+    auto safe = unsafe_function_ref_cast<function_ref<bool()>>(unsafe);
+
+    REQUIRE_EQ(false, safe());
+}
+} // namespace
+} // namespace tos
