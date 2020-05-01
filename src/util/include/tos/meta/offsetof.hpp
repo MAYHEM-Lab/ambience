7a78b6bc6edd1ac1c68b025091017e5b9525be58
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-01 00:30:04 -0700

    [meta] A function to do an inverse pointer-to-data-member operation. Has undefined behaviour, but is portable across all compilers for now. https://godbolt.org/z/97v-ek

---
+++ src/util/include/tos/meta/offsetof.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <cstdint>
+#include <cstddef>
+#include <type_traits>
+#include <utility>
+
+namespace tos::meta {
+template <class T1, class T2>
+auto class_type(T1 T2::*) -> T2;
+
+template <auto PtrToMem>
+int offset_of() {
+    using class_t = decltype(class_type(PtrToMem));
+    return reinterpret_cast<intptr_t>(&(static_cast<class_t*>(nullptr)->*PtrToMem));
+}
+}
