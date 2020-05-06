c3c1ccb49a178860194f70af6af3ac20027f6d6a
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-05 23:34:17 -0700

    [context] Move default context to a file, has an allocator

--- src/core/src/context.cpp
+++ src/core/src/context.cpp
@@ -1,1 +1,11 @@
+#include <tos/components/allocator.hpp>
 #include <tos/context.hpp>
+#include <tos/memory/malloc.hpp>
+
+namespace tos {
+context& default_context() {
+    static auto erased_alloc = memory::erase_allocator(memory::mallocator{});
+    static static_context<allocator_component> ctx{erased_alloc};
+    return ctx;
+}
+}
