332bc3c2962be0728e7ad7c7b958603c4b1bc63b
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-05 23:37:27 -0700

    [new] operator new/delete use the allocator component of the current context

--- src/util/src/new.cpp
+++ src/util/src/new.cpp
@@ -2,34 +2,56 @@
 // Created by Mehmet Fatih BAKIR on 14/05/2018.
 //
 
-#include <stdlib.h>
 #include <new>
+#include <tos/context.hpp>
+#include <tos/components/allocator.hpp>
+#include <tos/ft.hpp>
+
+using tos::allocator_component;
 
 void operator delete (void* pt, size_t){
-    free(pt);
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        alloc->allocator->free(pt);
+        return;
+    }
 }
 
 void operator delete (void* pt)
 {
-    free(pt);
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        alloc->allocator->free(pt);
+        return;
+    }
 }
 
 void operator delete[] (void* pt)
 {
-    free(pt);
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        alloc->allocator->free(pt);
+        return;
+    }
 }
 
 void operator delete[] (void* pt, size_t)
 {
-    free(pt);
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        alloc->allocator->free(pt);
+        return;
+    }
 }
 
 void* operator new(size_t sz)
 {
-    return malloc(sz);
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        return alloc->allocator->allocate(sz);
+    }
+    return nullptr;
 }
 
 void* operator new[](size_t sz)
 {
-    return malloc(sz);
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        return alloc->allocator->allocate(sz);
+    }
+    return nullptr;
 }
