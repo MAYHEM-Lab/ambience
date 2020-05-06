42bea99b26ca9ce596d4a577373da1e383644b07
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-05 22:15:28 -0700

    [cc32xx] Use the ALWAYS_INLINE macros

--- src/arch/cc32xx/include/tos_arch.hpp
+++ src/arch/cc32xx/include/tos_arch.hpp
@@ -4,12 +4,16 @@
 
 #pragma once
 
+#include <tos/compiler.hpp>
+
 extern "C" {
-inline void __attribute__((always_inline)) tos_set_stack_ptr(char* ptr) {
+ALWAYS_INLINE
+void tos_set_stack_ptr(char* ptr) {
     __asm__ __volatile__("mov sp, %0" : : "r"(ptr) : "memory");
 }
 
-inline void* __attribute__((always_inline)) tos_get_stack_ptr() {
+ALWAYS_INLINE
+void* tos_get_stack_ptr() {
     void* sp;
     __asm__ __volatile__("mov %0, sp" : "=r"(sp) : : "memory");
     return sp;
