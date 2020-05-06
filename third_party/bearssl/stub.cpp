3fc61d2f6ec51c9a8923227c902f0e6acd8baa51
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-05 22:13:45 -0700

    [bearssl] A stub `tos_rand_source`, should obviously not be used.

--- third_party/bearssl/stub.cpp
+++ third_party/bearssl/stub.cpp
@@ -1,4 +1,6 @@
 #include <time.h>
+#include <stdint.h>
+
 extern "C" {
 [[gnu::weak]]
 void tos_this_thread_yield() {}
@@ -6,5 +8,9 @@ void tos_this_thread_yield() {}
 [[gnu::weak]]
 time_t time(time_t*) {
     return {};
+}
+
+uint32_t tos_rand_source(void) {
+    return 4;
 }
 }
