a688cd741ee05051948577e12d780b730845e2c2
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-05 22:15:16 -0700

    [debug] A simple assert macro

--- src/core/include/tos/debug/assert.hpp
+++ src/core/include/tos/debug/assert.hpp
@@ -1,4 +1,14 @@
 #pragma once
 
+#include <tos/debug/panic.hpp>
+
+namespace tos::debug {
+[[noreturn]]
+bool default_assert_handler() {
+    panic("Assertion failed");
+}
+}
+
+#define Assert(x) ((x) || ::tos::debug::default_assert_handler())
 #define Expects(x)
 #define Ensures(x)
