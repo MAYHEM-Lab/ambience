f061ade7c2e27f48d550dfb120a321aab81cbf1a
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-05 23:32:53 -0700

    [context] Allocator component uses polymorphic allocators

--- src/core/include/tos/components/allocator.hpp
+++ src/core/include/tos/components/allocator.hpp
@@ -2,6 +2,13 @@
 
 #include "component.hpp"
 
+#include <tos/memory/polymorphic_allocator.hpp>
+
 namespace tos {
-struct allocator_component : id_component<1> {};
-}
+struct allocator_component : id_component<1> {
+    explicit allocator_component(memory::polymorphic_allocator& alloc)
+        : allocator{&alloc} {
+    }
+    memory::polymorphic_allocator* allocator;
+};
+} // namespace tos
