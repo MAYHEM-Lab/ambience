a6313d2a94d29b7fef4933b47ba27c3c7b57c4d3
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-08 14:33:24 -0700

    [debug] Assert prints the content

--- src/core/include/tos/debug/assert.hpp
+++ src/core/include/tos/debug/assert.hpp
@@ -4,11 +4,11 @@
 
 namespace tos::debug {
 [[noreturn]]
-inline bool default_assert_handler() {
-    panic("Assertion failed");
+inline bool default_assert_handler(const char* msg) {
+    panic(msg);
 }
 }
 
-#define Assert(x) ((x) || ::tos::debug::default_assert_handler())
+#define Assert(x) ((static_cast<bool>(x)) || ::tos::debug::default_assert_handler("Assertion failed " #x))
 #define Expects(x)
 #define Ensures(x)
