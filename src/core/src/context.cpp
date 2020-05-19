9243bccc82d5a1b346a08be9cf56c9a3d72dd1bd
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-19 15:13:14 -0700

    [raspi] Use the free list allocator for raspi

--- src/core/src/context.cpp
+++ src/core/src/context.cpp
@@ -2,10 +2,21 @@
 #include <tos/context.hpp>
 #include <tos/memory/malloc.hpp>
 
+#if defined(TOS_PLATFORM_raspi)
+#include <tos/memory/free_list.hpp>
+namespace {
+alignas(16) std::array<uint8_t, 16 * 1024 * 1024> heap_mem;
+}
+#endif
+
 namespace tos {
 context& default_context() {
+#if defined(TOS_PLATFORM_raspi)
+    static auto erased_alloc = memory::erase_allocator(memory::free_list{heap_mem});
+#else
     static auto erased_alloc = memory::erase_allocator(memory::mallocator{});
+#endif
     static static_context<allocator_component> ctx{erased_alloc};
     return ctx;
 }
-}
+} // namespace tos
