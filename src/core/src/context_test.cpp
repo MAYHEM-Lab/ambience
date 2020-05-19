36fbac876b35aed12f165bfd7aaad53d203b7626
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-19 00:56:39 -0700

    [context] Fix tests

--- src/core/src/context_test.cpp
+++ src/core/src/context_test.cpp
@@ -1,19 +1,22 @@
 #include <doctest.h>
-#include <tos/context.hpp>
-#include <tos/ft.hpp>
 #include <tos/components/allocator.hpp>
 #include <tos/components/name.hpp>
+#include <tos/context.hpp>
+#include <tos/ft.hpp>
+#include <tos/memory/malloc.hpp>
 
 namespace tos {
 namespace {
 TEST_CASE("Static context works with a single component") {
-    static_context<allocator_component> ctx;
+    auto erased = tos::memory::erase_allocator(memory::mallocator{});
+    static_context<allocator_component> ctx(erased);
     auto ptr = ctx.get_component<allocator_component>();
     REQUIRE_NE(nullptr, ptr);
 }
 
 TEST_CASE("Static context works with multiple components") {
-    static_context<allocator_component, name_component> ctx;
+    auto erased = tos::memory::erase_allocator(memory::mallocator{});
+    static_context<allocator_component, name_component> ctx(erased, name_component{});
     auto ptr = ctx.get_component<allocator_component>();
     REQUIRE_NE(nullptr, ptr);
 
@@ -27,10 +30,10 @@ TEST_CASE("Thread context setting works") {
     REQUIRE_NE(nullptr, name_ptr);
     name_ptr->name = "Test Context";
     // By default, each thread belongs to the default context, which does not have a name
-    REQUIRE_EQ(nullptr, current_context()->get_component<name_component>());
-    self()->m_context = &ctx;
-    REQUIRE_EQ("Test Context", current_context()->get_component<name_component>()->name);
-    self()->m_context = nullptr;
+    REQUIRE_EQ(nullptr, current_context().get_component<name_component>());
+    self()->set_context(ctx);
+    REQUIRE_EQ("Test Context", current_context().get_component<name_component>()->name);
+    self()->set_context(default_context());
 }
 } // namespace
 } // namespace tos
