b65e33c127525f4df53fda4b559adeb6fd36d5ed
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-20 15:18:02 -0700

    [gfx] Opensans Regular is available

---
+++ libs/gfx/include/tos/gfx/fonts/opensans_regular.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <tos/span.hpp>
+#include <cstdint>
+
+namespace tos::gfx::fonts {
+extern span<const uint8_t> opensans_regular;
+}
--- libs/gfx/src/truetype_test.cpp
+++ libs/gfx/src/truetype_test.cpp
@@ -5,19 +5,19 @@
 #include <tos/gfx/truetype.hpp>
 #include <tos/debug/log.hpp>
 #include <tos/gfx/bitmap_io.hpp>
+#include <tos/gfx/fonts/opensans_regular.hpp>
 
-extern tos::span<const uint8_t> font_data;
 namespace tos::gfx {
 namespace {
 TEST_CASE("Truetype font loading works") {
-    auto font = font::make(font_data);
+    auto font = font::make(tos::gfx::fonts::opensans_regular);
     REQUIRE(font.valid());
     REQUIRE_EQ(24, font.text_dimensions("hello", 16).width);
     REQUIRE_EQ(13, font.text_dimensions("hello", 16).height);
 }
 
 TEST_CASE("Truetype text rendering works") {
-    auto font = font::make(font_data);
+    auto font = font::make(tos::gfx::fonts::opensans_regular);
 
     mono8 bitmap[128 * 24] = {};
     basic_bitmap_view<mono8> view(bitmap, 128, {128, 24});
