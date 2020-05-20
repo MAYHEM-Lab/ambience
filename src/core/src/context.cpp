89f92fe21b665fe19d53c072cca9325e27a9099b
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-19 19:00:58 -0700

    [context] Use forgotten variables for statics so it works without atexit

--- src/core/src/context.cpp
+++ src/core/src/context.cpp
@@ -1,11 +1,13 @@
 #include <tos/components/allocator.hpp>
 #include <tos/context.hpp>
 #include <tos/memory/malloc.hpp>
+#include <tos/utility.hpp>
+#include <type_traits>
 
 namespace tos {
 context& default_context() {
-    static auto erased_alloc = memory::erase_allocator(memory::mallocator{});
-    static static_context<allocator_component> ctx{erased_alloc};
-    return ctx;
+    static auto erased_alloc = forget(memory::erase_allocator(memory::mallocator{}));
+    static auto ctx = forget<static_context<allocator_component>>(erased_alloc.get());
+    return ctx.get();
 }
-}
+} // namespace tos
