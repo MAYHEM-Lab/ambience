6c44a738290859a1b405d4e12c7338cab8b898eb
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-28 16:31:57 -0700

    [aarch64] dsb and isb

--- src/arch/aarch64/include/arch/assembly.hpp
+++ src/arch/aarch64/include/arch/assembly.hpp
@@ -1,10 +1,25 @@
 #pragma once
 
+#include <tos/barrier.hpp>
+
 namespace tos::aarch64 {
-void nop() {
+inline void nop() {
     asm volatile("nop");
 }
-void breakpoint() {
+
+inline void breakpoint() {
     asm volatile("brk #0");
+}
+
+inline void isb() {
+    tos::detail::memory_barrier();
+    asm volatile("isb");
+    tos::detail::memory_barrier();
+}
+
+inline void dsb() {
+    tos::detail::memory_barrier();
+    asm volatile("dsb");
+    tos::detail::memory_barrier();
 }
 } // namespace tos::aarch64
