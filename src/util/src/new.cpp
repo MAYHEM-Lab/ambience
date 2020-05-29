74ea92d3053f9c99ca521f99c51b0ca8a7df4735
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-29 15:49:36 -0700

    [util] nothrow operator news

--- src/util/src/new.cpp
+++ src/util/src/new.cpp
@@ -3,53 +3,62 @@
 //
 
 #include <new>
-#include <tos/context.hpp>
 #include <tos/components/allocator.hpp>
+#include <tos/context.hpp>
 #include <tos/ft.hpp>
 
 using tos::allocator_component;
 
-void operator delete (void* pt, size_t){
+void operator delete(void* pt, size_t) {
     if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
         alloc->allocator->free(pt);
         return;
     }
 }
 
-void operator delete (void* pt)
-{
+void operator delete(void* pt) {
     if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
         alloc->allocator->free(pt);
         return;
     }
 }
 
-void operator delete[] (void* pt)
-{
+void operator delete[](void* pt) {
     if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
         alloc->allocator->free(pt);
         return;
     }
 }
 
-void operator delete[] (void* pt, size_t)
-{
+void operator delete[](void* pt, size_t) {
     if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
         alloc->allocator->free(pt);
         return;
     }
 }
 
-void* operator new(size_t sz)
-{
+void* operator new(size_t sz) {
     if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
         return alloc->allocator->allocate(sz);
     }
     return nullptr;
 }
 
-void* operator new[](size_t sz)
-{
+void* operator new[](size_t sz) {
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        return alloc->allocator->allocate(sz);
+    }
+    return nullptr;
+}
+
+void* operator new(size_t sz, const std::nothrow_t&) {
+    if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
+        return alloc->allocator->allocate(sz);
+    }
+    return nullptr;
+}
+
+void* operator new[](size_t sz, const std::nothrow_t&) {
     if (auto alloc = tos::current_context().get_component<allocator_component>(); alloc) {
         return alloc->allocator->allocate(sz);
     }
