4d6cbca3c31428f9aac94b3810f6374582820b97
Fatih Bakir <mfatihbakir@gmail.com>
2020-05-29 15:48:44 -0700

    [context] Fix warnings

--- src/core/include/tos/context.hpp
+++ src/core/include/tos/context.hpp
@@ -15,8 +15,8 @@ struct context : list_node<context> {
 
     virtual component* get_component_with_id(component_id_t) = 0;
 
-    virtual void switch_out(context& to) {}
-    virtual void switch_in(context& from) {}
+    virtual void switch_out(context&) {}
+    virtual void switch_in(context&) {}
 };
 
 template<class... Components>
