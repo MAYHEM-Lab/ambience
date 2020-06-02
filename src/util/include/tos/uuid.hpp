18a3bfb1ed031f90987b9b5a351d8c4514560ef7
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-02 00:42:48 -0700

    [util] UUID from string order was wrong, move it to a static function

--- src/util/include/tos/uuid.hpp
+++ src/util/include/tos/uuid.hpp
@@ -12,9 +12,7 @@
 #include <utility>
 
 namespace tos {
-struct uuid {
-    uint8_t id_[16];
-};
+namespace detail {
 
 constexpr uint8_t nibble_to_int(char nibble) {
     switch (nibble) {
@@ -65,29 +63,36 @@ constexpr uint8_t byte_from_hex(tos::span<const char> nibbles) {
     auto right_nibble = nibble_to_int(nibbles[1]);
     return (left_nibble << 4) | right_nibble;
 }
+} // namespace detail
 
-constexpr uuid uuid_from_canonical_string(tos::span<const char> canonical) {
-    // Canonical string format:
-    // 00112233-4455-6677-8899-aabbccddeeff
-    uuid res{};
-    res.id_[0] = byte_from_hex(canonical.slice(0, 2));
-    res.id_[1] = byte_from_hex(canonical.slice(2, 2));
-    res.id_[2] = byte_from_hex(canonical.slice(4, 2));
-    res.id_[3] = byte_from_hex(canonical.slice(6, 2));
-    res.id_[4] = byte_from_hex(canonical.slice(9, 2));
-    res.id_[5] = byte_from_hex(canonical.slice(11, 2));
-    res.id_[6] = byte_from_hex(canonical.slice(14, 2));
-    res.id_[7] = byte_from_hex(canonical.slice(16, 2));
-    res.id_[8] = byte_from_hex(canonical.slice(19, 2));
-    res.id_[9] = byte_from_hex(canonical.slice(21, 2));
-    res.id_[10] = byte_from_hex(canonical.slice(24, 2));
-    res.id_[11] = byte_from_hex(canonical.slice(26, 2));
-    res.id_[12] = byte_from_hex(canonical.slice(28, 2));
-    res.id_[13] = byte_from_hex(canonical.slice(30, 2));
-    res.id_[14] = byte_from_hex(canonical.slice(32, 2));
-    res.id_[15] = byte_from_hex(canonical.slice(34, 2));
-    return res;
-}
+struct uuid {
+    uint8_t id_[16];
+
+    static constexpr uuid from_canonical_string(tos::span<const char> canonical) {
+        using detail::byte_from_hex;
+        // Canonical string format:
+        // 00112233-4455-6677-8899-aabbccddeeff
+        uuid res{};
+        res.id_[15] = byte_from_hex(canonical.slice(0, 2));
+        res.id_[14] = byte_from_hex(canonical.slice(2, 2));
+        res.id_[13] = byte_from_hex(canonical.slice(4, 2));
+        res.id_[12] = byte_from_hex(canonical.slice(6, 2));
+        res.id_[11] = byte_from_hex(canonical.slice(9, 2));
+        res.id_[10] = byte_from_hex(canonical.slice(11, 2));
+        res.id_[9] = byte_from_hex(canonical.slice(14, 2));
+        res.id_[8] = byte_from_hex(canonical.slice(16, 2));
+        res.id_[7] = byte_from_hex(canonical.slice(19, 2));
+        res.id_[6] = byte_from_hex(canonical.slice(21, 2));
+        res.id_[5] = byte_from_hex(canonical.slice(24, 2));
+        res.id_[4] = byte_from_hex(canonical.slice(26, 2));
+        res.id_[3] = byte_from_hex(canonical.slice(28, 2));
+        res.id_[2] = byte_from_hex(canonical.slice(30, 2));
+        res.id_[1] = byte_from_hex(canonical.slice(32, 2));
+        res.id_[0] = byte_from_hex(canonical.slice(34, 2));
+
+        return res;
+    }
+};
 
 inline bool operator==(const uuid& a, const uuid& b) {
     return std::equal(std::begin(a.id_), std::end(a.id_), std::begin(b.id_));
