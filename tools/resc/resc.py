ac180eb55bc83f2b991936fd80b9fb0cf28bfeba
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-15 18:55:26 -0700

    [resc] Respect names in the file

--- tools/resc/resc.py
+++ tools/resc/resc.py
@@ -6,91 +6,97 @@ import typing
 import os
 from jinja2 import Template
 from typing import NamedTuple
+import operator, functools
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 
-class Fragment:
-    body: str
-
-
-class Fragments(NamedTuple):
-    header: str
-    source: str
+class Fragment(NamedTuple):
+    # Type of the resource.
+    type: str
+    # Body of the definition.
+    def_body: str
+    # Declaration bodies do not contain the declaration of the getter, but rather contain additional declarations
+    # if needed.
+    extra_decl: str = ""
+    # Required non-standard headers
+    headers: typing.List[str] = []
 
 
 class ResourceCompiler:
-    def compile(self, node: object):
+    def compile(self, node: object) -> Fragment:
         raise NotImplementedError()
 
 
 class StringCompiler(ResourceCompiler):
-    header_template: Template
     source_template: Template
 
     def __init__(self):
-        with open(os.path.join(script_dir, "templates/string.hpp.in"), "r") as tmpl:
-            self.header_template = Template(tmpl.read())
         with open(os.path.join(script_dir, "templates/string.cpp.jinja"), "r") as tmpl:
             self.source_template = Template(tmpl.read())
         pass
 
-    def compile(self, node):
-        header_frag = self.header_template.render({
-            "obj_name": "str",
-        })
-
+    def compile(self, node) -> Fragment:
         source_frag = self.source_template.render({
-            "obj_name": "str",
             "string_val": node,
-            "class_name": "res"
         })
 
-        return Fragments(header_frag, source_frag)
+        return Fragment("std::string_view", source_frag, headers=["string_view"])
 
 
 class BlobCompiler(ResourceCompiler):
-    header_template: Template
     source_template: Template
 
     def __init__(self):
-        with open(os.path.join(script_dir, "templates/blob.hpp.jinja"), "r") as tmpl:
-            self.header_template = Template(tmpl.read())
         with open(os.path.join(script_dir, "templates/blob.cpp.jinja"), "r") as tmpl:
             self.source_template = Template(tmpl.read())
         pass
 
-    def compile(self, node):
+    def compile(self, node) -> Fragment:
         if "from_file" not in node:
             raise ValueError("Blobs must come from a file!")
-
-        initializer = ""
 
         with open(node["from_file"], "rb") as file:
             initializer = ",".join([str(b) for b in file.read()])
 
-        header_frag = self.header_template.render({
-            "obj_name": "blob",
-        })
+            source_frag = self.source_template.render({
+                "initializer": initializer,
+            })
 
-        source_frag = self.source_template.render({
-            "obj_name": "blob",
-            "initializer": initializer,
-            "class_name": "res"
-        })
+            return Fragment("tos::span<const uint8_t>", source_frag, headers=["tos/span.hpp"])
 
-        return Fragments(header_frag, source_frag)
+
+class KVCompiler(ResourceCompiler):
+    def compile(self, node: dict) -> Fragment:
+        for key, val in node.items():
+            print("{}:{}".format(key, val))
+        return super().compile(node)
 
 
 def get_compilers() -> typing.Dict[str, ResourceCompiler]:
     return {
         'string': StringCompiler(),
-        'blob': BlobCompiler()
+        'blob': BlobCompiler(),
+        'kv': KVCompiler()
     }
 
 
-def merge(frags: [Fragments]) -> Fragments:
-    return Fragments("\n".join([frag.header for frag in frags]), "\n".join([frag.source for frag in frags]))
+def make_declaration(name: str, frag: Fragment) -> str:
+    template = Template("static auto {{name}}() const -> {{type}};")
+    return template.render({
+        'name': name,
+        'type': frag.type
+    })
+
+
+def make_definition(class_name: str, name: str, frag: Fragment) -> str:
+    template = Template("""auto {{class_name}}::{{name}}() const -> {{type}} {\n{{body}}\n}""")
+    return template.render({
+        'class_name': class_name,
+        'name': name,
+        'type': frag.type,
+        'body': frag.def_body
+    })
 
 
 class Compiler:
@@ -105,27 +111,29 @@ class Compiler:
         with open(os.path.join(script_dir, "templates/resources.cpp.in"), "r") as tmpl:
             self.source_template = Template(tmpl.read())
 
-    def compile(self, file):
+    def compile(self, resources, namespace, class_name):
         allfrags = []
-        for elem in file["resources"]:
+        for elem in resources:
             if elem["type"] not in self.compilers:
                 print("Unknown resource type: {}".format(elem["type"]))
                 continue
             frags = self.compilers[elem["type"]].compile(elem["value"])
-            allfrags.append(frags)
+            allfrags.append((elem["name"], frags))
 
-        merged = merge(allfrags)
         header = self.header_template.render({
-            "declarations": merged.header,
-            "namespace": "tos",
-            "class_name": "res"
+            "declarations": "\n".join([make_declaration(name, frag) for name, frag in allfrags]),
+            "namespace": namespace,
+            "class_name": class_name,
+            "all_headers": functools.reduce(operator.add, [frag.headers for _, frag in allfrags], [])
         })
+
         source = self.source_template.render({
-            "definitions": merged.source,
-            "namespace": "tos",
-            "class_name": "res",
+            "definitions": "\n".join([make_definition("class_name", name, frag) for name, frag in allfrags]),
+            "namespace": namespace,
+            "class_name": class_name,
             "header_path": "res_generated.hpp"
         })
+
         print(header)
         print(source)
 
@@ -134,4 +142,4 @@ if __name__ == "__main__":
     comp = Compiler()
     with open(sys.argv[1], "r") as f:
         file = yaml.safe_load(f)
-        comp.compile(file)
+        comp.compile(file["resources"], namespace="tos", class_name="resources")
