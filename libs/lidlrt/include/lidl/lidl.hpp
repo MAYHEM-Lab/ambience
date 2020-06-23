2fbf0f9423c7429ca8d4dfc7e4ac94a521fbedff
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-22 19:21:53 -0700

    [lidl] Sync lidlrt

--- libs/lidlrt/include/lidl/lidl.hpp
+++ libs/lidlrt/include/lidl/lidl.hpp
@@ -8,3 +8,17 @@
 #include "structure.hpp"
 #include "union.hpp"
 #include "vector.hpp"
+
+#ifndef LIDL_ASSERT
+#ifdef TOS
+#include <tos/debug/assert.hpp>
+#define LIDL_ASSERT Assert
+#else
+#include <cassert>
+#define LIDL_ASSERT assert
+#endif
+#endif
+
+#ifndef LIDL_UNION_ASSERT
+#define LIDL_UNION_ASSERT LIDL_ASSERT
+#endif
