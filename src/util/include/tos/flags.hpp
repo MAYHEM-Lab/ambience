321bcd034e96aa92fc606715eebd2f9fb151691d
Fatih Bakir <mfatihbakir@gmail.com>
2020-06-30 21:53:08 -0700

    [util] flag functions are constexpr

--- src/util/include/tos/flags.hpp
+++ src/util/include/tos/flags.hpp
@@ -4,14 +4,14 @@
 
 namespace tos::util {
 template<class EnumT>
-bool is_flag_set(EnumT all, EnumT look_for) {
+constexpr bool is_flag_set(EnumT all, EnumT look_for) {
     return (std::underlying_type_t<EnumT>(all) &
             std::underlying_type_t<EnumT>(look_for)) ==
            std::underlying_type_t<EnumT>(look_for);
 }
 
 template<class EnumT>
-EnumT set_flag(EnumT all, EnumT to_set) {
+constexpr EnumT set_flag(EnumT all, EnumT to_set) {
     return EnumT(std::underlying_type_t<EnumT>(all) |
                  std::underlying_type_t<EnumT>(to_set));
 }
