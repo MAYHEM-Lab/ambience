b533ee691c9f0be3c3904b11ac04db32a5ac3709
Fatih Bakir <mfatihbakir@gmail.com>
2020-07-01 23:24:36 -0700

    [gui] Box uses rgb color

--- libs/gui/src/elements/box.cpp
+++ libs/gui/src/elements/box.cpp
@@ -1,10 +1,9 @@
 #include <tos/gui/elements/box.hpp>
 
 namespace tos::gui::elements {
-
 void box::draw(services::painter& painter, const gfx2::rectangle& at) {
-    tos::services::style fill_style(tos::gfx2::binary_color{true});
+    tos::services::style fill_style(tos::gfx2::rgb8{255, 255, 255});
     painter.set_style(fill_style);
     painter.draw_rect(at, 0, true);
 }
-}
+} // namespace tos::gui::elements
