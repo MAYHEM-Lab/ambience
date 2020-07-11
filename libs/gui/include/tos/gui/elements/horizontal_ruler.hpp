aff26fcacd71b413228e9251fce7f881308656cc
goksuguvendiren <goksuguvendiren@gmail.com>
2020-07-10 17:20:42 -0700

    [gui] Implemented a vertical ruler to complement the horizontal ruler.

--- libs/gui/include/tos/gui/elements/horizontal_ruler.hpp
+++ libs/gui/include/tos/gui/elements/horizontal_ruler.hpp
@@ -6,4 +6,8 @@ namespace tos::gui::elements {
 struct horizontal_ruler : public element {
     void draw(tos::services::painter& painter, const tos::gfx2::rectangle& at);
 };
+struct vertical_ruler : public element {
+    void draw(tos::services::painter& painter, const tos::gfx2::rectangle& at);
+};
+
 } // namespace tos::gui::elements
--- libs/gui/src/elements/horizontal_ruler.cpp
+++ libs/gui/src/elements/horizontal_ruler.cpp
@@ -7,4 +7,12 @@ void horizontal_ruler::draw(services::painter& painter, const gfx2::rectangle& a
     painter.draw_line(
         {at.corner(), {at.corner().x() + at.dims().width(), at.corner().y()}});
 }
+
+void vertical_ruler::draw(tos::services::painter& painter,
+                          const tos::gfx2::rectangle& at) {
+    tos::services::style fill_style(tos::gfx2::binary_color{false});
+    painter.set_style(fill_style);
+    painter.draw_line(
+        {at.corner(), {at.corner().x(), at.corner().y() + at.dims().height()}});
+}
 }
