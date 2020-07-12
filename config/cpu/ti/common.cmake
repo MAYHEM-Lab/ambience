75664b63f34f5c680f8f5c07d1aa04649b1bbef9
Fatih Bakir <mfatihbakir@gmail.com>
2020-07-11 20:34:13 -0700

    [cc32xx] Remove duplication.

---
+++ config/cpu/ti/common.cmake
@@ -0,0 +1,10 @@
+set(TOS_ARCH arm)
+set(TOS_CPU_ARCH cortex-m4)
+set(COMPILE_FLAGS "-mfloat-abi=soft")
+
+set(TOS_PLATFORM cc32xx)
+set(TOS_DRIVERS cc32xx)
+
+set(TOS_DEF_STACK 1024)
+set(TOS_TOOLCHAIN TIToolchain)
+set(TI_TARGET CC32XX)
