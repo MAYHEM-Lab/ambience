07953888a40058dbc646392f0c3603403377d09d
Fatih Bakir <mfatihbakir@gmail.com>
2020-07-11 20:34:57 -0700

    [x86_hosted] Format impl.

--- src/platform/x86_hosted/x86_impl.cpp
+++ src/platform/x86_hosted/x86_impl.cpp
@@ -2,34 +2,31 @@
 // Created by fatih on 3/20/18.
 //
 
-#include <string.h>
-#include <stdlib.h>
-#include <iostream>
-#include <tos/interrupt.hpp>
-#include <tos/ft.hpp>
 #include <boost/asio.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <tos/ft.hpp>
+#include <tos/interrupt.hpp>
 
 namespace asio = boost::asio;
 
 void tos_main();
 
-namespace boost
-{
-    void throw_exception(std::exception const& e){
-        std::cerr << "exception: " << e.what() << '\n';
-        std::abort();
-    }
+namespace boost {
+void throw_exception(std::exception const& e) {
+    std::cerr << "exception: " << e.what() << '\n';
+    std::abort();
 }
+} // namespace boost
 
 static asio::io_service* g_io;
-asio::io_service& get_io()
-{
+asio::io_service& get_io() {
     return *g_io;
 }
 
-extern "C"
-int main()
-{
+extern "C" {
+USED int main() {
     asio::io_service io;
     g_io = &io;
 
@@ -39,20 +36,18 @@ int main()
     tos::global::sched.schedule();
     io.run_one();
 
-    while (true)
-    {
+    while (true) {
         auto res = tos::global::sched.schedule();
-        if (io.stopped())
-        {
+        if (io.stopped()) {
             io.reset();
         }
         io.poll();
-        if (res == tos::exit_reason::restart)
-        {
+        if (res == tos::exit_reason::restart) {
             break;
         }
     }
 
     io.run();
     return 0;
+}
 }
