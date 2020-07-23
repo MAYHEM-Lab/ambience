855cae5b9ceede5f4e1a4f60460775144c9d1dfc
Fatih Bakir <mfatihbakir@gmail.com>
2020-07-22 17:57:53 -0700

    [gfx2] Format bit_painter.

--- libs/gfx2/src/bit_painter.cpp
+++ libs/gfx2/src/bit_painter.cpp
@@ -23,12 +23,14 @@ bit_painter::draw_rect(const rectangle& rect, const int8_t& radius, const bool& 
                 this->fill();
                 return 0;
             }
+
             for (int16_t row = rect.corner().y(); row <= other_corner(rect).y(); ++row) {
                 for (int16_t col = rect.corner().x(); col <= other_corner(rect).x();
                      ++col) {
                     draw_point(tos::gfx2::point{col, row});
                 }
             }
+
             return 0;
         }
 
@@ -148,14 +150,12 @@ int8_t bit_painter::set_style(const services::style& s) {
 bool bit_painter::set_orientation(const tos::services::rotation& orientation) {
     m_rotation = orientation;
 
-    if (m_rotation == services::rotation::horizontal)
-    {
+    if (m_rotation == services::rotation::horizontal) {
         m_virtual_dims.width() = m_physical_dims.height();
         m_virtual_dims.height() = m_physical_dims.width();
         return true;
     }
-    if (m_rotation == services::rotation::vertical)
-    {
+    if (m_rotation == services::rotation::vertical) {
         m_virtual_dims = m_physical_dims;
         return true;
     }
@@ -169,27 +169,22 @@ bool bit_painter::draw_bitmap(const tos::gfx2::colors& color_type,
                               const tos::gfx2::rectangle& screen_rect) {
     // assumes the color type is binary for now.
 
-//     type conversion done here
-    auto get_pixel = [&](int row, int col)
-    {
-        switch (color_type)
-        {
-        case tos::gfx2::colors::mono8:
-        {
+    //     type conversion done here
+    auto get_pixel = [&](int row, int col) {
+        switch (color_type) {
+        case tos::gfx2::colors::mono8: {
             auto base = reinterpret_cast<const tos::gfx2::mono8*>(buffer.data());
             auto color = base[row * stride + col];
 
             return color_convert<binary_color>(color);
         }
-        case tos::gfx2::colors::rgb8:
-        {
+        case tos::gfx2::colors::rgb8: {
             auto base = reinterpret_cast<const tos::gfx2::rgb8*>(buffer.data());
             auto color = base[row * stride + col];
 
             return color_convert<binary_color>(color);
         }
-        case tos::gfx2::colors::binary_color:
-        {
+        case tos::gfx2::colors::binary_color: {
             auto base = reinterpret_cast<const tos::gfx2::binary_color*>(buffer.data());
             auto color = base[row * stride + col];
 
@@ -199,13 +194,13 @@ bool bit_painter::draw_bitmap(const tos::gfx2::colors& color_type,
         TOS_UNREACHABLE();
     };
 
-    auto vert_scale = (float)image_rect.dims().height() / (float)screen_rect.dims().height();
-    auto horz_scale = (float)image_rect.dims().width() / (float)screen_rect.dims().width();
+    auto vert_scale =
+        (float)image_rect.dims().height() / (float)screen_rect.dims().height();
+    auto horz_scale =
+        (float)image_rect.dims().width() / (float)screen_rect.dims().width();
 
-    for (int i = 0; i < screen_rect.dims().height(); ++i)
-    {
-        for (int j = 0; j < screen_rect.dims().width(); ++j)
-        {
+    for (int i = 0; i < screen_rect.dims().height(); ++i) {
+        for (int j = 0; j < screen_rect.dims().width(); ++j) {
             auto image_color = get_pixel(i * vert_scale, j * horz_scale);
 
             auto dest_point = translate_point(tos::gfx2::point(
