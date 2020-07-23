1e9f1d032ad1edb41fa255ea93845915923d0b15
Fatih Bakir <mfatihbakir@gmail.com>
2020-07-22 20:57:16 -0700

    [util] Make add_saturating inline.

--- src/util/include/tos/math/saturated.hpp
+++ src/util/include/tos/math/saturated.hpp
@@ -5,6 +5,7 @@
 #include <algorithm>
 
 namespace tos::math {
+inline
 int16_t add_saturating(int16_t a, int16_t b) {
     return std::clamp<int32_t>(
         a + b, std::numeric_limits<int16_t>::min(), std::numeric_limits<int16_t>::max());
