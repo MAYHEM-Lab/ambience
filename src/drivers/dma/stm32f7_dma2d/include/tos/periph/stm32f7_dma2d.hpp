7d1f3f52da1cb33cc46760817d00c04c83ca86f9
Fatih Bakir <mfatihbakir@gmail.com>
2020-10-23 21:50:37 -0700

    [stm32f7_dma2d] Interrupt driven

--- src/drivers/dma/stm32f7_dma2d/include/tos/periph/stm32f7_dma2d.hpp
+++ src/drivers/dma/stm32f7_dma2d/include/tos/periph/stm32f7_dma2d.hpp
@@ -1,18 +1,26 @@
 #pragma once
 
+#include <common/driver_base.hpp>
 #include <cstdint>
 #include <stm32f7xx_hal_conf.h>
 #include <stm32f7xx_hal_dma2d.h>
+#include <tos/semaphore.hpp>
 
 namespace tos::stm32::f7 {
-class dma2d {
+class dma2d : public tracked_driver<dma2d, 1> {
 public:
     dma2d();
 
     void fill_rect(void* data, uint32_t color, int width, int height);
 
-private:
+    void irq();
 
+    auto native_handle() {
+        return &m_dma;
+    }
+
+private:
+    semaphore m_sem{0};
     DMA2D_HandleTypeDef m_dma{};
 };
-}
+} // namespace tos::stm32::f7
--- src/drivers/dma/stm32f7_dma2d/stm32f7_dma2d.cpp
+++ src/drivers/dma/stm32f7_dma2d/stm32f7_dma2d.cpp
@@ -1,10 +1,18 @@
-#include <tos/debug/panic.hpp>
-#include <tos/periph/stm32f7_dma2d.hpp>
 #include <stm32_hal/rcc.hpp>
 #include <stm32f7xx_ll_dma2d.h>
+#include <tos/debug/panic.hpp>
+#include <tos/periph/stm32f7_dma2d.hpp>
+
+extern "C" {
+void DMA2D_IRQHandler() {
+    auto driver = tos::stm32::f7::dma2d::get(0);
+    HAL_DMA2D_IRQHandler(driver->native_handle());
+}
+}
 
 namespace tos::stm32::f7 {
-dma2d::dma2d() {
+dma2d::dma2d()
+    : tracked_driver(0) {
     m_dma.Instance = DMA2D;
 
     m_dma.Init.Mode = DMA2D_R2M;
@@ -17,10 +25,22 @@ dma2d::dma2d() {
     if (res != HAL_OK) {
         debug::panic("Couldn't init dma2d");
     }
+
+    HAL_NVIC_EnableIRQ(DMA2D_IRQn);
+    HAL_NVIC_SetPriority(DMA2D_IRQn, 0, 0);
+    m_dma.XferCpltCallback = [](auto handle) {
+        auto driver = tos::stm32::f7::dma2d::get(0);
+        driver->irq();
+    };
+}
+
+void dma2d::irq() {
+    m_sem.up_isr();
 }
 
 void dma2d::fill_rect(void* data, uint32_t color, int width, int height) {
-    HAL_DMA2D_Start(&m_dma, color, reinterpret_cast<uintptr_t>(data), width, height);
-    HAL_DMA2D_PollForTransfer(&m_dma, 100);
+    HAL_DMA2D_Start_IT(&m_dma, color, reinterpret_cast<uintptr_t>(data), width, height);
+    m_sem.down();
+    //    HAL_DMA2D_PollForTransfer(&m_dma, 100);
 }
-}
+} // namespace tos::stm32::f7
