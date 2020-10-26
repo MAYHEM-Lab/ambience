0714b5f2d7aabe24edfd65f3a23b82b254dbfc11
Fatih Bakir <mfatihbakir@gmail.com>
2020-10-26 10:15:41 -0700

    STM32F7 Demo update

--- examples/stm32f7_disco/main.cpp
+++ examples/stm32f7_disco/main.cpp
@@ -2,26 +2,20 @@
 // Created by fatih on 10/25/18.
 //
 
+#include "arch/gpio.hpp"
 #include <arch/drivers.hpp>
 #include <numeric>
+#include <tos/board.hpp>
 #include <tos/fixed_fifo.hpp>
 #include <tos/ft.hpp>
 #include <tos/mem_stream.hpp>
+#include <tos/periph/stm32f7_dma2d.hpp>
 #include <tos/periph/stm32f7_ltdc.hpp>
 #include <tos/periph/stm32f7_sdram.hpp>
-#include <tos/periph/stm32f7_dma2d.hpp>
 #include <tos/print.hpp>
 #include <tos/semaphore.hpp>
 
-namespace boards {
-struct stm32f7_disco {
-    static constexpr int led_pin = ('i' - 'a') * 16 + 1;
-    static constexpr int tx_pin = 9;
-    static constexpr int rx_pin = 23;
-};
-} // namespace boards
-
-constexpr auto board = boards::stm32f7_disco{};
+using bs = tos::bsp::board_spec;
 
 #define SDRAM_BANK_ADDR ((uint32_t)0xC0000000)
 
@@ -33,10 +27,6 @@ void blink_task() {
     using namespace tos;
     using namespace tos_literals;
 
-    auto led_pin = operator""_pin(board.led_pin);
-    auto usart_rx_pin = operator""_pin(board.rx_pin);
-    auto usart_tx_pin = operator""_pin(board.tx_pin);
-
     auto g = tos::open(tos::devs::gpio);
 
     auto ram_span = sdram_span();
@@ -45,19 +35,8 @@ void blink_task() {
     auto timer = open(devs::timer<2>);
     tos::alarm alarm(&timer);
 
-    auto usart =
-        open(devs::usart<1>, tos::uart::default_115200, usart_rx_pin, usart_tx_pin);
+    auto usart = bs::default_com::open();
     tos::println(usart, "Hello From Tos!");
-
-//    for (auto& i : sdram_span()) {
-//        tos::println(usart, int(i));
-//    }
-//
-//    std::iota(ram_span.begin(), ram_span.end(), 0);
-//
-//    for (auto& i : sdram_span()) {
-//        tos::println(usart, int(i));
-//    }
 
     tos::println(usart, "init display!");
 
@@ -74,6 +53,7 @@ void blink_task() {
     tos::stm32::f7::ltdc display(layer);
     tos::println(usart, "past!");
 
+    auto led_pin = tos::stm32::instantiate_pin(bs::led_pin);
     g.set_pin_mode(led_pin, tos::pin_mode::out);
 
     g.write(led_pin, tos::digital::high);
@@ -84,14 +64,9 @@ void blink_task() {
     while (true) {
         using namespace std::chrono_literals;
         dma2d.fill_rect(ram_span.data(), 0xFF0000FF, 480, 272);
-        tos::this_thread::sleep_for(alarm, 1000ms);
+        tos::this_thread::sleep_for(alarm, 330ms);
         dma2d.fill_rect(ram_span.data(), 0xFFFF0000, 480, 272);
-        tos::this_thread::sleep_for(alarm, 1000ms);
-        tos::println(usart, "tick");
-//        dma2d.fill_rect(sdram_span().data(), 0xF00F, 480, 272);
-//        tos::this_thread::sleep_for(alarm, 100ms);
-//        std::fill(sdram_span().begin(), sdram_span().end(), 0x0FF0);
-//        std::fill(sdram_span().begin(), sdram_span().end(), 0xF00F);
+        tos::this_thread::sleep_for(alarm, 330ms);
     }
 }
 
