9053f1bb8b904f932b25f0a72079b3541d815e12
Fatih Bakir <mfatihbakir@gmail.com>
2020-11-02 16:11:05 -0800

    [ft] suspended_launch support, clean up tests

---
+++ src/ft/include/tos/suspended_launch.hpp
@@ -0,0 +1,24 @@
+#include <tos/ft.hpp>
+#include <tos/semaphore.hpp>
+
+namespace tos {
+template<class StackT, class FnT, class... Args>
+auto& suspended_launch(StackT&& s, FnT&& fn, Args&&... arg) {
+    tos::semaphore sem{0};
+    auto& res = tos::launch(std::forward<StackT>(s),
+                            [&sem,
+                             fn = std::forward<FnT>(fn),
+                             args = std::tuple<Args...>(std::forward<Args>(arg)...)] {
+                                // Let the launcher know we were scheduled and have a
+                                // context.
+                                sem.up();
+                                // Now, suspend the thread.
+                                tos::kern::suspend_self(tos::int_guard{});
+                                // Someone else made us runnable, call the user entry
+                                // point.
+                                std::apply(fn, std::move(args));
+                            });
+    sem.down();
+    return res;
+}
+} // namespace tos
