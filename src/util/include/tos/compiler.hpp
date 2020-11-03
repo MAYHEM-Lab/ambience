bedb96537827f892e3203173a05922bca5e31937
Fatih Bakir <mfatihbakir@gmail.com>
2020-11-02 16:11:25 -0800

    [util_core] Format compiler header, undef PACKED if defined

--- src/util/include/tos/compiler.hpp
+++ src/util/include/tos/compiler.hpp
@@ -9,7 +9,7 @@
 #endif
 
 #define ALWAYS_INLINE [[gnu::always_inline]] inline
-#define NO_INLINE [[gnu::noinline]]
+#define NO_INLINE     [[gnu::noinline]]
 
 #define TOS_EXPORT [[gnu::visibility("default")]]
 #define TOS_HIDDEN [[gnu::visibility("hidden")]]
@@ -21,10 +21,10 @@
 #define TOS_UNREACHABLE() __builtin_unreachable()
 
 #if defined(__GNUC__) && !defined(__clang__)
-#define TOS_NO_OPTIMIZE [[gnu::optimize("-O0")]]
+#define TOS_NO_OPTIMIZE   [[gnu::optimize("-O0")]]
 #define TOS_SIZE_OPTIMIZE [[gnu::optimize("Os")]]
 #elif defined(__clang__)
-#define TOS_NO_OPTIMIZE [[clang::optnone]]
+#define TOS_NO_OPTIMIZE   [[clang::optnone]]
 #define TOS_SIZE_OPTIMIZE [[clang::minsize]]
 #endif
 
@@ -32,4 +32,7 @@
 
 #define USED [[gnu::used]]
 
+#if defined(PACKED)
+#undef PACKED
+#endif
 #define PACKED [[gnu::packed]]
