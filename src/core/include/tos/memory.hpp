a836a9055e5d19ad1283d75414e78f7f47301414
Fatih Bakir <mfatihbakir@gmail.com>
2020-11-03 16:51:27 -0800

    [core] read_write and read_execute flags in permissions, format

--- src/core/include/tos/memory.hpp
+++ src/core/include/tos/memory.hpp
@@ -15,8 +15,7 @@ struct memory_region {
     std::uintptr_t base;
     std::uint32_t size;
 
-    [[nodiscard]]
-    std::uintptr_t end() const {
+    [[nodiscard]] std::uintptr_t end() const {
         return base + size;
     }
 };
@@ -32,11 +31,14 @@ constexpr bool contains(const memory_region& big, const memory_region& small) {
     return big.base <= small.base && big.base + big.size >= small.base + small.size;
 }
 
-enum class permissions : uint8_t {
+enum class permissions : uint8_t
+{
     none,
     read = 1,
     write = 2,
+    read_write = 3,
     execute = 4,
+    read_execute = 5,
     all = 7
 };
 } // namespace tos
