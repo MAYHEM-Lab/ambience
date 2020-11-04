f1d950fb4e570fa1cb4f35c36badda3aabefc877
Fatih Bakir <mfatihbakir@gmail.com>
2020-11-03 23:47:10 -0800

    [aarch64] Exception types

--- src/arch/aarch64/include/tos/aarch64/interrupts.hpp
+++ src/arch/aarch64/include/tos/aarch64/interrupts.hpp
@@ -3,6 +3,13 @@
 #include <tos/compiler.hpp>
 
 namespace tos::aarch64 {
+enum class exception_type {
+    synchronous,
+    irq,
+    fiq,
+    system_error
+};
+
 ALWAYS_INLINE
 void enable_interrupts() {
     __asm__ __volatile__("msr daifclr, #2" ::: "memory");
