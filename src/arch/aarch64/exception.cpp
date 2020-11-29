762641a08391e19ed8b78e81916b4ef136669662
Fatih Bakir <mfatihbakir@gmail.com>
2020-11-29 00:29:50 -0800

    [aarch64] Fault handling clean up

--- src/arch/aarch64/exception.cpp
+++ src/arch/aarch64/exception.cpp
@@ -3,27 +3,40 @@
 #include <tos/debug/debug.hpp>
 #include <tos/debug/log.hpp>
 
+extern "C" {
+int64_t _irq_count;
+int64_t _irq_exit_count;
+}
+
 namespace tos::aarch64::exception {
 namespace {
 svc_handler_t _svc_handler{[](int, stack_frame_t&, void*) {}};
-}
+fault_handler_t _fault_handler{[](const fault_variant&, stack_frame_t&, void*) {}};
+} // namespace
 
 svc_handler_t set_svc_handler(svc_handler_t handler) {
     std::swap(_svc_handler, handler);
     return handler;
 }
 
+fault_handler_t set_fault_handler(fault_handler_t handler) {
+    std::swap(_fault_handler, handler);
+    return handler;
+}
+
 data_abort analyze_data_abort(uint64_t esr, stack_frame_t* frame) {
-    auto ISS = esr & 0xFFFFFF;
-//    LOG("ISS", itoa(ISS, 2).data());
+    auto far = aarch64::get_far_l1();
+
+    const auto ISS = esr & 0x1FFFFFF;
+    //    LOG("ISS", itoa(ISS, 2).data());
 
     auto isv = static_cast<bool>((ISS >> 24) & 0b1);
 
     if (!isv) {
-//        LOG_WARN("Invalid syndrome!");
+        return data_abort{
+            {}, far, {}, {}, ISS,
+        };
     }
-
-    auto far = aarch64::get_far_l1();
 
     auto size = static_cast<access_size>((ISS >> 22) & 0b11);
     auto sign_extend = static_cast<bool>((ISS >> 21) & 1);
@@ -32,24 +45,17 @@ data_abort analyze_data_abort(uint64_t esr, stack_frame_t* frame) {
     tos::debug::do_not_optimize(size);
     tos::debug::do_not_optimize(sign_extend);
 
-    while (true);
-    return data_abort{
-
-    };
+    return data_abort{{}, far, size, sign_extend, ISS};
 }
 
-void sync_handler([[maybe_unused]] uint64_t from, stack_frame_t* frame) {
-    auto esr = aarch64::get_esr_l1();
+fault_variant analyze_fault(stack_frame_t* frame) {
+    const auto esr = aarch64::get_esr_l1();
 
     auto EC = (esr >> 26) & 0b111111;
 
-    auto ISS = esr & 0xFFFFFF;
-
     switch (static_cast<exception_classes>(EC)) {
     case exception_classes::undefined:
-        while (true)
-            ;
-        break;
+        return undefined_instruction{};
     case exception_classes::SVC64: {
         // The LR stores the instruction to return to. So the SVC instruction is the one
         // before it. Thankfully, ARM ISA is sane and we can get the previous instruction
@@ -59,25 +65,26 @@ void sync_handler([[maybe_unused]] uint64_t from, stack_frame_t* frame) {
         // https://developer.arm.com/docs/ddi0596/h/base-instructions-alphabetic-order/svc-supervisor-call
         auto svnum = (instr >> 5) & 0xFFFF;
         _svc_handler(svnum, *frame);
-        break;
+        return svc_exception{{}, svnum};
     }
     case exception_classes::instruction_abort:
     case exception_classes::instruction_abort_lower_level:
-//        LOG("Instruction abort");
-//        LOG("ISS", itoa(ISS, 2).data());
-        while (true);
-        break;
+        return unknown_fault{};
     case exception_classes::data_abort:
     case exception_classes::data_abort_lower_level:
-        analyze_data_abort(esr, frame);
-//        LOG("Data abort");
-//        LOG("ISS", itoa(ISS, 2).data());
-        break;
-    default:
-//        LOG("Not handled");
-        while (true);
-        break;
+        return analyze_data_abort(esr, frame);
     }
+
+    return unknown_fault{};
+}
+
+void sync_handler([[maybe_unused]] uint64_t from, stack_frame_t* frame) {
+    auto fault = analyze_fault(frame);
+    if (std::get_if<svc_exception>(&fault)) {
+        return;
+    }
+    std::visit([](auto& f) { f.return_address = get_elr_l1(); }, fault);
+    _fault_handler(fault, *frame);
 }
 } // namespace tos::aarch64::exception
 
--- src/arch/aarch64/include/tos/aarch64/exception.hpp
+++ src/arch/aarch64/include/tos/aarch64/exception.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <cstdint>
-#include <variant>
 #include <tos/function_ref.hpp>
+#include <variant>
 
 namespace tos::aarch64::exception {
 struct stack_frame_t {
@@ -29,7 +29,8 @@ struct fault_t {
 
 struct instruction_abort : fault_t {};
 
-enum class access_size : uint8_t {
+enum class access_size : uint8_t
+{
     byte,
     halfword,
     word,
@@ -37,15 +38,29 @@ enum class access_size : uint8_t {
 };
 
 struct data_abort : fault_t {
+    uintptr_t data_addr;
     access_size size;
     bool sign_extend;
+    uint64_t iss;
 };
 
 struct undefined_instruction : fault_t {};
 
-using fault_variant = std::variant<instruction_abort, data_abort, undefined_instruction>;
+struct svc_exception : fault_t {
+    uint32_t svnum;
+};
+
+struct unknown_fault : fault_t {};
+
+using fault_variant = std::variant<unknown_fault,
+                                   instruction_abort,
+                                   data_abort,
+                                   undefined_instruction,
+                                   svc_exception>;
 
 using svc_handler_t = tos::function_ref<void(int, stack_frame_t&)>;
+using fault_handler_t = tos::function_ref<void(const fault_variant&, stack_frame_t&)>;
 
 svc_handler_t set_svc_handler(svc_handler_t handler);
+fault_handler_t set_fault_handler(fault_handler_t handler);
 } // namespace tos::aarch64::exception
