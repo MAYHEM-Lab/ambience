48e8152ddb02f6fe3aeeb97caeae6d10d402fe58
Fatih Bakir <mfatihbakir@gmail.com>
2020-12-17 22:22:15 -0800

    [core] Virtual memory support and physical memory management improvements

--- src/core/include/tos/memory.hpp
+++ src/core/include/tos/memory.hpp
@@ -6,6 +6,10 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <memory>
+#include <tos/intrusive_list.hpp>
+#include <tos/intrusive_ptr.hpp>
+#include <tos/span.hpp>
 
 namespace tos {
 /**
@@ -34,7 +38,7 @@ constexpr bool contains(const memory_range& big, const memory_range& small) {
 }
 
 constexpr bool contains(const memory_range& range, uintptr_t addr) {
-    return range.base <= addr && range.end() >= addr;
+    return range.base <= addr && addr < range.end();
 }
 
 enum class permissions : uint8_t
@@ -60,4 +64,141 @@ memory_range text();
 memory_range rodata();
 memory_range bss();
 } // namespace default_segments
+
+struct mapping;
+struct fault_frame;
+
+struct physical_page : ref_counted<physical_page> {
+    const mapping* map;
+
+    bool free() const {
+        return reference_count() == 0;
+    }
+};
+
+class physical_page_allocator {
+public:
+    explicit physical_page_allocator(size_t num_pages);
+
+    // Allocates the given number of physical pages on the specified alignment.
+    // Both arguments are in numbers of physical pages.
+    // The alignment argument must be a power of 2.
+    intrusive_ptr<physical_page> allocate(int count, int align = 1);
+
+    void* address_of(const physical_page& page) const;
+
+    int page_num(const physical_page& page) const;
+
+    // Marks the given range of physical memory as unavailable.
+    void mark_unavailable(const memory_range& len);
+
+    physical_page* info(int32_t page_num);
+    physical_page* info(void* ptr);
+
+private:
+    span<physical_page> get_table() {
+        return {m_table, m_num_pages};
+    }
+
+    span<const physical_page> get_table() const {
+        return {m_table, m_num_pages};
+    }
+
+    size_t m_num_pages;
+    physical_page m_table[];
+};
+
+struct job;
+struct memory_fault {
+    const mapping* map;
+
+    bool non_resident : 1;
+
+    uintptr_t virt_addr;
+    permissions access_perms;
+
+    job* faulting_job;
+    fault_frame* frame;
+};
+
+enum class memory_types
+{
+    normal = 1,
+    device = 2
+};
+
+enum class user_accessible {
+    no,
+    yes
+};
+
+// Might be anonymous, a device or an abstract object.
+// A backing object is responsible for handling page faults in the segments it is mapped
+// to in an address space.
+class backing_object : public ref_counted<backing_object> {
+public:
+    virtual auto create_mapping(const segment& vm_segment, const memory_range& obj_range)
+        -> std::unique_ptr<mapping> = 0;
+    virtual auto handle_memory_fault(const memory_fault& fault) -> void = 0;
+
+    virtual ~backing_object() = default;
+};
+
+class anonymous_backing : public backing_object {};
+
+// Segments mapped from these objects correspond to addresses in the physical memory of
+// the system and correspond to stuff like IO memory.
+// There can be multiple such objects, each mapping to the different part of the physical
+// address space with different permissions.
+class physical_memory_backing : public backing_object {
+public:
+    explicit physical_memory_backing(const segment& phys_seg, memory_types type)
+        : m_seg{phys_seg}
+        , m_type{type} {
+    }
+
+    void handle_memory_fault(const memory_fault& fault) override {
+    }
+
+    std::unique_ptr<mapping> create_mapping(const segment& vm_segment,
+                                            const memory_range& obj_range) override;
+
+private:
+    segment m_seg;
+    memory_types m_type;
+};
+
+struct vm_backend {
+public:
+    virtual bool do_map(const mapping&) = 0;
+    virtual bool
+    mark_resident(const mapping&, const memory_range& virt_range, bool resident) = 0;
+    virtual ~vm_backend() = 0;
+};
+
+class virtual_address_space;
+struct mapping {
+    intrusive_ptr<backing_object> obj;
+    virtual_address_space* va;
+
+    segment vm_segment;
+    memory_range obj_range;
+
+    memory_types mem_type;
+
+    list_node<mapping> obj_list;
+    list_node<mapping> va_list;
+
+    // Used by the backend to store its private data about this mapping.
+    void* backend_data;
+    // copy on write flags etc can be here? or should it be per page?
+};
+
+class virtual_address_space {
+public:
+    void create_mapping();
+
+private:
+    intrusive_list<mapping, through_member<&mapping::va_list>> m_mappings;
+};
 } // namespace tos
--- src/core/src/memory.cpp
+++ src/core/src/memory.cpp
@@ -1,3 +1,4 @@
+#include <new>
 #include <tos/memory.hpp>
 
 extern "C" {
@@ -21,44 +22,100 @@ namespace tos::default_segments {
 memory_range image() {
     auto beg = reinterpret_cast<uintptr_t>(&__start);
     auto end = reinterpret_cast<uintptr_t>(&__end);
-    return {
-        .base = beg,
-        .size = ptrdiff_t (end - beg)
-    };
+    return {.base = beg, .size = ptrdiff_t(end - beg)};
 }
 
 memory_range data() {
     auto beg = reinterpret_cast<uintptr_t>(&__data_start);
     auto end = reinterpret_cast<uintptr_t>(&__data_end);
-    return {
-        .base = beg,
-        .size = ptrdiff_t (end - beg)
-    };
+    return {.base = beg, .size = ptrdiff_t(end - beg)};
 }
 memory_range text() {
     auto beg = reinterpret_cast<uintptr_t>(&__text_start);
     auto end = reinterpret_cast<uintptr_t>(&__text_end);
-    return {
-        .base = beg,
-        .size = ptrdiff_t (end - beg)
-    };
+    return {.base = beg, .size = ptrdiff_t(end - beg)};
 }
 
 memory_range rodata() {
     auto beg = reinterpret_cast<uintptr_t>(&__rodata_start);
     auto end = reinterpret_cast<uintptr_t>(&__rodata_end);
-    return {
-        .base = beg,
-        .size = ptrdiff_t (end - beg)
-    };
+    return {.base = beg, .size = ptrdiff_t(end - beg)};
 }
 
 memory_range bss() {
     auto beg = reinterpret_cast<uintptr_t>(&__bss_start);
     auto end = reinterpret_cast<uintptr_t>(&__bss_end);
-    return {
-        .base = beg,
-        .size = ptrdiff_t (end - beg)
-    };
+    return {.base = beg, .size = ptrdiff_t(end - beg)};
 }
+} // namespace tos::default_segments
+
+namespace tos {
+std::unique_ptr<mapping>
+physical_memory_backing::create_mapping(const segment& vm_segment,
+                                        const memory_range& obj_range) {
+    if (!contains(m_seg.range, obj_range)) {
+        return nullptr;
+    }
+
+    if ((int(vm_segment.perms) & int(m_seg.perms)) != int(vm_segment.perms)) {
+        return nullptr;
+    }
+
+    auto res = std::make_unique<mapping>();
+    res->obj = intrusive_ptr<backing_object>(this);
+    res->vm_segment = vm_segment;
+    res->obj_range = obj_range;
+    res->mem_type = m_type;
+    return res;
 }
+
+physical_page_allocator::physical_page_allocator(size_t num_pages)
+    : m_num_pages{num_pages} {
+    for (auto& page : get_table()) {
+        new (&page) physical_page();
+    }
+    memory_range this_obj;
+    this_obj.base = reinterpret_cast<uintptr_t>(this);
+    this_obj.size = sizeof *this + get_table().size_bytes();
+    mark_unavailable(this_obj);
+}
+
+intrusive_ptr<physical_page> physical_page_allocator::allocate(int count, int align) {
+    if (count != 1 || align != 1)
+        return nullptr;
+    for (auto& page : get_table()) {
+        if (page.free()) {
+            return intrusive_ptr<physical_page> (&page);
+        }
+    }
+    return nullptr;
+}
+
+void* physical_page_allocator::address_of(const physical_page& page) const {
+    return reinterpret_cast<void*>(page_num(page) * 4096);
+}
+
+int physical_page_allocator::page_num(const physical_page& page) const {
+    return std::distance(get_table().data(), &page);
+}
+
+void physical_page_allocator::mark_unavailable(const memory_range& len) {
+    auto begin_num = align_nearest_down_pow2(len.base, 4096) / 4096;
+    auto end_num = align_nearest_up_pow2(len.end(), 4096) / 4096;
+    for (int i = begin_num; i < end_num; ++i) {
+        intrusive_ref(&get_table()[i]);
+    }
+}
+
+physical_page* physical_page_allocator::info(void* ptr) {
+    return info(reinterpret_cast<uintptr_t>(ptr) / 4096);
+}
+
+physical_page* physical_page_allocator::info(int32_t page_num) {
+    if (page_num >= m_num_pages) {
+        return nullptr;
+    }
+
+    return &get_table()[page_num];
+}
+} // namespace tos
