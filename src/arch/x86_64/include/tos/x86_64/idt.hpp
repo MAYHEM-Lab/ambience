34a371501fd699f7d8b3173539695db9405b2a2c
Fatih Bakir <mfatihbakir@gmail.com>
2021-01-26 11:36:09 -0800

    [x86_64] Fix IDT setup

--- src/arch/x86_64/include/tos/x86_64/idt.hpp
+++ src/arch/x86_64/include/tos/x86_64/idt.hpp
@@ -7,32 +7,41 @@ enum class idt_error {
 
 };
 
-struct interrupt_stack_frame_t {};
+struct interrupt_stack_frame_t {
+    uintptr_t instr_ptr;
+    uint64_t code_segment;
+    uint64_t flags;
+    uintptr_t stack_ptr;
+    uint64_t stack_sgement;
+};
 
 using interrupt_handler_t = void (*)(interrupt_stack_frame_t*);
+using exception_handler_t = void (*)(interrupt_stack_frame_t*, unsigned long int error);
 
 template <class HandlerT>
-struct idt_entry {
+struct [[gnu::packed]] idt_entry {
     uint16_t ptr_low;
     uint16_t gdt_sel;
-    uint16_t options;
+    uint8_t ist;
+    uint8_t type_attributes;
     uint16_t ptr_mid;
     uint32_t ptr_high;
     uint32_t __res;
 
     static expected<idt_entry, idt_error> create(HandlerT handler) {
-        idt_entry res;
+        idt_entry res{};
         auto ptr = reinterpret_cast<uint64_t>(handler);
         res.ptr_low = ptr & 0xFFFF;
         res.ptr_mid = (ptr >> 16) & 0xFFFF;
         res.ptr_high = (ptr >> 32) & 0xFFFFFFFF;
-        res.options = 0;
-        res.gdt_sel = 0;
+        res.ist = 0;
+        res.type_attributes = 0x8e;
+        res.gdt_sel = 8;
         return res;
     }
 };
 
-struct interrupt_descriptor_table {
+struct [[gnu::packed]] interrupt_descriptor_table {
     idt_entry<interrupt_handler_t> div_by_zero;
     idt_entry<interrupt_handler_t> debug;
     idt_entry<interrupt_handler_t> nmi;
@@ -41,11 +50,11 @@ struct interrupt_descriptor_table {
     idt_entry<interrupt_handler_t> out_of_bounds;
     idt_entry<interrupt_handler_t> invalid_opcode;
     idt_entry<interrupt_handler_t> device_not_available;
-    idt_entry<interrupt_handler_t> double_fault;
-    idt_entry<interrupt_handler_t> invalid_tss;
-    idt_entry<interrupt_handler_t> segment_not_present;
-    idt_entry<interrupt_handler_t> stack_segment_fault;
-    idt_entry<interrupt_handler_t> general_protection_fault;
+    idt_entry<exception_handler_t> double_fault;
+    idt_entry<exception_handler_t> invalid_tss;
+    idt_entry<exception_handler_t> segment_not_present;
+    idt_entry<exception_handler_t> stack_segment_fault;
+    idt_entry<exception_handler_t> general_protection_fault;
     idt_entry<interrupt_handler_t> page_fault;
     idt_entry<interrupt_handler_t> x87_fpu_fault;
     idt_entry<interrupt_handler_t> alignment_check;
@@ -53,5 +62,8 @@ struct interrupt_descriptor_table {
     idt_entry<interrupt_handler_t> simd_fpu_fault;
     idt_entry<interrupt_handler_t> virt;
     idt_entry<interrupt_handler_t> security_exception;
+    idt_entry<interrupt_handler_t> rest[256 - 20];
 };
+
+static_assert(sizeof (interrupt_descriptor_table) == 256 * 16);
 }
