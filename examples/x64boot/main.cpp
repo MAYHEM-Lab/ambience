aba68e4534f52bd6c687c7d1c2b9438f921a4d76
Fatih Bakir <mfatihbakir@gmail.com>
2021-01-28 00:21:22 -0800

    x64boot example dumps its page table

--- examples/x64boot/main.cpp
+++ examples/x64boot/main.cpp
@@ -1,8 +1,29 @@
 #include <tos/debug/dynamic_log.hpp>
 #include <tos/debug/sinks/serial_sink.hpp>
+#include <tos/flags.hpp>
 #include <tos/ft.hpp>
 #include <tos/peripheral/uart_16550.hpp>
 #include <tos/peripheral/vga_text.hpp>
+#include <tos/x86_64/mmu.hpp>
+
+void dump_table(tos::x86_64::translation_table& table) {
+    tos::x86_64::traverse_table_entries(
+        table, [](tos::memory_range range, tos::x86_64::table_entry& entry) {
+          LOG_TRACE(
+              "VirtAddress:", "[", (void*)(range.base), ",", (void*)(range.end()), ")");
+          LOG_TRACE("PhysAddress:",
+                    (void*)tos::x86_64::page_to_address(entry.page_num()));
+          char perm_string[4] = "R__";
+          auto perms = tos::x86_64::translate_permissions(entry);
+          if (tos::util::is_flag_set(perms, tos::permissions::write)) {
+              perm_string[1] = 'W';
+          }
+          if (tos::util::is_flag_set(perms, tos::permissions::execute)) {
+              perm_string[2] = 'X';
+          }
+          LOG_TRACE("Perms:", perm_string, "User:", entry.allow_user());
+        });
+}
 
 void thread() {
     auto uart_res = tos::x86_64::uart_16550::open();
@@ -17,10 +38,31 @@ void thread() {
 
     tos::debug::serial_sink uart_sink(&uart);
     tos::debug::detail::any_logger uart_log{&uart_sink};
-    uart_log.set_log_level(tos::debug::log_level::log);
+    uart_log.set_log_level(tos::debug::log_level::trace);
     tos::debug::set_default_log(&uart_log);
 
     LOG("Hello world!");
+
+    uint32_t cpuid_data[4];
+    __get_cpuid(0, &cpuid_data[0], &cpuid_data[1], &cpuid_data[2], &cpuid_data[3]);
+
+    char manufacturer_name[12];
+    memcpy(&manufacturer_name[0], &cpuid_data[1], 4);
+    memcpy(&manufacturer_name[4], &cpuid_data[3], 4);
+    memcpy(&manufacturer_name[8], &cpuid_data[2], 4);
+    LOG(manufacturer_name);
+
+    auto cr3 = tos::x86_64::read_cr3();
+    LOG("Page table at:", (void*)cr3);
+
+    auto& level0_table = tos::x86_64::get_current_translation_table();
+
+    dump_table(level0_table);
+
+    for (uintptr_t i = 0; i < 0x40000000; ++i) {
+        auto ptr = (volatile char*)i;
+        LOG((void*)i, *ptr);
+    }
 
     while (true) {
         tos::this_thread::yield();
