21658bce0e4b4ef2f818f48aedfbc59749aa23b4
Fatih Bakir <mfatihbakir@gmail.com>
2021-02-01 11:03:27 -0800

    [clangd] Clangd changed the location of its cache

--- switch_clangd.sh
+++ switch_clangd.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 
 rm -f compile_commands.json
-rm -f .clangd
+rm -f .cache
 
 touch $1/compile_commands.json
-mkdir -p $1/.clangd
+mkdir -p $1/.cache/clangd
 
 ln -s "$1/compile_commands.json"
-ln -s "$1/.clangd"
+ln -s "$1/.cache"
