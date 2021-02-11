81ebf6e1f8f5a7d0ce9a2f3299e9b71091e01e78
Fatih Bakir <mfatihbakir@gmail.com>
2021-02-10 17:24:54 -0800

    [util] Format

--- src/util/include/tos/sync_ring_buf.hpp
+++ src/util/include/tos/sync_ring_buf.hpp
@@ -10,57 +10,60 @@
 #include <tos/semaphore.hpp>
 #include <tos/fixed_fifo.hpp>
 
-namespace tos
-{
-    class sync_ring_buf : private ring_buf
-    {
-    public:
-        explicit sync_ring_buf(size_t cap, size_t sz, size_t begin)
-            : ring_buf{cap, sz, begin}, m_read{int16_t(sz)}, m_put{int16_t(cap - sz)} {}
-        /**
-         * Creates a new synchronized ring buf with the given capacity
-         * @param cap capacity
-         */
-        explicit sync_ring_buf(size_t cap) : ring_buf{cap}, m_read{0}, m_put{int16_t(cap)} {}
+namespace tos {
+class sync_ring_buf : private ring_buf {
+public:
+    explicit sync_ring_buf(size_t cap, size_t sz, size_t begin)
+        : ring_buf{cap, sz, begin}
+        , m_read{ssize_t(sz)}
+        , m_put{ssize_t(cap - sz)} {
+    }
+    /**
+     * Creates a new synchronized ring buf with the given capacity
+     * @param cap capacity
+     */
+    explicit sync_ring_buf(size_t cap)
+        : ring_buf{cap}
+        , m_read{0}
+        , m_put{ssize_t(cap)} {
+    }
 
-        using ring_buf::size;
-        using ring_buf::capacity;
-        using ring_buf::translate;
+    using ring_buf::capacity;
+    using ring_buf::size;
+    using ring_buf::translate;
 
-        size_t push()
-        {
-            m_put.down();
-            auto res = ring_buf::push();
-            m_read.up();
-            return res;
-        }
+    size_t push() {
+        m_put.down();
+        auto res = ring_buf::push();
+        m_read.up();
+        return res;
+    }
 
-        //TODO: return an optional<size_t> here
-        size_t push_isr()
-        {
-            auto cont = try_down_isr(m_put);
-            if (!cont) return -1;
-            auto res = ring_buf::push();
-            m_read.up_isr();
-            return res;
-        }
+    // TODO: return an optional<size_t> here
+    size_t push_isr() {
+        auto cont = try_down_isr(m_put);
+        if (!cont)
+            return -1;
+        auto res = ring_buf::push();
+        m_read.up_isr();
+        return res;
+    }
 
-        size_t pop()
-        {
-            m_read.down();
-            auto res = ring_buf::pop();
-            m_put.up();
-            return res;
-        }
+    size_t pop() {
+        m_read.down();
+        auto res = ring_buf::pop();
+        m_put.up();
+        return res;
+    }
 
-        bool empty() const {
-            return size() == 0;
-        }
+    bool empty() const {
+        return size() == 0;
+    }
 
-    private:
-        tos::semaphore m_read, m_put;
-    };
+private:
+    tos::semaphore_base<ssize_t> m_read, m_put;
+};
 
-    template <class ElemT, size_t Size>
-    using sync_fixed_fifo = basic_fixed_fifo<ElemT, Size, sync_ring_buf>;
-}
+template<class ElemT, size_t Size>
+using sync_fixed_fifo = basic_fixed_fifo<ElemT, Size, sync_ring_buf>;
+} // namespace tos
