9f84729ba9ddad1b0cf5625b595772f9ceb2a98e
Fatih Bakir <mfatihbakir@gmail.com>
2021-02-12 10:40:06 -0800

    [arm] Build ARM targets with LLVM

--- cmake/ArmToolchain.cmake
+++ cmake/ArmToolchain.cmake
@@ -2,6 +2,10 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
 include(FindToolchain)
-find_gnu_toolchain(TRIPLE arm-none-eabi SET)
+find_llvm_toolchain(TRIPLE arm-none-eabi SET)
 
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_LINKER ${TOOLCHAIN_LD})
+message(STATUS ${CMAKE_LINKER})
+set(CMAKE_CXX_LINK_EXECUTABLE "<CMAKE_LINKER> -o <TARGET> --lto-O3 --icf=all --gc-sections <OBJECTS> <LINK_LIBRARIES>")
