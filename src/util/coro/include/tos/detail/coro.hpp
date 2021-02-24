bc1f77339b87d6404854bf47e93a971d376b91e8
Fatih Bakir <mfatihbakir@gmail.com>
2021-02-23 23:03:59 -0800

    [coro] Trying to understand coroutines ...

--- src/util/coro/include/tos/detail/coro.hpp
+++ src/util/coro/include/tos/detail/coro.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
-#if __has_include(<experimental/coroutine>)
+#if __has_include(<coroutine>)
+#include <coroutine>
+#elif __has_include(<experimental/coroutine>)
 #include <experimental/coroutine>
 
 namespace std {
@@ -8,6 +10,4 @@ using experimental::coroutine_handle;
 using experimental::suspend_always;
 using experimental::suspend_never;
 } // namespace std
-#elif __has_include(<coroutine>)
-#include <coroutine>
 #endif
--- src/util/coro/include/tos/task.hpp
+++ src/util/coro/include/tos/task.hpp
@@ -1,49 +1,74 @@
 #pragma once
 
+#include <iostream>
 #include <tos/detail/coro.hpp>
 #include <utility>
+
+#define __PRETTY_FUNCTION__ __FUNCSIG__
+#define dump() std::cerr << this << " " << __PRETTY_FUNCTION__ << '\n';
 
 namespace tos {
 template<class ResultT, class ErrorT = void>
 class task {
 public:
+    struct promise_type;
+    using coro_handle = std::coroutine_handle<promise_type>;
+
     struct promise_type {
         task get_return_object() noexcept {
-            return {};
+            dump();
+            return task{coro_handle::from_promise(*this)};
         }
+
         std::suspend_never initial_suspend() noexcept {
+            dump();
             return {};
         }
+
         std::suspend_never final_suspend() noexcept {
+            dump();
+
             return {};
         }
+
         void unhandled_exception() {
         }
 
         template<class T>
         void return_value(T&& val) {
+            dump();
+
             m_val = std::forward<T>(val);
         }
 
         ResultT m_val;
     };
 
-    struct promise_type;
-    using coro_handle = std::coroutine_handle<promise_type>;
+    task(coro_handle handle)
+        : handle_(handle) {
+        dump();
+    }
+    task(task&) = delete;
+    task(task&&) = delete;
 
     constexpr bool await_ready() const noexcept {
+        dump();
+
         return false;
     }
 
-    std::coroutine_handle<> await_suspend(std::coroutine_handle<> h) {
+    void await_suspend(std::coroutine_handle<> h) {
+        dump();
+
         m_cont = h;
-        return *this;
     }
 
     constexpr void await_resume() const noexcept {
+        dump();
     }
 
 private:
     std::coroutine_handle<> m_cont;
+    coro_handle handle_;
 };
 } // namespace tos
