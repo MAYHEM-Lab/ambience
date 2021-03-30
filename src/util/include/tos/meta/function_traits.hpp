49b3ae69702f07e56227e363ba5eb8d5cb17f01c
Fatih Bakir <mfatihbakir@gmail.com>
2021-03-30 01:11:23 -0700

    [util] clean_t not needed

--- src/util/include/tos/meta/function_traits.hpp
+++ src/util/include/tos/meta/function_traits.hpp
@@ -7,8 +7,6 @@ template <class T> struct identity{
     using type = T;
 };
 template <class...> struct list {};
-template<class T>
-using clean_t = std::remove_const_t<std::remove_reference_t<T>>;
 
 template<class T>
 struct function_traits;
@@ -16,14 +14,14 @@ struct function_traits;
 template<class RetT, class... ArgTs>
 struct function_traits<RetT (*)(ArgTs...)> {
     using ret_t = RetT;
-    using arg_ts = list<clean_t<ArgTs>...>;
+    using arg_ts = list<ArgTs...>;
     static constexpr auto arg_len = sizeof...(ArgTs);
 };
 
 template<class RetT, class... ArgTs>
 struct function_traits<RetT (&)(ArgTs...)> {
     using ret_t = RetT;
-    using arg_ts = list<clean_t<ArgTs>...>;
+    using arg_ts = list<ArgTs...>;
     static constexpr auto arg_len = sizeof...(ArgTs);
 };
 
@@ -31,7 +29,7 @@ template<class RetT, class ClassT, class... ArgTs>
 struct function_traits<RetT (ClassT::*)(ArgTs...)> {
     using ret_t = RetT;
     using class_t = ClassT;
-    using arg_ts = list<clean_t<ArgTs>...>;
+    using arg_ts = list<ArgTs...>;
     static constexpr auto arg_len = sizeof...(ArgTs);
     static constexpr bool is_const = false;
 };
@@ -40,7 +38,7 @@ template<class RetT, class ClassT, class... ArgTs>
 struct function_traits<RetT (ClassT::*)(ArgTs...) const> {
     using ret_t = RetT;
     using class_t = ClassT;
-    using arg_ts = list<clean_t<ArgTs>...>;
+    using arg_ts = list<ArgTs...>;
     static constexpr auto arg_len = sizeof...(ArgTs);
     static constexpr bool is_const = true;
 };
@@ -49,7 +47,7 @@ template<class RetT, class ClassT, class... ArgTs>
 struct function_traits<RetT (ClassT::*)(ArgTs...) noexcept> {
     using ret_t = RetT;
     using class_t = ClassT;
-    using arg_ts = list<clean_t<ArgTs>...>;
+    using arg_ts = list<ArgTs...>;
     static constexpr auto arg_len = sizeof...(ArgTs);
     static constexpr bool is_const = true;
 };
