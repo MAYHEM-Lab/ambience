a026f667dec0efad344321ede97c80f4480ef4ee
Fatih Bakir <mfatihbakir@gmail.com>
2021-03-31 23:44:20 -0700

    [cmake] Build tests with address sanitizer

--- cmake/Tos.cmake
+++ cmake/Tos.cmake
@@ -46,6 +46,10 @@ if(ENABLE_UBSAN)
     set(TOS_FLAGS "${TOS_FLAGS} -fsanitize=bool")
 endif()
 
+if(BUILD_TESTS)
+    set(TOS_FLAGS "${TOS_FLAGS} -fsanitize=address")
+endif()
+
 if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
     set(TOS_FLAGS "${TOS_FLAGS} \
             -Wno-c99-extensions -Wno-gnu-anonymous-struct -Wno-new-returns-null \
