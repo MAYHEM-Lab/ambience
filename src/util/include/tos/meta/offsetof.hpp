904f4fd22bb2e2555b21e948500b9ad42610e00a
Fatih Bakir <mfatihbakir@gmail.com>
2021-04-07 00:51:43 -0700

    [util] offsetof is constexpr through a horrible hack

--- src/util/include/tos/meta/offsetof.hpp
+++ src/util/include/tos/meta/offsetof.hpp
@@ -1,17 +1,39 @@
 #pragma once
 
-#include <cstdint>
 #include <cstddef>
+#include <cstdint>
 #include <type_traits>
 #include <utility>
 
 namespace tos::meta {
-template <class T1, class T2>
+namespace detail {
+template<typename T>
+struct declval_helper {
+    static inline T value;
+};
+
+template<typename T, typename Z, Z T::*MPtr>
+struct offset_helper {
+    using TV = declval_helper<T>;
+    char for_sizeof[(char*)&(TV::value.*MPtr) - (char*)&TV::value];
+};
+
+template<typename T, typename Z, Z T::*MPtr>
+constexpr int offset_of() {
+    return sizeof(detail::offset_helper<T, Z, MPtr>::for_sizeof);
+}
+
+template<class T1, class T2>
 auto class_type(T1 T2::*) -> T2;
 
-template <auto PtrToMem>
-int offset_of() {
-    using class_t = decltype(class_type(PtrToMem));
-    return reinterpret_cast<intptr_t>(&(static_cast<class_t*>(nullptr)->*PtrToMem));
+template<class T1, class T2>
+auto return_type(T1 T2::*) -> T1;
+} // namespace detail
+
+template<auto PtrToMem>
+constexpr int offset_of() {
+    using class_t = decltype(detail::class_type(PtrToMem));
+    using ret_t = decltype(detail::return_type(PtrToMem));
+    return detail::offset_of<class_t, ret_t, PtrToMem>();
 }
-}
+} // namespace tos::meta
