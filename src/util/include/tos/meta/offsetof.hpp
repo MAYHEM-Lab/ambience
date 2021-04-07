a35fb6f75c6856a1a0fc91d8863e3ee85e7eddbc
Fatih Bakir <mfatihbakir@gmail.com>
2021-04-07 15:55:25 -0700

    [util] Revert offsetof changes

--- src/util/include/tos/meta/offsetof.hpp
+++ src/util/include/tos/meta/offsetof.hpp
@@ -6,34 +6,12 @@
 #include <utility>
 
 namespace tos::meta {
-namespace detail {
-template<typename T>
-struct declval_helper {
-    static inline T value;
-};
-
-template<typename T, typename Z, Z T::*MPtr>
-struct offset_helper {
-    using TV = declval_helper<T>;
-    char for_sizeof[(char*)&(TV::value.*MPtr) - (char*)&TV::value];
-};
-
-template<typename T, typename Z, Z T::*MPtr>
-constexpr int offset_of() {
-    return sizeof(detail::offset_helper<T, Z, MPtr>::for_sizeof);
-}
-
-template<class T1, class T2>
+template <class T1, class T2>
 auto class_type(T1 T2::*) -> T2;
 
-template<class T1, class T2>
-auto return_type(T1 T2::*) -> T1;
-} // namespace detail
-
-template<auto PtrToMem>
+template <auto PtrToMem>
 int offset_of() {
-    using class_t = decltype(detail::class_type(PtrToMem));
-    using ret_t = decltype(detail::return_type(PtrToMem));
-    return detail::offset_of<class_t, ret_t, PtrToMem>();
+    using class_t = decltype(class_type(PtrToMem));
+    return reinterpret_cast<intptr_t>(&(static_cast<class_t*>(nullptr)->*PtrToMem));
 }
 } // namespace tos::meta
