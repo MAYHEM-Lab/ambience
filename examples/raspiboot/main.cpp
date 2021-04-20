dfd7f4b55a160a536a37e56beb78217b3e6dbb22
Fatih Bakir <mfatihbakir@gmail.com>
2021-04-20 14:43:11 -0700

    [raspi3] Move generic timer out of raspiboot

--- examples/raspiboot/main.cpp
+++ examples/raspiboot/main.cpp
@@ -412,59 +412,6 @@ public:
     }
 };
 } // namespace tos::aarch64
-
-namespace tos::raspi3 {
-class generic_timer : public self_pointing<generic_timer> {
-public:
-    generic_timer(interrupt_controller& ic, int core_num)
-        : m_handler(mem_function_ref<&generic_timer::irq>(*this))
-        , m_core{core_num} {
-        ic.register_handler(bcm283x::irq_channels::generic_timer, m_handler);
-    }
-
-    void enable();
-    void disable();
-
-    void set_frequency(uint16_t hz);
-    void set_callback(function_ref<void()> fn) {
-        m_fn = fn;
-    }
-
-private:
-    bool irq();
-
-    irq_handler m_handler;
-    int m_core;
-    uint64_t m_period;
-    function_ref<void()> m_fn{[](auto...) {}};
-};
-} // namespace tos::raspi3
-
-namespace tos::raspi3 {
-void generic_timer::set_frequency(uint16_t hz) {
-    m_period = aarch64::generic_timer::get_frequency() / hz;
-}
-
-void generic_timer::enable() {
-    // Enable the SVC generic timer interrupt.
-    bcm2837::ARM_CORE->core0_timers_irq_control = 2;
-    bcm2837::INTERRUPT_CONTROLLER->enable_basic_irq = 1;
-    aarch64::generic_timer::set_timeout(m_period);
-    aarch64::generic_timer::enable();
-}
-
-void generic_timer::disable() {
-    aarch64::generic_timer::disable();
-    bcm2837::INTERRUPT_CONTROLLER->disable_basic_irq = 1;
-}
-
-bool generic_timer::irq() {
-    m_fn();
-    aarch64::generic_timer::set_timeout(m_period);
-    return true;
-}
-} // namespace tos::raspi3
-
 enum class service_errors
 {
     not_found,
--- src/drivers/arch/raspi3/include/arch/drivers.hpp
+++ src/drivers/arch/raspi3/include/arch/drivers.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
 #include "framebuffer.hpp"
+#include "general.hpp"
+#include "generic_timer.hpp"
 #include "mailbox.hpp"
 #include "system_timer.hpp"
 #include "usart.hpp"
-#include "general.hpp"
