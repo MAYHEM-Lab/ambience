7f49465e9b8bc688c6e0090f97ccbfc806db9178
Fatih Bakir <mfatihbakir@gmail.com>
2021-04-26 16:39:28 -0700

    [ubsan] __ubsan_handle_load_invalid_value takes params

--- src/sanitizer/ubsan/handlers.cpp
+++ src/sanitizer/ubsan/handlers.cpp
@@ -1,4 +1,5 @@
 #include <tos/sanitizer/ubsan.hpp>
+#include <cstdint>
 
 namespace tos::ubsan::handlers {
 void nonnull_arg() {
@@ -14,7 +15,7 @@ extern "C" {
 void __ubsan_handle_nonnull_arg() {
     tos::ubsan::handlers::nonnull_arg();
 }
-void __ubsan_handle_load_invalid_value() {
+void __ubsan_handle_load_invalid_value(int32_t, int32_t) {
     tos::ubsan::handlers::nonnull_arg();
 }
 }
