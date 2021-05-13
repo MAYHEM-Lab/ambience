4f006984093f9269ca7e0fab35bafd2750c195b7
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-13 14:48:52 -0500

    [ambience] Async service host has proc runner

--- ambience/include/tos/ae/service_host.hpp
+++ ambience/include/tos/ae/service_host.hpp
@@ -30,15 +30,23 @@ struct async_service_host {
     template<class ServiceT>
     explicit async_service_host(ServiceT* serv)
         : impl{serv}
-        , zerocopy_vtable{
-              lidl::make_async_zerocopy_vtable<typename ServiceT::service_type>()} {
+        , zerocopy_vtable{lidl::make_async_zerocopy_vtable<
+              typename ServiceT::service_type>()}
+        , message_runner{lidl::make_async_erased_procedure_runner<
+              typename ServiceT::service_type::async_server>()} {
     }
 
     tos::Task<bool> run_zerocopy(int proc, const void* arg, void* res) const {
         return zerocopy_vtable[proc](*impl, arg, res);
     }
 
+    tos::Task<bool> run_message(tos::span<uint8_t> buffer,
+                                lidl::message_builder& response_builder) const {
+        return message_runner(*impl, buffer, response_builder);
+    }
+
     lidl::service_base* impl;
     lidl::async_zerocopy_vtable_t zerocopy_vtable;
+    lidl::async_erased_procedure_runner_t message_runner;
 };
 } // namespace tos::ae
