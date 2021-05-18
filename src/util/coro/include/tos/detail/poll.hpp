f6462b884306806f2f121a4caf234c6a0a2fc0d6
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-18 15:29:15 -0700

    [coro] detached coroutines

--- src/util/coro/include/tos/detail/poll.hpp
+++ src/util/coro/include/tos/detail/poll.hpp
@@ -1,10 +1,44 @@
 #pragma once
 
 #include <tos/detail/coro.hpp>
+#include <tos/expected.hpp>
 #include <tos/task.hpp>
 #include <utility>
 
 namespace tos::coro {
+class detached {
+public:
+    struct promise_type;
+    using coro_handle = std::coroutine_handle<promise_type>;
+
+    struct promise_type {
+        detached get_return_object() noexcept {
+            return detached{coro_handle::from_promise(*this)};
+        }
+
+        std::suspend_never initial_suspend() noexcept {
+            return {};
+        }
+
+        std::suspend_never final_suspend() const noexcept {
+            return {};
+        }
+
+        void unhandled_exception() {
+        }
+
+        void return_void() {
+        }
+    };
+
+    explicit detached(coro_handle handle)
+        : handle_(handle) {
+    }
+
+private:
+    coro_handle handle_;
+};
+
 class pollable {
 public:
     struct promise_type;
@@ -59,10 +93,17 @@ private:
     coro_handle handle_;
 };
 
-
-template<class T>
-pollable make_pollable(Task<T> x) {
+template<class T, class BeforeFin = tos::ignore_t>
+pollable make_pollable(Task<T> x, BeforeFin fin = {}) {
     co_await x;
+    fin();
+    co_return;
+}
+
+template<class T, class BeforeFin = tos::ignore_t>
+detached make_detached(Task<T> x, BeforeFin fin = {}) {
+    co_await x;
+    fin();
     co_return;
 }
 } // namespace tos::coro
