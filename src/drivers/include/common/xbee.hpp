0bede73fa5462d409130eaf68249fbb7f336da12
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 16:37:56 -0700

    [xbee] Format

--- src/drivers/include/common/xbee.hpp
+++ src/drivers/include/common/xbee.hpp
@@ -1,42 +1,37 @@
 #pragma once
 
-#include <stdint.h>
-#include <tos/span.hpp>
 #include "xbee/constants.hpp"
-#include "xbee/types.hpp"
 #include "xbee/request.hpp"
 #include "xbee/response.hpp"
-#include <tos/utility.hpp>
+#include "xbee/types.hpp"
+#include <stdint.h>
 #include <tos/print.hpp>
+#include <tos/span.hpp>
+#include <tos/utility.hpp>
 
-namespace tos
-{
-    template <class UsartT>
-    class xbee_s1
-    {
-    public:
+namespace tos {
+template<class UsartT>
+class xbee_s1 {
+public:
+    explicit xbee_s1(UsartT& dev) noexcept
+        : m_usart{&dev} {
+    }
 
-        explicit xbee_s1(UsartT& dev) noexcept
-            : m_usart{&dev} {}
+    template<class TxPacket>
+    void transmit(const TxPacket& packet);
 
-        template <class TxPacket>
-        void transmit(const TxPacket& packet);
-
-    private:
-
-        UsartT* m_usart;
-    };
+private:
+    UsartT* m_usart;
+};
 } // namespace tos
 
 
 // IMPL
 
-namespace tos
-{
-    template<class UsartT>
-    template<class TxPacket>
-    void xbee_s1<UsartT>::transmit(const TxPacket& packet)
-    {
-        xbee::write_to(*m_usart, packet);
-    }
+namespace tos {
+template<class UsartT>
+template<class TxPacket>
+void xbee_s1<UsartT>::transmit(const TxPacket& packet) {
+    xbee::write_to(*m_usart, packet);
+}
 } // namespace tos
