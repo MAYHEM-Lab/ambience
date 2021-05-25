6d78935fec154628663708e6a2b41edc78881561
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 16:44:01 -0700

    [debug] async_lidl_sink

--- src/core/include/tos/debug/sinks/lidl_sink.hpp
+++ src/core/include/tos/debug/sinks/lidl_sink.hpp
@@ -2,7 +2,7 @@
 
 #include <log_generated.hpp>
 #include <tos/debug/sinks/sink.hpp>
-#include <tos/mutex.hpp>
+#include <tos/debug/sinks/async_sink.hpp>
 
 namespace tos::debug {
 class log_server : public services::logger::sync_server {
@@ -41,7 +41,26 @@ public:
     void end() override;
 
 private:
-    mutex m_prot;
     services::logger::sync_server* m_logger;
+};
+
+class async_lidl_sink : public detail::any_async_sink {
+public:
+    explicit async_lidl_sink(services::logger::async_server& log)
+        : m_logger{&log} {
+    }
+
+    tos::Task<bool> begin(log_level level) override;
+    tos::Task<void> add(int64_t i) override;
+    tos::Task<void> add(std::string_view str) override;
+    tos::Task<void> add(span<const uint8_t> buf) override;
+    tos::Task<void> add(bool b) override;
+    tos::Task<void> add(const void* ptr) override;
+    tos::Task<void> add(log_level level) override;
+    tos::Task<void> add(double d) override;
+    tos::Task<void> end() override;
+
+private:
+    services::logger::async_server* m_logger;
 };
 } // namespace tos::debug
