10fd765a7cc215d99a92db310729709a48d5d59e
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 16:43:41 -0700

    [ambience] virtio_blk and stm32_flash services implement the new interfaces

--- ambience/services/impls/stm32_flash_serv/impl.cpp
+++ ambience/services/impls/stm32_flash_serv/impl.cpp
@@ -4,18 +4,40 @@
 namespace {
 using namespace tos::stm32;
 
-struct impl : tos::ae::services::block_memory::sync_server {
+struct impl final : tos::ae::services::block_memory::sync_server {
     int32_t get_block_size() override {
         return m_flash.sector_size_bytes();
     }
+
     int32_t get_block_count() override {
         return m_flash.number_of_sectors();
+    }
+
+    tos::span<uint8_t> read(const int32_t& block,
+                            const int32_t& offset,
+                            const int32_t& len,
+                            ::lidl::message_builder& response_builder) override {
+        auto start = response_builder.allocate(len, 1);
+        auto res = tos::span<uint8_t>(start, len);
+        m_flash.read(block, res, offset);
+        return res;
+    }
+
+    bool
+    write(const int32_t& block, const int32_t& offset, tos::span<uint8_t> data) override {
+        m_flash.write(block, data, offset);
+        return true;
+    }
+
+    bool erase(const int32_t& block) override {
+        m_flash.erase(block);
+        return true;
     }
 
     flash m_flash;
 };
 } // namespace
 
-std::unique_ptr<tos::ae::services::block_memory::sync_server> init() {
-    return std::make_unique<impl>();
+tos::ae::services::block_memory::sync_server* init_stm32_flash_serv() {
+    return new impl;
 }
--- ambience/services/impls/virtio_blk_serv/impl.cpp
+++ ambience/services/impls/virtio_blk_serv/impl.cpp
@@ -1,33 +1,74 @@
 #include <block_memory_generated.hpp>
+#include <stdio.h>
 #include <tos/virtio/block_device.hpp>
 
 namespace {
-struct impl final : public tos::ae::services::block_memory::async_server {
+struct impl final : public tos::ae::services::block_memory::sync_server {
     explicit impl(tos::virtio::block_device* dev)
         : m_blk_dev(dev) {
     }
 
-    tos::Task<int32_t> get_block_size() override {
-        co_return m_blk_dev->sector_size_bytes();
+    int32_t get_block_size() override {
+        return m_blk_dev->sector_size_bytes();
     }
 
-    tos::Task<int32_t> get_block_count() override {
-        co_return m_blk_dev->number_of_sectors();
+    int32_t get_block_count() override {
+        return m_blk_dev->number_of_sectors();
     }
 
-    tos::Task<tos::span<uint8_t>> read(const int32_t& block,
-                                       const int32_t& offset,
-                                       lidl::message_builder& response_builder) override {
-        auto& res = lidl::create_vector_sized<uint8_t>(response_builder, m_blk_dev->sector_size_bytes() - offset);
-        co_await m_blk_dev->async_read(block, res, offset);
-        co_return tos::span<uint8_t>(res);
+    tos::span<uint8_t> read(const int32_t& block,
+                            const int32_t& offset,
+                            const int32_t& len,
+                            lidl::message_builder& response_builder) override {
+        auto start = response_builder.allocate(len, 1);
+        auto res = tos::span<uint8_t>(start, len);
+        printf(
+            "read %d bytes to %p at offset %d from block %d", len, start, offset, block);
+
+        if (len != get_block_size()) {
+            std::vector<uint8_t> buffer(get_block_size());
+            m_blk_dev->read(block, buffer, 0);
+            tos::safe_span_copy(res, tos::span<const uint8_t>(buffer).slice(offset, len));
+            return res;
+        }
+
+        m_blk_dev->read(block, res, offset);
+        return res;
+    }
+
+    bool erase(const int32_t& block) override {
+        std::vector<uint8_t> buffer(get_block_size(), 0xff);
+        return write(block, 0, buffer);
+    }
+
+    bool
+    write(const int32_t& block, const int32_t& offset, tos::span<uint8_t> data) override {
+        printf("write %d bytes from %p at offset %d to block %d",
+               int(data.size()),
+               data.data(),
+               offset,
+               block);
+
+        if (data.size() != get_block_size()) {
+            std::vector<uint8_t> buffer(get_block_size());
+            m_blk_dev->read(block, buffer, 0);
+            tos::safe_span_copy(tos::span(buffer).slice(offset, data.size()),
+                                tos::span<const uint8_t>(data));
+            return write(block, 0, buffer);
+        }
+
+        if (!m_blk_dev->write(block, data, offset)) {
+            return false;
+        }
+
+        return true;
     }
 
     tos::virtio::block_device* m_blk_dev;
 };
 } // namespace
 
-tos::ae::services::block_memory::async_server*
-init_virtio_blk(tos::virtio::block_device* dev) {
+tos::ae::services::block_memory::sync_server*
+init_virtio_blk(tos::virtio::block_device* dev){
     return new impl(dev);
 }
