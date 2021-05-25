275b04def147fa9b22a04741ffcfee25da3910e1
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 16:50:04 -0700

    [ambience] Exposed services might be async as well

--- ambience/kernel_space/include/tos/ae/kernel/user_group.hpp
+++ ambience/kernel_space/include/tos/ae/kernel/user_group.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <memory>
+#include <nonstd/variant.hpp>
 #include <tos/ae/kernel/rings.hpp>
 #include <tos/ae/service_host.hpp>
 #include <tos/context.hpp>
@@ -11,6 +13,9 @@ struct user_group {
     kernel_interface iface;
     kern::tcb* state;
     std::vector<std::unique_ptr<lidl::service_base>> channels;
-    std::vector<tos::ae::sync_service_host> exposed_services;
+    std::vector<mpark::variant<mpark::monostate,
+                               tos::ae::sync_service_host,
+                               tos::ae::async_service_host>>
+        exposed_services;
 };
 } // namespace tos::ae::kernel
