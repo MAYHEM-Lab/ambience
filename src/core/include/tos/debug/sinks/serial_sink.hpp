948c3a0725305a6106bed6c001886b6247926bab
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 16:44:18 -0700

    [debug] Deref the serial before using

--- src/core/include/tos/debug/sinks/serial_sink.hpp
+++ src/core/include/tos/debug/sinks/serial_sink.hpp
@@ -9,7 +9,7 @@
 
 namespace tos::debug {
 template<class SerialT>
-class serial_sink
+class serial_sink final
     : public detail::any_sink
     , public self_pointing<serial_sink<SerialT>> {
 public:
@@ -20,48 +20,48 @@ public:
 
     bool begin(log_level level) override {
         if (!platform::interrupts_disabled()) {
-            m_prot->lock();
+//            m_prot->lock();
         }
         if (!m_tag.empty()) {
-            tos::print(m_serial, "[", m_tag, "] ", tos::no_separator());
+            tos::print(*m_serial, "[", m_tag, "] ", tos::no_separator());
         }
-        tos::print(m_serial, "[", level, "] [", seq++, "] ", tos::no_separator());
+        tos::print(*m_serial, "[", level, "] [", seq++, "] ", tos::no_separator());
         return true;
     }
 
     void add(int64_t i) override {
-        tos::print(m_serial, i, "");
+        tos::print(*m_serial, i, "");
     }
 
     void add(std::string_view str) override {
-        tos::print(m_serial, str, "");
+        tos::print(*m_serial, str, "");
     }
 
     void add(bool b) override {
-        tos::print(m_serial, b, "");
+        tos::print(*m_serial, b, "");
     }
 
     void add(const void* ptr) override {
-        tos::print(m_serial, ptr, "");
+        tos::print(*m_serial, ptr, "");
     }
 
     void add(log_level level) override {
-        tos::print(m_serial, "[", level, "]", tos::no_separator());
-        tos::print(m_serial, " ");
+        tos::print(*m_serial, "[", level, "]", tos::no_separator());
+        tos::print(*m_serial, " ");
     }
 
     void add(span<const uint8_t> buf) override {
-        tos::print(m_serial, buf, "");
+        tos::print(*m_serial, buf, "");
     }
 
     void add(double d) override {
-        tos::print(m_serial, d, "");
+        tos::print(*m_serial, d, "");
     }
 
     void end() override {
-        tos::println(m_serial);
+        tos::println(*m_serial);
         if (!platform::interrupts_disabled()) {
-            m_prot->unlock();
+//            m_prot->unlock();
         }
     }
 
