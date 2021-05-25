30d7607abb0c1f6f1f8f17d4f330ccac91aaaad0
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 16:48:48 -0700

    [ambience] stm32 implements the common platform support

--- ambience/kernel_space/platform/stm32.cpp
+++ ambience/kernel_space/platform/stm32.cpp
@@ -1,11 +1,17 @@
+#include "kernel.hpp"
+#include <alarm_generated.hpp>
+#include <block_memory_generated.hpp>
 #include <calc_generated.hpp>
 #include <nonstd/variant.hpp>
 #include <tos/address_space.hpp>
 #include <tos/ae/kernel/user_group.hpp>
+#include <tos/ae/transport/downcall.hpp>
 #include <tos/arm/exception.hpp>
 #include <tos/board.hpp>
 #include <tos/debug/dynamic_log.hpp>
+#include <tos/debug/sinks/lidl_sink.hpp>
 #include <tos/debug/sinks/serial_sink.hpp>
+#include <tos/detail/tos_bind.hpp>
 #include <tos/expected.hpp>
 #include <tos/ft.hpp>
 #include <tos/interrupt_trampoline.hpp>
@@ -28,7 +34,6 @@ public:
     }
 };
 
-alignas(4) uint8_t stk[1024];
 expected<tos::ae::kernel::user_group, errors> start_group(
     tos::span<uint8_t> stack, void (*entry)(), tos::interrupt_trampoline& trampoline) {
     auto& self = *tos::self();
@@ -60,9 +65,6 @@ expected<tos::ae::kernel::user_group, errors> start_group(
         tos::arm::exception::set_svc_handler(
             tos::arm::exception::svc_handler_t(task_svc_handler));
 
-        //        LOG("MPU setup complete, allowing stack & flash access");
-
-        //        LOG("Switching to unprivileged mode");
         tos::arm::set_control(1);
         tos::arm::isb();
     };
@@ -77,76 +79,112 @@ expected<tos::ae::kernel::user_group, errors> start_group(
     return res;
 }
 
-expected<void, errors> kernel() {
-    auto log_backend = bs::default_com::open();
-    tos::println(log_backend, "ambience");
-
-    tos::debug::serial_sink sink(&log_backend);
-    tos::debug::detail::any_logger logger(&sink);
-    tos::debug::set_default_log(&logger);
-
-    LOG("Log setup complete");
-
-    auto timer = tos::open(tos::devs::timer<2>);
-    LOG("Timer setup complete");
-
-    svc_on_demand_interrupt odi{};
-    auto trampoline = tos::make_interrupt_trampoline(odi);
-
-    LOG("Trampoline setup complete");
-
-    std::vector<tos::ae::kernel::user_group> runnable_groups;
-
-    runnable_groups.push_back(force_get(
-        start_group(stk, reinterpret_cast<void (*)()>(0x8020d79), *trampoline)));
-
-    LOG("Group started");
-    int32_t x = 3, y = 42;
-    auto params = std::make_tuple(&x, &y);
-    auto results = tos::ae::services::calculator::wire_types::add_results{-1};
-
-    auto& req1 = tos::ae::submit_req<true>(
-        *runnable_groups.front().iface.user_iface, 0, 0, &params, &results);
-
-    auto& self = *tos::self();
-
-    auto syshandler2 = [&](int svc, tos::arm::exception::stack_frame_t& frame) {
-        tos::arm::set_control(0);
-        tos::arm::isb();
-
-        tos::swap_context(*runnable_groups.front().state, self, tos::int_ctx{});
-    };
-
-    bool preempted = false;
-    auto preempt = [&] {
-        tos::arm::set_control(0);
-        tos::arm::isb();
-
-        preempted = true;
-        tos::swap_context(*runnable_groups.front().state, self, tos::int_ctx{});
-    };
-    timer.set_callback(tos::function_ref<void()>(preempt));
-    timer.set_frequency(10);
-
-    for (int i = 0; i < 10; ++i) {
-        LOG("back", results.ret0(), "Preempted", preempted);
-        proc_req_queue([](auto& req, auto done) { done(); },
-                       runnable_groups.front().iface);
-        tos::this_thread::yield();
-
-        odi([&](auto...) {
-            tos::arm::exception::set_svc_handler(
-                tos::arm::exception::svc_handler_t(syshandler2));
-
-            preempted = false;
-            timer.enable();
-            tos::swap_context(self, *runnable_groups.front().state, tos::int_ctx{});
-            timer.disable();
-        });
-        // std::swap(runnable_groups.front(), runnable_groups.back());
+alignas(4) uint8_t stk[2048];
+class stm32_platform_support {
+public:
+    void stage1_init() {
+    }
+    void stage2_init() {
+    }
+    auto init_serial() {
+        return bs::default_com::open();
     }
 
-    return {};
+    auto init_timer() {
+        return tos::open(tos::devs::timer<2>);
+    }
+
+    auto init_odi() {
+        return svc_on_demand_interrupt();
+    }
+
+    tos::physical_page_allocator* init_physical_memory_allocator() {
+        return nullptr;
+    }
+
+    std::vector<tos::ae::kernel::user_group>
+    init_groups(tos::interrupt_trampoline& trampoline,
+                tos::physical_page_allocator& palloc) {
+        std::vector<tos::ae::kernel::user_group> runnable_groups;
+
+        runnable_groups.push_back(force_get(
+            start_group(stk, reinterpret_cast<void (*)()>(0x8021129), trampoline)));
+
+        runnable_groups.back().channels.push_back(
+            std::make_unique<tos::ae::services::calculator::async_zerocopy_client<
+                tos::ae::downcall_transport>>(*runnable_groups.back().iface.user_iface,
+                                              0));
+
+        return runnable_groups;
+    }
+
+    template<class FnT>
+    void set_syscall_handler(FnT&& syshandler) {
+        tos::arm::exception::set_svc_handler(
+            tos::arm::exception::svc_handler_t(syshandler));
+    }
+
+    void return_to_thread_from_irq(tos::kern::tcb& from, tos::kern::tcb& to) {
+        tos::swap_context(from, to, tos::int_ctx{});
+    }
+};
+
+template<class BaseAlarm>
+struct async_alarm_impl : tos::ae::services::alarm::async_server {
+    template<class... ArgTs>
+    async_alarm_impl(ArgTs&&... args)
+        : alarm(std::forward<ArgTs>(args)...) {
+    }
+
+    tos::Task<bool> sleep_for(tos::ae::services::milliseconds dur) override {
+        co_await tos::async_sleep_for(alarm, std::chrono::milliseconds(dur.count()));
+        co_return true;
+    }
+
+    BaseAlarm alarm;
+};
+
+tos::ae::services::block_memory::sync_server* init_stm32_flash_serv();
+expected<void, errors> kernel() {
+    tos::ae::manager<stm32_platform_support> man;
+    man.initialize();
+
+    auto blk_mem = init_stm32_flash_serv();
+
+    auto buf = std::vector<uint8_t>(512);
+    lidl::message_builder builder{buf};
+    auto res = blk_mem->read(0, 0, 512, builder);
+    LOG(res);
+
+    auto tim = tos::open(tos::devs::timer<3>);
+    auto alarm = tos::alarm(&tim);
+
+    async_alarm_impl<decltype(&alarm)> async_alarm{&alarm};
+
+    tos::debug::log_server serv(man.get_log_sink());
+
+    man.groups().front().exposed_services.emplace_back(tos::ae::sync_service_host(&serv));
+    man.groups().front().exposed_services.emplace_back(
+        tos::ae::async_service_host(&async_alarm));
+
+    auto& g = man.groups().front();
+
+    auto req_task = [&g = man.groups().front(), &async_alarm]() -> tos::Task<void> {
+        auto serv = static_cast<tos::ae::services::calculator::async_server*>(
+            g.channels.front().get());
+
+        auto res = co_await serv->add(3, 4);
+
+        tos::launch(tos::alloc_stack, [res] { LOG("3 + 4 =", res); });
+    };
+
+    tos::coro::make_detached(req_task());
+
+    for (int i = 0; i < 1000000; ++i) {
+        man.run();
+    }
+
+    tos::this_thread::block_forever();
 }
 
 static tos::stack_storage kern_stack;
