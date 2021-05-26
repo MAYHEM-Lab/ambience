b2cf9063198da353bd9eab16e32a7b45c5c7abef
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-26 00:40:21 -0700

    [ambience] Remove log_str

--- ambience/user_space/user_space.cpp
+++ ambience/user_space/user_space.cpp
@@ -6,10 +6,6 @@ extern tos::ae::interface iface;
 tos::Task<bool> handle_req(tos::ae::req_elem el);
 
 namespace tos::ae {
-tos::Task<void> log_str(std::string_view sv) {
-    co_await tos::ae::submit_req<false>(iface, 0, 1, &sv, nullptr);
-}
-
 void proc_res_queue(interface& iface) {
     iface.res_last_seen = for_each(
         iface, *iface.host_to_guest, iface.res_last_seen, iface.size, [&](ring_elem& elem) {
