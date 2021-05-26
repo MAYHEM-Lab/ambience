ac85000895d23ad3d6c6cf4bc21ad6cd6a0dbd3c
Fatih Bakir <mfatihbakir@gmail.com>
2021-05-25 17:00:07 -0700

    [ambience] Auto-generated group init

--- ambience/user_space/sample.cpp
+++ ambience/user_space/sample.cpp
@@ -1,4 +1,6 @@
+#include <alarm_generated.hpp>
 #include <calc_generated.hpp>
+#include <file_system_generated.hpp>
 #include <log_generated.hpp>
 #include <tos/ae/group.hpp>
 #include <tos/ae/transport/upcall.hpp>
@@ -18,11 +20,16 @@ tos::Task<bool> handle_req(tos::ae::req_elem el) {
     return tos::ae::run_req(*g, el);
 }
 
-tos::Task<tos::ae::services::calculator::async_server*>
-init_basic_calc(tos::services::logger::async_server* logger);
+auto init_basic_calc(tos::services::logger::async_server* logger,
+                     tos::ae::services::alarm::async_server* alarm,
+                     tos::ae::services::filesystem::async_server* fs)
+    -> tos::Task<tos::ae::services::calculator::async_server*>;
 tos::Task<void> task() {
-    auto log_client = transport.get_service<tos::services::logger, 1>();
+    auto ext_dep1 = transport.get_service<tos::ae::services::filesystem, 3>();
+    auto ext_dep2 = transport.get_service<tos::ae::services::alarm, 2>();
+    auto ext_dep3 = transport.get_service<tos::services::logger, 1>();
 
-    ::g = new tos::ae::group<1>(
-        tos::ae::group<1>::make(co_await init_basic_calc(log_client)));
+    auto calc = co_await init_basic_calc(ext_dep3, ext_dep2, ext_dep1);
+
+    ::g = new tos::ae::group<1>(tos::ae::group<1>::make(calc));
 }
