3af5b07660a204f30b647e05f66359396f83e779
Fatih Bakir <mfatihbakir@gmail.com>
2021-06-29 20:59:24 -0700

    [ambience] Page allocator init logs

--- ambience/kernel_space/platform/x86_64/page_allocator.cpp
+++ ambience/kernel_space/platform/x86_64/page_allocator.cpp
@@ -4,6 +4,8 @@
 
 tos::expected<tos::physical_page_allocator*, tos::cur_arch::mmu_errors>
 initialize_page_allocator() {
+    auto& root_table = tos::cur_arch::get_current_translation_table();
+
     constexpr auto page_num = 2048;
     auto vmem_end = (void*)tos::default_segments::image().end();
 
@@ -17,18 +19,15 @@ initialize_page_allocator() {
     auto allocator_segment =
         tos::segment{tos::memory_range{uintptr_t(vmem_end), ptrdiff_t(allocator_space)},
                      tos::permissions::read_write};
+    LOG("Call allocate", allocator_space, "bytes");
 
-    EXPECTED_TRYV(
-        tos::cur_arch::allocate_region(tos::cur_arch::get_current_translation_table(),
-                                       allocator_segment,
-                                       tos::user_accessible::no,
-                                       nullptr));
+    EXPECTED_TRYV(tos::cur_arch::allocate_region(
+        root_table, allocator_segment, tos::user_accessible::no, nullptr));
+    LOG("Allocated", allocator_space, "bytes");
 
-    EXPECTED_TRYV(
-        tos::cur_arch::mark_resident(tos::cur_arch::get_current_translation_table(),
-                                     allocator_segment.range,
-                                     tos::memory_types::normal,
-                                     vmem_end));
+    EXPECTED_TRYV(tos::cur_arch::mark_resident(
+        root_table, allocator_segment.range, tos::memory_types::normal, vmem_end));
+    LOG("Marked resident", allocator_space, "bytes");
 
     auto palloc = new (vmem_end) tos::physical_page_allocator(page_num);
 
