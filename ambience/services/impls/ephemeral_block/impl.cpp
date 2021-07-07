5f9992902b8bfa6ad8de89395ce81f1e8c3530c0
Fatih Bakir <mfatihbakir@gmail.com>
2021-07-07 14:04:21 -0700

    [ambience] buffered_write for async_ephemeral_block

--- ambience/services/impls/ephemeral_block/impl.cpp
+++ ambience/services/impls/ephemeral_block/impl.cpp
@@ -85,6 +85,12 @@ public:
         co_return true;
     }
 
+    tos::Task<bool> buffered_write(const int32_t& block,
+                                   const int32_t& offset,
+                                   tos::span<uint8_t> data) override {
+        return write(block, offset, data);
+    }
+
     tos::span<uint8_t> span() {
         return m_data;
     }
