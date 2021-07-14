7ae2e5edcdd97a85454ce2eb7354c77d9776badb
Fatih Bakir <mfatihbakir@gmail.com>
2021-07-14 11:43:08 -0700

    [ambictl] Need to create the deployment dir

--- ambience/ambictl/build.py
+++ ambience/ambictl/build.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     dir = sys.argv[1]
     load_dir(dir)
     deployment = ambidecl._finalize()
+    mkbuild.create_build_root("/tmp/mydeployment", "/home/fatih/tos")
     mkbuild.build_all("/tmp/mydeployment", deployment)
