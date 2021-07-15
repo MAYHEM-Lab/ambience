71c7193559162ad6ff8d11aa9685cec6d9521fe7
Fatih Bakir <mfatihbakir@gmail.com>
2021-07-15 12:20:27 -0700

    [boards] Enable uart by default on digital ocean for debugging

--- config/boards/digitalocean_vm/board.hpp
+++ config/boards/digitalocean_vm/board.hpp
@@ -22,9 +22,10 @@ struct digitalocean_vm_spec {
                 ensure(tos::platform::take_irq(1));
                 tos::platform::set_irq(1, tos::free_function_ref(+kb_isr));
             }*/
-//            return force_get(tos::x86_64::uart_16550::open());
 
-            return scrollable{};
+            return force_get(tos::x86_64::uart_16550::open());
+
+//            return scrollable{};
         }
     };
 
