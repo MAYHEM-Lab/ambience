a60682c426deaa90bd1ec1177b17502e38970d4a
Fatih Bakir <mfatihbakir@gmail.com>
2021-07-15 12:25:34 -0700

    [ambience] User space has a proper logger

--- ambience/user_space/entry.cpp
+++ ambience/user_space/entry.cpp
@@ -2,6 +2,9 @@
 #include <memory>
 #include <tos/ae/detail/syscall.hpp>
 #include <tos/ae/user_space.hpp>
+#include <tos/debug/detail/logger_base.hpp>
+#include <tos/debug/log.hpp>
+#include <tos/debug/sinks/serial_sink.hpp>
 #include <tos/detail/poll.hpp>
 #include <tos/memory.hpp>
 
@@ -24,6 +27,14 @@ void __cxa_atexit() {
 }
 }
 
+tos::debug::detail::any_logger* g_logger;
+
+namespace tos::debug {
+detail::any_logger& default_log() {
+    return *g_logger;
+}
+}
+
 [[gnu::used, noreturn]] extern "C" void _user_code() {
 //    auto data = tos::default_segments::data();
 //    auto data_start = reinterpret_cast<uint64_t*>(data.base);
@@ -35,11 +46,18 @@ void __cxa_atexit() {
     auto bss_start = reinterpret_cast<uint64_t*>(bss.base);
     auto bss_end = reinterpret_cast<uint64_t*>(bss.end());
 
+    std::uninitialized_value_construct_n(iface.elems, iface.size);
+
     // Zero out BSS
     std::fill(bss_start, bss_end, 0);
 
     // Call constructors
     std::for_each(start_ctors, end_ctors, [](auto x) { x(); });
+
+    tos::debug::serial_sink sink(&tos::ae::low_level_output);
+    tos::debug::detail::any_logger logger(&sink);
+    g_logger = &logger;
+    tos::debug::log("Call init syscall");
 
     tos::ae::detail::do_init_syscall(iface);
 
