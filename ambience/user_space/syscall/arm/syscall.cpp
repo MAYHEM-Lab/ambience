17e00c0c06e5a149a7fdfd006ea7da3b68e449be
Fatih Bakir <mfatihbakir@gmail.com>
2021-07-16 12:22:30 -0700

    [ambience] Dummy low_level_output on arm

--- ambience/user_space/syscall/arm/syscall.cpp
+++ ambience/user_space/syscall/arm/syscall.cpp
@@ -1,6 +1,12 @@
 #include <tos/ae/detail/syscall.hpp>
 #include <tos/arm/assembly.hpp>
 
+namespace tos::ae {
+int low_level_output_t::write(tos::span<const uint8_t> data) {
+    return data.size();
+}
+} // namespace tos::ae
+
 namespace tos::ae::detail {
 namespace {
 int sysreq(int num, uintptr_t data);
@@ -8,11 +14,11 @@ int sysreq(int num, uintptr_t data);
 [[gnu::naked]] int sysreq(int num, uintptr_t data) {
     asm volatile("svc #0x6F \n"
                  "bx lr"
-    :
-    :
-    : "memory");
+                 :
+                 :
+                 : "memory");
 }
-}
+} // namespace
 
 void do_init_syscall(interface& iface) {
     sysreq(1, reinterpret_cast<uintptr_t>(&iface));
