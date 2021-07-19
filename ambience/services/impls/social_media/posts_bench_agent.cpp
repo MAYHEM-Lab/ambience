3fef3ae201c79cd172d1e2148eaf48706ab51631
Fatih Bakir <mfatihbakir@gmail.com>
2021-07-19 14:32:43 -0700

    [ambience] Experiment now has multiple hops

--- ambience/services/impls/social_media/posts_bench_agent.cpp
+++ ambience/services/impls/social_media/posts_bench_agent.cpp
@@ -1,21 +1,43 @@
 #include <agent_generated.hpp>
 #include <posts_generated.hpp>
+#include <tos/coro/countdown.hpp>
+#include <tos/debug/log.hpp>
+#include <tos/detail/poll.hpp>
 
 namespace social_media {
 namespace {
 struct posts_bench_agent : tos::ae::agent::async_server {
+    posts_bench_agent(social_media::posts::async_server* posts_serv)
+        : m_posts_serv{posts_serv} {
+    }
+
 
     tos::Task<bool> start() override {
-//        co_await m_posts_serv->send_post("foobar", "Hello!");
+        constexpr auto extent = 100'000;
+        constexpr auto concurrency = 10;
+        tos::coro::countdown cd{concurrency};
+
+        co_await cd.start([&] {
+            for (int c = 0; c < concurrency; ++c) {
+                tos::coro::make_detached([c, this, &cd]() -> tos::Task<void> {
+                    for (int i = c * (extent / concurrency);
+                         i < (c + 1) * (extent / concurrency);
+                         ++i) {
+                        auto id = co_await m_posts_serv->send_post("foobar", "Hello @bulut #meow!");
+                    }
+                    co_await cd.signal();
+                });
+            }
+        });
         co_return true;
     }
 
-//    social_media::posts::async_server* m_posts_serv;
+    social_media::posts::async_server* m_posts_serv;
 };
 } // namespace
 } // namespace social_media
 
 tos::Task<tos::ae::agent::async_server*>
 init_posts_bench_agent(social_media::posts::async_server* posts_serv) {
-    co_return new social_media::posts_bench_agent();
+    co_return new social_media::posts_bench_agent(posts_serv);
 }
--- ambience/services/impls/social_media/posts_manager.cpp
+++ ambience/services/impls/social_media/posts_manager.cpp
@@ -1,5 +1,6 @@
 #include <analysis_generated.hpp>
 #include <posts_generated.hpp>
+#include <tos/coro/when_all.hpp>
 #include <unordered_map>
 
 namespace social_media {
@@ -18,13 +19,34 @@ struct posts_manager : posts::async_server {
             lidl::create_string(response_builder, post.body),
             lidl::create_string(response_builder, post.user),
             0,
-            lidl::create_vector<common::int_range>(response_builder),
-            lidl::create_vector<common::int_range>(response_builder));
+            lidl::create_vector<common::int_range>(response_builder, post.hashtags),
+            lidl::create_vector<common::int_range>(response_builder, post.mentions));
     }
 
     tos::Task<uint64_t> send_post(std::string_view user, std::string_view body) override {
-        auto res = m_posts.emplace(
-            m_next++, post_data{.user = std::string(user), .body = std::string(body)});
+        if (m_posts.size() > 1000) {
+            m_posts.erase(m_posts.begin());
+        }
+
+        std::array<uint8_t, 1024> hashtags_response;
+        lidl::message_builder hashtags_response_builder{hashtags_response};
+
+        std::array<uint8_t, 1024> mentions_response;
+        lidl::message_builder mentions_response_builder{mentions_response};
+
+        auto&& [hashtags, mentions] = co_await tos::coro::when_all(
+            m_analysis->get_hashtags(body, hashtags_response_builder),
+            m_analysis->get_mentions(body, mentions_response_builder));
+
+//        tos::debug::trace(hashtags.size(), mentions.size());
+
+        auto res = m_posts.emplace(m_next++,
+                                   post_data{.user = std::string(user),
+                                             .body = std::string(body),
+                                             .hashtags = std::vector<common::int_range>(
+                                                 hashtags.begin(), hashtags.end()),
+                                             .mentions = std::vector<common::int_range>(
+                                                 mentions.begin(), mentions.end())});
         co_return res.first->first;
     }
 
@@ -32,6 +54,8 @@ struct posts_manager : posts::async_server {
     struct post_data {
         std::string user;
         std::string body;
+        std::vector<common::int_range> hashtags;
+        std::vector<common::int_range> mentions;
     };
 
     uint64_t m_next;
