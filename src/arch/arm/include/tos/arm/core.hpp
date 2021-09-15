876642099e66c07a8f1ca5828c432d41fad1dc76
Fatih Bakir <mfatihbakir@gmail.com>
2021-09-15 11:10:35 -0700

    [arm] LAR register

--- src/arch/arm/include/tos/arm/core.hpp
+++ src/arch/arm/include/tos/arm/core.hpp
@@ -19,6 +19,7 @@ inline bool in_handler_mode() {
 struct DWT {
     static constexpr mmio::reg<0xE0001000, uint32_t> CONTROL{};
     static constexpr mmio::reg<0xE0001004, uint32_t> CYCCNT{};
+    static constexpr mmio::reg<0xE0001FB0, uint32_t> LAR{};
 };
 
 struct SCB {
