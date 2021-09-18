ceca828b58570d3b49edb5a0f7904d94b1daf98f
Fatih Bakir <mfatihbakir@gmail.com>
2021-09-18 10:45:51 -0700

    [ambience] Test the rings in a multithreaded setting

--- ambience/tests/ring_tests.cpp
+++ ambience/tests/ring_tests.cpp
@@ -1,5 +1,8 @@
 #include <doctest.h>
+#include <iostream>
+#include <thread>
 #include <tos/ae/rings.hpp>
+#include <tos/debug/debug.hpp>
 
 namespace tos::ae {
 namespace {
@@ -45,6 +48,40 @@ TEST_CASE("User submission works") {
         });
 
     REQUIRE_EQ(1, req_last_seen);
+}
+
+TEST_CASE("Multithreading") {
+    auto storage = new tos::ae::interface_storage<128>;
+    auto iface = storage->make_interface();
+
+    std::atomic<bool> run = true;
+    std::atomic<int> x = 0;
+
+    std::thread host([&] {
+        auto last_seen = 0;
+        while (run) {
+            last_seen = for_each(iface, *iface.guest_to_host, last_seen, [](ring_elem& el) {
+                tos::debug::do_not_optimize(&el);
+            });
+        }
+    });
+
+    std::thread guest([&] {
+        int i = 0;
+        while (run) {
+            auto& req = submit_req<false>(iface, i++, 0, nullptr, nullptr);
+            std::this_thread::sleep_for(std::chrono::nanoseconds(425));
+        }
+    });
+
+    std::this_thread::sleep_for(std::chrono::seconds(20));
+    run = false;
+
+    host.join();
+    guest.join();
+
+    delete storage;
+    std::cerr << x << '\n';
 }
 } // namespace
 } // namespace tos::ae
