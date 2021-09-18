052b17b48f00d4f5068a271d22f3326222812d03
Fatih Bakir <mfatihbakir@gmail.com>
2021-09-18 11:44:01 -0700

    [ubsan] Wrong prototypes apparently

--- src/sanitizer/ubsan/handlers.cpp
+++ src/sanitizer/ubsan/handlers.cpp
@@ -2,13 +2,13 @@
 #include <cstdint>
 
 extern "C" {
-void __ubsan_handle_nonnull_arg() {
+void __ubsan_handle_nonnull_arg(void*) {
     tos::ubsan::handlers::handlers->error();
 }
-void __ubsan_handle_load_invalid_value(int32_t, int32_t) {
+void __ubsan_handle_load_invalid_value(void*, void*) {
     tos::ubsan::handlers::handlers->error();
 }
-void __ubsan_handle_type_mismatch_v1() {
+void __ubsan_handle_type_mismatch_v1(void*, void*) {
     tos::ubsan::handlers::handlers->error();
 }
 }
