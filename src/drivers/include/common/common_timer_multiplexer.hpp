e4a3bf5a73225bec995bb85422e2440b0a521094
Fatih Bakir <mfatihbakir@gmail.com>
2021-09-29 14:06:30 -0700

    [timer] Add an auxiliary timer

--- src/drivers/include/common/common_timer_multiplexer.hpp
+++ src/drivers/include/common/common_timer_multiplexer.hpp
@@ -5,7 +5,7 @@
 #include <common/clock.hpp>
 
 namespace tos {
-template<class BaseTimerT, size_t Count = 3>
+template<class BaseTimerT, size_t Count = 4>
 struct common_timer_multiplex {
     using timer_mux_type = tos::timer_multiplexer<BaseTimerT, Count>;
     using channel_type = typename timer_mux_type::multiplexed_timer;
@@ -19,6 +19,7 @@ struct common_timer_multiplex {
     channel_type timer{tim_mux.channel(0)};
     erased_clock_type clock{tim_mux.channel(1)};
     erased_alarm_type alarm{tim_mux.channel(2)};
+    channel_type aux_timer{tim_mux.channel(3)};
 
     template<class... Ts>
     common_timer_multiplex(Ts&&... args)
