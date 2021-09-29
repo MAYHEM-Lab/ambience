51d644a4d8ee34d8ea06fbac9e1c518d3e225fae
Fatih Bakir <mfatihbakir@gmail.com>
2021-09-29 16:55:39 -0700

    [util] maybe_constructed

--- src/util/include/tos/late_constructed.hpp
+++ src/util/include/tos/late_constructed.hpp
@@ -4,6 +4,86 @@
 #include <memory>
 
 namespace tos {
+template<class T, bool = std::is_reference_v<T>>
+struct maybe_constructed {
+    constexpr maybe_constructed() {
+    }
+    ~maybe_constructed() {
+    }
+
+    void destroy() {
+        std::destroy_at(&t);
+    }
+
+    template<class FnT, class... ArgTs>
+    constexpr void emplace_fn(FnT&& fn, ArgTs&&... args) {
+        new (&t) T(std::invoke(fn, std::forward<ArgTs>(args)...));
+    }
+
+    template<class... ArgTs>
+    constexpr void emplace(ArgTs&&... args) {
+        new (&t) T(std::forward<ArgTs>(args)...);
+    }
+
+    constexpr operator T&() {
+        return t;
+    }
+
+    constexpr operator const T&() const {
+        return t;
+    }
+
+    constexpr T& get() {
+        return t;
+    }
+
+    constexpr const T& get() const {
+        return t;
+    }
+
+private:
+    union {
+        T t;
+    };
+};
+
+template<class T>
+struct maybe_constructed<T, true> {
+private:
+    using type = std::remove_reference_t<T>;
+
+public:
+    constexpr void destroy() {
+    }
+
+    template<class FnT, class... ArgTs>
+    constexpr void emplace_fn(FnT&& fn, ArgTs&&... args) {
+        emplace(std::invoke(fn, std::forward<ArgTs>(args)...));
+    }
+
+    constexpr void emplace(T arg) {
+        m_internal.emplace(&arg);
+    }
+
+    constexpr operator T() {
+        return static_cast<T>(*m_internal.get());
+    }
+
+    constexpr operator const T() const {
+        return static_cast<T>(*m_internal.get());
+    }
+
+    constexpr T get() {
+        return static_cast<T>(*m_internal.get());
+    }
+
+    constexpr const T get() const {
+        return static_cast<T>(*m_internal.get());
+    }
+
+private:
+    maybe_constructed<type*> m_internal;
+};
 /*
  * This template allows us to postpone the initialization of objects of types without
  * default constructors. For instance, a class member who cannot be initialized in a
@@ -36,79 +116,10 @@ namespace tos {
  *      // no_moves.emplace(func(42)); <- does not compile
  *      no_moves.emplace_fn(func, 42); // workaround
  */
-template<class T, bool = std::is_reference_v<T>>
-struct late_constructed {
-    late_constructed() {
-    }
-
-    ~late_constructed() {
-        std::destroy_at(&t);
-    }
-
-    template<class FnT, class... ArgTs>
-    void emplace_fn(FnT&& fn, ArgTs&&... args) {
-        new (&t) T(std::invoke(fn, std::forward<ArgTs>(args)...));
-    }
-
-    template<class... ArgTs>
-    void emplace(ArgTs&&... args) {
-        new (&t) T(std::forward<ArgTs>(args)...);
-    }
-
-    operator T&() {
-        return t;
-    }
-
-    operator const T&() const {
-        return t;
-    }
-
-    T& get() {
-        return t;
-    }
-
-    const T& get() const {
-        return t;
-    }
-
-private:
-    union {
-        T t;
-    };
-};
-
 template<class T>
-struct late_constructed<T, true> {
-private:
-    using type = std::remove_reference_t<T>;
-
-public:
-    template<class FnT, class... ArgTs>
-    void emplace_fn(FnT&& fn, ArgTs&&... args) {
-        emplace(std::invoke(fn, std::forward<ArgTs>(args)...));
+struct late_constructed : public maybe_constructed<T> {
+    ~late_constructed() {
+        this->destroy();
     }
-
-    void emplace(T arg) {
-        m_internal.emplace(&arg);
-    }
-
-    operator T() {
-        return static_cast<T>(*m_internal.get());
-    }
-
-    operator const T() const {
-        return static_cast<T>(*m_internal.get());
-    }
-
-    T get() {
-        return static_cast<T>(*m_internal.get());
-    }
-
-    const T get() const {
-        return static_cast<T>(*m_internal.get());
-    }
-
-private:
-    late_constructed<type*> m_internal;
 };
 } // namespace tos
