7e99222766e7037b3b34dfbe0ee21163dc0b63f9
Fatih Bakir <mfatihbakir@gmail.com>
2021-10-22 09:41:53 -0700

    [ambience] as_traversal is mostly superseded by quikshare

--- ambience/kernel_space/include/tos/ae/kernel/as_traversal.hpp
+++ ambience/kernel_space/include/tos/ae/kernel/as_traversal.hpp
@@ -7,90 +7,6 @@
 #include <tos/meta/algorithm.hpp>
 
 namespace tos::ae {
-enum class ipc_policy
-{
-    copy,
-    ro_share,
-    rw_share,
-};
-
-struct always_copy_decision {
-    template<class T>
-    static constexpr ipc_policy value(T&) {
-        return ipc_policy::copy;
-    }
-
-    template<class T>
-    static constexpr size_t ipc_copy_size(const T& t) {
-        return sizeof t;
-    }
-};
-
-template<class T>
-struct ipc_policy_decision;
-
-template<>
-struct ipc_policy_decision<uint32_t> : always_copy_decision {};
-
-template<>
-struct ipc_policy_decision<uint64_t> : always_copy_decision {};
-
-template<>
-struct ipc_policy_decision<int32_t> : always_copy_decision {};
-
-template<>
-struct ipc_policy_decision<int64_t> : always_copy_decision {};
-
-template<>
-struct ipc_policy_decision<bool> : always_copy_decision {};
-
-template<>
-struct ipc_policy_decision<std::string_view> {
-    static ipc_policy value(const std::string_view& str) {
-        if (str.size() < 32) {
-            return ipc_policy::copy;
-        }
-        return ipc_policy::ro_share;
-    }
-
-    static constexpr size_t ipc_copy_size(const std::string_view& t) {
-        return t.size();
-    }
-};
-
-template<>
-struct ipc_policy_decision<tos::span<uint8_t>> {
-    static ipc_policy value(const tos::span<uint8_t>& data) {
-        if (data.size() < 32) {
-            return ipc_policy::copy;
-        }
-        return ipc_policy::ro_share;
-    }
-};
-
-template<>
-struct ipc_policy_decision<lidl::message_builder> {
-    static ipc_policy value(const tos::span<uint8_t>& data) {
-        return ipc_policy::rw_share;
-    }
-
-    static constexpr size_t ipc_copy_size(const lidl::message_builder& t) {
-        return 0;
-    }
-};
-
-template<class T>
-constexpr ipc_policy decide_policy(const T& val) {
-    using arg_t = std::remove_const_t<std::remove_reference_t<decltype(val)>>;
-    return ipc_policy_decision<arg_t>::value(val);
-}
-
-template<class T>
-constexpr size_t ipc_copy_size(const T& val) {
-    using arg_t = std::remove_const_t<std::remove_reference_t<decltype(val)>>;
-    return ipc_policy_decision<arg_t>::ipc_copy_size(val);
-}
-
 template<class ServiceT, int ProcId>
 struct procedure_decision {
     using ServDesc = lidl::service_descriptor<ServiceT>;
@@ -98,24 +14,6 @@ struct procedure_decision {
     using ProcTraits = lidl::procedure_traits<decltype(proc_desc.function)>;
     using ArgsTupleType = typename lidl::detail::convert_types<
         typename ProcTraits::param_types>::tuple_type;
-
-    static auto decide_policies(const ArgsTupleType& args_tuple) {
-        return meta::transform(args_tuple,
-                               [](auto* val_ptr) { return decide_policy(*val_ptr); });
-    }
-
-    static size_t ipc_area_size(const ArgsTupleType& args_tuple) {
-        const auto ipc_sizes = meta::transform(
-            args_tuple, [](auto* val_ptr) { return ipc_copy_size(*val_ptr); });
-        const auto copy_size_sum = meta::accumulate(ipc_sizes, std::size_t(0));
-        return copy_size_sum;
-    }
-
-    static size_t erased_ipc_area_size(const void* args_tuple_ptr) {
-        return ipc_area_size(*static_cast<const ArgsTupleType*>(args_tuple_ptr));
-    }
-
-    static ArgsTupleType translate(const ArgsTupleType& args_tuple);
 
     static std::unique_ptr<quik::share_base> do_share(cur_arch::address_space& from_space,
                                                       cur_arch::address_space& to_space,
@@ -128,15 +26,13 @@ struct procedure_decision {
     }
 };
 
-using do_share_fn =
-    std::unique_ptr<quik::share_base> (*)(cur_arch::address_space& from_space,
-                                          cur_arch::address_space& to_space,
-                                          const void*,
-                                          void*);
-using ipc_area_compute_fn = size_t (*)(const void*);
-
 struct sharer_vtbl {
-    ipc_area_compute_fn area_compute;
+    using do_share_fn =
+        std::unique_ptr<quik::share_base> (*)(cur_arch::address_space& from_space,
+                                              cur_arch::address_space& to_space,
+                                              const void*,
+                                              void*);
+
     do_share_fn do_share;
 };
 
@@ -144,8 +40,7 @@ namespace detail {
 template<class ServiceT, std::size_t... Is>
 auto make_downcall_sharer(std::index_sequence<Is...>) {
     static constexpr sharer_vtbl vtbl[] = {
-        sharer_vtbl{&procedure_decision<ServiceT, Is>::erased_ipc_area_size,
-                    &procedure_decision<ServiceT, Is>::do_share}...};
+        sharer_vtbl{&procedure_decision<ServiceT, Is>::do_share}...};
     return tos::span<const sharer_vtbl>(vtbl);
 }
 } // namespace detail
