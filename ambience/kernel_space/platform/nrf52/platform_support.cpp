ee03991fc0152460e58809f2dac8c3e64d455038
Fatih Bakir <mfatihbakir@gmail.com>
2021-10-22 09:45:51 -0700

    [ambience] nrf52 platform support

---
+++ ambience/kernel_space/platform/nrf52/platform_support.cpp
@@ -0,0 +1,25 @@
+#include <block_memory_generated.hpp>
+#include <tos/ae/kernel/platform_support.hpp>
+#include <tos/ae/kernel/runners/preemptive_user_runner.hpp>
+#include <tos/ae/registry.hpp>
+#include <tos/arm/core.hpp>
+
+tos::ae::registry_base& get_registry();
+
+void platform_support::stage1_init() {
+}
+
+void platform_support::stage2_init() {
+    tos::arm::SCB::DEMCR.write(tos::arm::SCB::DEMCR.read() | 0x01000000);
+
+    tos::arm::DWT::LAR.write(0xC5ACCE55);
+    tos::arm::DWT::CYCCNT.write(0);
+    tos::arm::DWT::CONTROL.write(tos::arm::DWT::CONTROL.read() | 1);
+
+    tos::ae::preemptive_user_group_runner::create(
+        tos::make_erased_preemptive_runner(*this));
+}
+
+platform_group_args platform_support::make_args() {
+    return platform_group_args{.trampoline = &*m_trampoline};
+}
