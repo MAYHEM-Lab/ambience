4512f706adbe89fa1a161d9330cda00888c1749b
Fatih Bakir <mfatihbakir@gmail.com>
2021-10-22 08:29:02 -0700

    [ambience] Get rid of unused stuff

--- ambience/kernel_space/include/tos/ae/kernel/syscall.hpp
+++ ambience/kernel_space/include/tos/ae/kernel/syscall.hpp
@@ -1,10 +1,1 @@
 #pragma once
-
-#include <tos/ae/kernel/user_group.hpp>
-#include <tos/ae/rings.hpp>
-#include <tos/function_ref.hpp>
-
-namespace tos::ae::kernel {
-using init_syscall_handler = function_ref<void(user_group& group, interface& iface)>;
-using yield_handler = function_ref<void(user_group& group)>;
-}
