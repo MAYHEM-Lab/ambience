52b745a9591e96f40eb1a5c4100d31861a127675
Fatih Bakir <mfatihbakir@gmail.com>
2021-10-29 07:54:46 -0700

    [ambictl] Use correct dirs for build

--- ambience/ambictl/build.py
+++ ambience/ambictl/build.py
@@ -2,10 +2,18 @@ import sys
 from ambictl.loading import *
 from ambictl.ambidecl import *
 import mkbuild
+import pwd
+
+
+def current_user_name():
+    return pwd.getpwuid(os.getuid())[0]
+
 
 if __name__ == "__main__":
     dir = sys.argv[1]
     load_dir(dir)
     deployment = ambidecl._finalize()
-    mkbuild.create_build_root("/tmp/mydeployment", "/home/fatih/tos")
-    mkbuild.build_all("/tmp/mydeployment", deployment)
+    script_path = os.path.dirname(os.path.abspath(__file__))
+    mkbuild.create_build_root(
+        f"/tmp/{current_user_name()}/mydeployment", os.path.join(script_path, "../.."))
+    mkbuild.build_all(f"/tmp/{current_user_name()}/mydeployment", deployment)
