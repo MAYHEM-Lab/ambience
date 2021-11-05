7aac5e54a78309de06581c76d91d155242870598
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-05 11:33:43 -0700

    [ambience] basic_echo doesn't log, support async

--- ambience/services/impls/basic_echo/basic_echo.cpp
+++ ambience/services/impls/basic_echo/basic_echo.cpp
@@ -1,12 +1,8 @@
+#include "lidlrt/string.hpp"
 #include <echo_generated.hpp>
-#include <log_generated.hpp>
 
 namespace {
 struct impl : tos::ae::services::echo::sync_server {
-    impl(tos::services::logger::sync_server* logger)
-        : m_logger{logger} {
-    }
-
     int32_t echo_num(const int32_t& num) override {
         return num;
     }
@@ -15,12 +11,24 @@ struct impl : tos::ae::services::echo::sync_server {
                               lidl::message_builder& response_builder) override {
         return data;
     }
+};
 
-    tos::services::logger::sync_server* m_logger;
+struct async_impl : tos::ae::services::echo::async_server {
+    tos::Task<int32_t> echo_num(const int32_t& num) override {
+        co_return num;
+    }
+
+    tos::Task<std::string_view>
+    echo_str(std::string_view data, lidl::message_builder& response_builder) override {
+        co_return lidl::create_string(response_builder, data).string_view();
+    }
 };
 } // namespace
 
-tos::ae::services::echo::sync_server*
-init_basic_echo(tos::services::logger::sync_server* logger) {
-    return new impl(logger);
+tos::ae::services::echo::sync_server* init_basic_echo() {
+    return new impl();
+}
+
+tos::Task<tos::ae::services::echo::async_server*> init_async_basic_echo() {
+    co_return new async_impl();
 }
