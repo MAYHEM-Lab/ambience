4e3b9ca4cf4c1e20ab89c6f47bbbeca4d8cd747f
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-08 08:37:14 -0800

    [quik] Share string_view by value

--- libs/quikshare/include/tos/quik.hpp
+++ libs/quikshare/include/tos/quik.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "lidlrt/builder.hpp"
+#include "tos/utility.hpp"
 #include <cstdint>
+#include <lidlrt/concepts.hpp>
+#include <lidlrt/find_extent.hpp>
+#include <lidlrt/service.hpp>
 #include <string_view>
 #include <utility>
-#include <lidlrt/service.hpp>
 
 namespace tos::quik {
 struct share_base {
@@ -14,17 +18,41 @@ struct share_base {
 template<class T>
 struct sharer;
 
+template<lidl::RefObject T>
+struct sharer<const T*> {
+    template<class ShareT>
+    static const T* do_share(ShareT& share, const T* obj) {
+        auto [extent, pos] = lidl::meta::detail::find_extent_and_position(*obj);
+        auto ptr = share.raw_allocate(extent.size(), 1);
+        memcpy(ptr.direct_mapped(), extent.data(), extent.size());
+        return reinterpret_cast<const T*>(static_cast<const char*>(ptr.direct_mapped()) +
+                                          pos);
+    }
+};
+
+template<>
+struct sharer<lidl::message_builder*> {
+    template<class ShareT>
+    static lidl::message_builder* do_share(ShareT& share, lidl::message_builder* builder) {
+        auto buf = builder->get_buffer();
+        share.map_read_write(align_nearest_down_pow2(reinterpret_cast<uintptr_t>(buf.data()), 4096));
+        // This should not be mapped, but a new one cretead!
+        share.map_read_write(align_nearest_down_pow2(reinterpret_cast<uintptr_t>(builder), 4096));
+        return builder;
+    }
+};
+
 template<class T>
 struct sharer<const T> : sharer<T> {};
 
 template<>
 struct sharer<std::string_view> {
     template<class ShareT>
-    static std::string_view* do_share(ShareT& share, const std::string_view& sv_in_from) {
+    static std::string_view do_share(ShareT& share, const std::string_view& sv_in_from) {
         auto ptr = share.raw_allocate(sv_in_from.size(), 1);
-        memcpy(ptr, sv_in_from.data(), sv_in_from.size());
-        return share.template allocate<std::string_view>(static_cast<const char*>(ptr),
-                                                         sv_in_from.size());
+        memcpy(ptr.direct_mapped(), sv_in_from.data(), sv_in_from.size());
+        return std::string_view(static_cast<const char*>(ptr.direct_mapped()),
+                                sv_in_from.size());
     }
 };
 
