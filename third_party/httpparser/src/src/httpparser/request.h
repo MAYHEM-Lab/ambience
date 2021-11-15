5e476fbc1e181d89f991301e34038ab1b0e2d72d
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-14 23:22:32 -0800

    [httpparser] No sstream

--- third_party/httpparser/src/src/httpparser/request.h
+++ third_party/httpparser/src/src/httpparser/request.h
@@ -8,7 +8,6 @@
 
 #include <string>
 #include <vector>
-#include <sstream>
 
 namespace httpparser
 {
@@ -32,23 +31,23 @@ struct Request {
     std::vector<char> content;
     bool keepAlive;
 
-    std::string inspect() const
-    {
-        std::stringstream stream;
-        stream << method << " " << uri << " HTTP/"
-               << versionMajor << "." << versionMinor << "\n";
+    // std::string inspect() const
+    // {
+    //     std::stringstream stream;
+    //     stream << method << " " << uri << " HTTP/"
+    //            << versionMajor << "." << versionMinor << "\n";
 
-        for(std::vector<Request::HeaderItem>::const_iterator it = headers.begin();
-            it != headers.end(); ++it)
-        {
-            stream << it->name << ": " << it->value << "\n";
-        }
+    //     for(std::vector<Request::HeaderItem>::const_iterator it = headers.begin();
+    //         it != headers.end(); ++it)
+    //     {
+    //         stream << it->name << ": " << it->value << "\n";
+    //     }
 
-        std::string data(content.begin(), content.end());
-        stream << data << "\n";
-        stream << "+ keep-alive: " << keepAlive << "\n";;
-        return stream.str();
-    }
+    //     std::string data(content.begin(), content.end());
+    //     stream << data << "\n";
+    //     stream << "+ keep-alive: " << keepAlive << "\n";;
+    //     return stream.str();
+    // }
 };
 
 } // namespace httpparser
