93f3e801690cf09c714ff994544f0d3210e5f5a1
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-17 16:17:17 -0800

    [virtio] netif supports zero copy reception

--- src/drivers/virtio/virtio_netif.cpp
+++ src/drivers/virtio/virtio_netif.cpp
@@ -27,15 +27,35 @@ void net_if::read_thread(cancellation_token& tok) {
         auto packet = m_dev->take_packet();
 
         //            tos::debug::log("Got packet", this, packet.size());
-        auto p = pbuf_alloc(pbuf_layer::PBUF_RAW, packet.size(), pbuf_type::PBUF_POOL);
+        // auto p = pbuf_alloc(pbuf_layer::PBUF_RAW, packet.size(), pbuf_type::PBUF_POOL);
+        struct zerocopy_rx_data : pbuf_custom {
+            span<uint8_t> packet;
+            tos::virtio::network_device* netdev;
+
+            ~zerocopy_rx_data() {
+                netdev->return_packet(packet);
+            }
+        };
+        auto custom_pbuf = new zerocopy_rx_data;
+        custom_pbuf->packet = packet;
+        custom_pbuf->netdev = m_dev;
+        custom_pbuf->custom_free_function = [](pbuf* data) {
+            delete reinterpret_cast<zerocopy_rx_data*>(data);
+        };
+        auto p = pbuf_alloced_custom(pbuf_layer::PBUF_RAW,
+                                     packet.size_bytes(),
+                                     pbuf_type::PBUF_POOL,
+                                     custom_pbuf,
+                                     packet.data(),
+                                     packet.size_bytes());
         if (!p) {
             tos::debug::error("Could not allocate pbuf!", packet.size());
             m_dev->return_packet(packet);
             continue;
         }
 
-        tos::lwip::copy_to_pbuf(packet.begin(), packet.end(), *p);
-        m_dev->return_packet(packet);
+        // tos::lwip::copy_to_pbuf(packet.begin(), packet.end(), *p);
+        // m_dev->return_packet(packet);
 
         {
             tos::lock_guard lg{tos::lwip::lwip_lock};
@@ -66,7 +86,8 @@ err_t net_if::link_output(pbuf* p) {
             tos::span<const uint8_t>{static_cast<const uint8_t*>(p->payload), p->len};
         p = p->next;
         return res;
-    }), [p] { pbuf_free(p); });
+    }),
+                             [p] { pbuf_free(p); });
     return ERR_OK;
 }
 } // namespace tos::virtio
