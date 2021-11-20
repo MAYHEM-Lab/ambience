768e9aa171573c1193f846fec7992ed33d227b60
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-19 22:56:23 -0800

    [arm] call_trampoline

--- src/arch/arm/include/tos/arm/processor_state.hpp
+++ src/arch/arm/include/tos/arm/processor_state.hpp
@@ -3,6 +3,7 @@
 #include <csetjmp>
 #include <cstdint>
 
+extern "C" void tos_call_trampoline();
 namespace tos::arm {
 inline uintptr_t get_rip(const jmp_buf& buf) {
     return buf[9];
@@ -18,5 +19,11 @@ inline void set_rip(jmp_buf& buf, uintptr_t val) {
 
 inline void set_rsp(jmp_buf& buf, uintptr_t val) {
     buf[8] = val;
+}
+
+inline void make_call(jmp_buf& buf, void(*entry)(void*), void* ptr) {
+    buf[0] = reinterpret_cast<uintptr_t>(entry);
+    buf[1] = reinterpret_cast<uintptr_t>(ptr);
+    set_rip(buf, reinterpret_cast<uintptr_t>(&tos_call_trampoline));
 }
 } // namespace tos::arm
