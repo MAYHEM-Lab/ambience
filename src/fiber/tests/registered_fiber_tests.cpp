1c0b0edb50aef0eb208830cb4f0c45ad6dadacaf
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-20 18:49:23 -0800

    [fiber] registered_fiber swap_fiber test

--- src/fiber/tests/registered_fiber_tests.cpp
+++ src/fiber/tests/registered_fiber_tests.cpp
@@ -99,5 +99,36 @@ TEST_CASE("registered_fiber works with nested resumes") {
     f2->resume();
     REQUIRE_EQ(45, x);
 }
+
+TEST_CASE("registered_fiber works with swap_fibers") {
+    int x = 1;
+    REQUIRE_EQ(nullptr, tos::fiber::current_fiber());
+    auto f1 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 42;
+            fib.suspend();
+        }));
+    auto f2 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            swap_fibers(fib, *f1, []{});
+            REQUIRE_EQ(42, x);
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 45;
+            fib.suspend();
+        }));
+    f1->resume();
+    f2->resume();
+
+    f2->resume();
+    REQUIRE_EQ(nullptr, tos::fiber::current_fiber());
+    REQUIRE_EQ(42, x);
+    f2->resume();
+    REQUIRE_EQ(nullptr, tos::fiber::current_fiber());
+    REQUIRE_EQ(45, x);
+}
 } // namespace
 } // namespace tos::fiber
