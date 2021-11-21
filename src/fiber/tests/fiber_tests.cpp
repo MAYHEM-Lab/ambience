6ab6fabda8c2e1726d100447130b2da7bacb69ee
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-20 18:16:48 -0800

    [fiber] Need empty template arg list

--- src/fiber/tests/fiber_tests.cpp
+++ src/fiber/tests/fiber_tests.cpp
@@ -17,13 +17,13 @@ TEST_CASE("now_owning fiber works") {
 
 TEST_CASE("swap_context works") {
     int x = 1;
-    auto f1 = unique(owning::start(stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+    auto f1 = unique(owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
         x = 42;
         fib.suspend();
     }));
     REQUIRE(f1);
     auto f2 = unique(
-        owning::start(stack_size_t{TOS_DEFAULT_STACK_SIZE},
+        owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE},
                       [f1 = f1.get()](auto& fib) { swap_fibers(fib, *f1, [] {}); }));
     REQUIRE(f2);
     f2->resume();
