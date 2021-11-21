2ba7194372cd75afae24d93d2965a74fde146aa6
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-20 18:24:49 -0800

    [fiber] nested resume test

--- src/fiber/tests/fiber_tests.cpp
+++ src/fiber/tests/fiber_tests.cpp
@@ -3,7 +3,6 @@
 #include "tos/stack_storage.hpp"
 #include <doctest.h>
 #include <tos/fiber.hpp>
-#include <tos/fiber/this_fiber.hpp>
 
 namespace tos::fiber {
 namespace {
@@ -15,84 +14,40 @@ TEST_CASE("now_owning fiber works") {
     REQUIRE_EQ(42, x);
 }
 
+TEST_CASE("nested resume works") {
+    int x = 1;
+    auto f1 =
+        unique(owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            x = 42;
+            fib.suspend();
+        }));
+    REQUIRE(f1);
+    auto f2 = unique(owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE},
+                                     [&x, f1 = f1.get()](auto& fib) {
+                                         f1->resume();
+                                         REQUIRE_EQ(42, x);
+                                         x = 45;
+                                         fib.suspend();
+                                     }));
+    REQUIRE(f2);
+    f2->resume();
+    REQUIRE_EQ(45, x);
+}
+
 TEST_CASE("swap_context works") {
     int x = 1;
-    auto f1 = unique(owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
-        x = 42;
-        fib.suspend();
-    }));
+    auto f1 =
+        unique(owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            x = 42;
+            fib.suspend();
+        }));
     REQUIRE(f1);
     auto f2 = unique(
         owning<>::start(stack_size_t{TOS_DEFAULT_STACK_SIZE},
-                      [f1 = f1.get()](auto& fib) { swap_fibers(fib, *f1, [] {}); }));
+                        [f1 = f1.get()](auto& fib) { swap_fibers(fib, *f1, [] {}); }));
     REQUIRE(f2);
     f2->resume();
     REQUIRE_EQ(42, x);
-}
-
-TEST_CASE("registered_fiber works") {
-    int x = 1;
-    auto f = unique(tos::fiber::registered_owning::start(
-        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
-            fib.suspend();
-            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
-            x = 42;
-            fib.suspend();
-        }));
-    f->resume();
-    f->resume();
-    REQUIRE_EQ(42, x);
-}
-
-TEST_CASE("registered_fiber works with 2 fibers") {
-    int x = 1;
-    auto f1 = unique(tos::fiber::registered_owning::start(
-        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
-            fib.suspend();
-            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
-            x = 42;
-            fib.suspend();
-        }));
-    auto f2 = unique(tos::fiber::registered_owning::start(
-        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
-            fib.suspend();
-            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
-            x = 45;
-            fib.suspend();
-        }));
-    f1->resume();
-    f2->resume();
-
-    f1->resume();
-    REQUIRE_EQ(42, x);
-    f2->resume();
-    REQUIRE_EQ(45, x);
-}
-
-TEST_CASE("registered_fiber works with nested resumes") {
-    int x = 1;
-    auto f1 = unique(tos::fiber::registered_owning::start(
-        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
-            fib.suspend();
-            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
-            x = 42;
-            fib.suspend();
-        }));
-    auto f2 = unique(tos::fiber::registered_owning::start(
-        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
-            fib.suspend();
-            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
-            f1->resume();
-            REQUIRE_EQ(42, x);
-            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
-            x = 45;
-            fib.suspend();
-        }));
-    f1->resume();
-    f2->resume();
-
-    f2->resume();
-    REQUIRE_EQ(45, x);
 }
 } // namespace
 } // namespace tos::fiber
---
+++ src/fiber/tests/registered_fiber_tests.cpp
@@ -0,0 +1,73 @@
+#include "tos/stack_storage.hpp"
+#include <doctest.h>
+#include <tos/fiber.hpp>
+#include <tos/fiber/this_fiber.hpp>
+
+namespace tos::fiber {
+namespace {
+TEST_CASE("registered_fiber works") {
+    int x = 1;
+    auto f = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 42;
+            fib.suspend();
+        }));
+    f->resume();
+    f->resume();
+    REQUIRE_EQ(42, x);
+}
+
+TEST_CASE("registered_fiber works with 2 fibers") {
+    int x = 1;
+    auto f1 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 42;
+            fib.suspend();
+        }));
+    auto f2 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 45;
+            fib.suspend();
+        }));
+    f1->resume();
+    f2->resume();
+
+    f1->resume();
+    REQUIRE_EQ(42, x);
+    f2->resume();
+    REQUIRE_EQ(45, x);
+}
+
+TEST_CASE("registered_fiber works with nested resumes") {
+    int x = 1;
+    auto f1 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 42;
+            fib.suspend();
+        }));
+    auto f2 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            f1->resume();
+            REQUIRE_EQ(42, x);
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 45;
+            fib.suspend();
+        }));
+    f1->resume();
+    f2->resume();
+
+    f2->resume();
+    REQUIRE_EQ(45, x);
+}
+} // namespace
+} // namespace tos::fiber
