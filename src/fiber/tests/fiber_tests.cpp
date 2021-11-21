38866e3ad97228c8ccea4b87af06e0bbf6b85ba5
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-20 18:20:19 -0800

    [fiber] More registered_fiber tests

--- src/fiber/tests/fiber_tests.cpp
+++ src/fiber/tests/fiber_tests.cpp
@@ -43,5 +43,56 @@ TEST_CASE("registered_fiber works") {
     f->resume();
     REQUIRE_EQ(42, x);
 }
+
+TEST_CASE("registered_fiber works with 2 fibers") {
+    int x = 1;
+    auto f1 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 42;
+            fib.suspend();
+        }));
+    auto f2 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 45;
+            fib.suspend();
+        }));
+    f1->resume();
+    f2->resume();
+
+    f1->resume();
+    REQUIRE_EQ(42, x);
+    f2->resume();
+    REQUIRE_EQ(45, x);
+}
+
+TEST_CASE("registered_fiber works with nested resumes") {
+    int x = 1;
+    auto f1 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 42;
+            fib.suspend();
+        }));
+    auto f2 = unique(tos::fiber::registered_owning::start(
+        stack_size_t{TOS_DEFAULT_STACK_SIZE}, [&](auto& fib) {
+            fib.suspend();
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            f1->resume();
+            REQUIRE_EQ(42, x);
+            REQUIRE_EQ(&fib, tos::fiber::current_fiber());
+            x = 45;
+            fib.suspend();
+        }));
+    f1->resume();
+    f2->resume();
+
+    f2->resume();
+    REQUIRE_EQ(45, x);
+}
 } // namespace
 } // namespace tos::fiber
