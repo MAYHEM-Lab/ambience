5efe67bfe5ef52f532386215f1b19ad8d7fd267a
Fatih Bakir <mfatihbakir@gmail.com>
2021-11-22 10:49:41 -0800

    [ambictl] .ambience dirs

--- ambience/ambictl/ambictl/loading.py
+++ ambience/ambictl/ambictl/loading.py
@@ -5,6 +5,7 @@ from . import *
 import importlib.util
 import importlib
 import glob
+import itertools
 
 
 def load_file(path: str):
@@ -14,8 +15,23 @@ def load_file(path: str):
         exec(contents, globals())
 
 
-def load_dir(path: str):
+def load_one_dir(path: str):
     files = glob.glob(os.path.join(path, "*.py"))
     files.sort()
     for file in files:
         load_file(file)
+
+
+def load_dir(path: str):
+    path = os.path.realpath(path)
+
+    parts = path.split(os.sep)
+    parts = (f"{x}/" for x in parts)
+    all_paths = itertools.accumulate(parts)
+    for path in all_paths:
+        print(path)
+        ambience_dir = os.path.join(path, ".ambience")
+        if os.path.exists(ambience_dir):
+            load_one_dir(ambience_dir)
+
+    load_one_dir(path)
