695e16da1077a13bb9f7b7fde6614dbe218c4b5b
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-01 16:31:21 -0800

    [nrf52] UART supports multiple instances and read timeouts

--- src/drivers/arch/nrf52/include/arch/usart.hpp
+++ src/drivers/arch/nrf52/include/arch/usart.hpp
@@ -5,14 +5,19 @@
 #pragma once
 
 #include "gpio.hpp"
-
+#include "tos/semaphore.hpp"
 #include <common/driver_base.hpp>
 #include <common/usart.hpp>
+#include <nrfx_uarte.h>
 #include <tos/mutex.hpp>
 #include <tos/span.hpp>
 
 namespace tos {
 namespace nrf52 {
+namespace detail {
+inline const nrfx_uarte_t uarts[2]{{NRF_UARTE0, NRFX_UARTE0_INST_IDX},
+                                   {NRF_UARTE1, NRFX_UARTE1_INST_IDX}};
+} // namespace detail
 using usart_constraint = ct_map<usart_key_policy,
                                 el_t<usart_baud_rate, const usart_baud_rate&>,
                                 el_t<usart_parity, const usart_parity&>,
@@ -22,12 +27,33 @@ class uart
     : public self_pointing<uart>
     , public non_copy_movable {
 public:
-    explicit uart(usart_constraint&& config,
+    explicit uart(const nrfx_uarte_t& dev,
+                  usart_constraint&& config,
                   gpio::pin_type rx,
                   gpio::pin_type tx) noexcept;
 
     int write(span<const uint8_t> buf);
     span<uint8_t> read(span<uint8_t> buf);
+
+    template<class AlarmT>
+    tos::span<uint8_t>
+    read(tos::span<uint8_t> buf, AlarmT& alarm, std::chrono::milliseconds to) {
+        lock_guard lk{m_read_busy};
+
+        auto err = nrfx_uarte_rx(m_dev, buf.data(), buf.size());
+
+        if (err != NRFX_SUCCESS) {
+            return span<uint8_t>();
+        }
+
+        auto wait_res = m_read_sync.down(alarm, to);
+        if (wait_res == sem_ret::normal) {
+            return buf;
+        }
+
+        nrfx_uarte_rx_abort(m_dev);
+        return span<uint8_t>();
+    }
 
     ~uart();
 
@@ -39,6 +65,8 @@ private:
 
     tos::mutex m_read_busy;
     tos::semaphore m_read_sync;
+
+    const nrfx_uarte_t* m_dev;
 };
 } // namespace nrf52
 
@@ -46,6 +74,13 @@ inline nrf52::uart open_impl(devs::usart_t<0>,
                              nrf52::usart_constraint&& c,
                              nrf52::gpio::pin_type rx_pin,
                              nrf52::gpio::pin_type tx_pin) {
-    return nrf52::uart{std::move(c), rx_pin, tx_pin};
+    return nrf52::uart{nrf52::detail::uarts[0], std::move(c), rx_pin, tx_pin};
+}
+
+inline nrf52::uart open_impl(devs::usart_t<1>,
+                             nrf52::usart_constraint&& c,
+                             nrf52::gpio::pin_type rx_pin,
+                             nrf52::gpio::pin_type tx_pin) {
+    return nrf52::uart{nrf52::detail::uarts[1], std::move(c), rx_pin, tx_pin};
 }
 } // namespace tos
--- src/drivers/arch/nrf52/src/usart.cpp
+++ src/drivers/arch/nrf52/src/usart.cpp
@@ -3,114 +3,102 @@
 //
 
 #include <arch/usart.hpp>
-#include <nrfx_uarte.h>
-#include <drivers/include/nrfx_uarte.h>
 
-namespace tos
-{
-    namespace nrf52
-    {
-        static const nrfx_uarte_t uart0 { NRF_UARTE0, NRFX_UARTE0_INST_IDX };
+namespace tos {
+namespace nrf52 {
+static constexpr uint32_t compute_baudrate_register(uint32_t desired_rate) {
+    // https://devzone.nordicsemi.com/f/nordic-q-a/391/uart-baudrate-register-values
 
-        static constexpr uint32_t compute_baudrate_register(uint32_t desired_rate)
-        {
-            //https://devzone.nordicsemi.com/f/nordic-q-a/391/uart-baudrate-register-values
+    constexpr uint64_t multiplier = 4294967296UL;
+    return uint64_t(desired_rate) * multiplier / 16'000'000UL;
+}
 
-            constexpr uint64_t multiplier = 4294967296UL;
-            return uint64_t (desired_rate) * multiplier / 16'000'000UL;
-        }
+static_assert(compute_baudrate_register(9600) == 0x275254);
 
-        static_assert(compute_baudrate_register(9600) == 0x275254);
+uart::uart(const nrfx_uarte_t& dev,
+           usart_constraint&& config,
+           gpio::pin_type rx,
+           gpio::pin_type tx) noexcept
+    : m_write_sync{0}
+    , m_read_sync{0}
+    , m_dev{&dev} {
+    nrfx_uarte_config_t conf{};
 
-        uart::uart(usart_constraint&& config, gpio::pin_type rx, gpio::pin_type tx) noexcept
-                : m_write_sync{0}, m_read_sync{0}
-        {
-            nrfx_uarte_config_t conf{};
+    conf.interrupt_priority = 7;
+    conf.parity = NRF_UARTE_PARITY_EXCLUDED;
+    conf.hwfc = NRF_UARTE_HWFC_DISABLED;
+    conf.baudrate = nrf_uarte_baudrate_t(
+        compute_baudrate_register(tos::get<tos::usart_baud_rate>(config).rate));
+    conf.pselcts = NRF_UARTE_PSEL_DISCONNECTED;
+    conf.pselrts = NRF_UARTE_PSEL_DISCONNECTED;
+    conf.pselrxd = detail::to_sdk_pin(rx);
+    conf.pseltxd = detail::to_sdk_pin(tx);
+    conf.p_context = this;
 
-            conf.interrupt_priority = 7;
-            conf.parity = NRF_UARTE_PARITY_EXCLUDED;
-            conf.hwfc = NRF_UARTE_HWFC_DISABLED;
-            conf.baudrate = nrf_uarte_baudrate_t(
-                            compute_baudrate_register(tos::get<tos::usart_baud_rate>(config).rate));
-            conf.pselcts = NRF_UARTE_PSEL_DISCONNECTED;
-            conf.pselrts = NRF_UARTE_PSEL_DISCONNECTED;
-            conf.pselrxd = detail::to_sdk_pin(rx);
-            conf.pseltxd = detail::to_sdk_pin(tx);
-            conf.p_context = this;
+    auto res = nrfx_uarte_init(
+        m_dev, &conf, [](nrfx_uarte_event_t const* p_event, void* p_context) {
+            auto self = static_cast<uart*>(p_context);
+            self->handle_callback(p_event);
+        });
 
-            auto res = nrfx_uarte_init(&uart0, &conf, [](nrfx_uarte_event_t const *p_event, void *p_context){
-                auto self = static_cast<uart*>(p_context);
-                self->handle_callback(p_event);
-            });
+    if (res != NRFX_SUCCESS) {
+        // TODO: report error
+    }
 
-            if (res != NRFX_SUCCESS)
-            {
-                // TODO: report error
-            }
+    tos::kern::refresh_interrupts();
+}
 
-            tos::kern::refresh_interrupts();
-        }
+void uart::handle_callback(const void* event) {
+    auto p_event = static_cast<const nrfx_uarte_event_t*>(event);
 
-        void uart::handle_callback(const void *event) {
-            auto p_event = static_cast<const nrfx_uarte_event_t*>(event);
-
-            if (p_event->type == NRFX_UARTE_EVT_TX_DONE)
-            {
-                m_write_sync.up_isr();
-            }
-            else if (p_event->type == NRFX_UARTE_EVT_RX_DONE)
-            {
-                m_read_sync.up_isr();
-            }
-        }
-
-        int uart::write(span<const uint8_t> buf) {
-            tos::lock_guard<tos::mutex> lk { m_write_busy };
-
-            nrfx_err_t err;
-            if (nrfx_is_in_ram(buf.data()))
-            {
-                err = nrfx_uarte_tx(&uart0, buf.data(), buf.size());
-                if (err != NRFX_SUCCESS)
-                {
-                    return -1;
-                    // TODO: report error
-                }
-
-                m_write_sync.down();
-            }
-            else
-            {
-                while (!buf.empty())
-                {
-                    uint8_t tbuf[32];
-                    auto len = std::min<size_t>(32, buf.size());
-                    std::copy_n(buf.begin(), len, std::begin(tbuf));
-                    buf = buf.slice(len);
-                    err = nrfx_uarte_tx(&uart0, tbuf, len);
-                    m_write_sync.down();
-                }
-            }
-            return buf.size();
-        }
-
-        span<uint8_t> uart::read(span<uint8_t> buf) {
-            tos::lock_guard<tos::mutex> lk { m_read_busy };
-
-            auto err = nrfx_uarte_rx(&uart0, (uint8_t*)buf.data(), buf.size());
-
-            if (err != NRFX_SUCCESS)
-            {
-                return tos::empty_span<uint8_t>();
-            }
-
-            m_read_sync.down();
-
-            return buf;
-        }
-
-        uart::~uart() {
-            nrfx_uarte_uninit(&uart0);
-        }
+    if (p_event->type == NRFX_UARTE_EVT_TX_DONE) {
+        m_write_sync.up_isr();
+    } else if (p_event->type == NRFX_UARTE_EVT_RX_DONE) {
+        m_read_sync.up_isr();
     }
 }
+
+int uart::write(span<const uint8_t> buf) {
+    lock_guard lk{m_write_busy};
+
+    nrfx_err_t err;
+    if (nrfx_is_in_ram(buf.data())) {
+        err = nrfx_uarte_tx(m_dev, buf.data(), buf.size());
+        if (err != NRFX_SUCCESS) {
+            return -1;
+            // TODO: report error
+        }
+
+        m_write_sync.down();
+    } else {
+        while (!buf.empty()) {
+            uint8_t tbuf[32];
+            auto len = std::min<size_t>(32, buf.size());
+            std::copy_n(buf.begin(), len, std::begin(tbuf));
+            buf = buf.slice(len);
+            err = nrfx_uarte_tx(m_dev, tbuf, len);
+            m_write_sync.down();
+        }
+    }
+    return buf.size();
+}
+
+span<uint8_t> uart::read(span<uint8_t> buf) {
+    lock_guard lk{m_read_busy};
+
+    auto err = nrfx_uarte_rx(m_dev, buf.data(), buf.size());
+
+    if (err != NRFX_SUCCESS) {
+        return span<uint8_t>();
+    }
+
+    m_read_sync.down();
+
+    return buf;
+}
+
+uart::~uart() {
+    nrfx_uarte_uninit(m_dev);
+}
+} // namespace nrf52
+} // namespace tos
