2acf6146a302b76451e4f5b7882acef2fc542f2c
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-02 14:05:46 -0800

    [nrf52] Board has correct pins

--- config/boards/nrf52840_dk/board.hpp
+++ config/boards/nrf52840_dk/board.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "common/usart.hpp"
 #include <arch/drivers.hpp>
 
 namespace tos::bsp {
@@ -12,9 +13,9 @@ struct nrf52840_dk_spec {
 
     struct xbee_uart {
         static constexpr auto tag = devs::usart<1>;
-        static constexpr auto conf = uart::default_115200;
-        static constexpr auto tx_pin = 8;
-        static constexpr auto rx_pin = 9;
+        static constexpr auto conf = uart::default_38400;
+        static constexpr auto tx_pin = 36;
+        static constexpr auto rx_pin = 37;
 
         static auto open() {
             return tos::open(tag,
@@ -33,7 +34,7 @@ struct nrf52840_dk_spec {
         static auto open() {
             return tos::open(tag,
                              std::move(conf),
-                             nrf52::instantiate_pin(tx_pin),
+                             nrf52::instantiate_pin(rx_pin),
                              nrf52::instantiate_pin(tx_pin));
         }
     };
