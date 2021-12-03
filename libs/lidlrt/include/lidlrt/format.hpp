fa5a6717b642259c3c930581d7fd211954daf485
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-03 14:52:01 -0800

    [lidlrt] Formatting of lidl structs

---
+++ libs/lidlrt/include/lidlrt/format.hpp
@@ -0,0 +1,64 @@
+#include "lidlrt/structure.hpp"
+#include <fmt/compile.h>
+#include <fmt/format.h>
+#include <lidlrt/concepts.hpp>
+#include <lidlrt/lib.hpp>
+#include <tos/meta/algorithm.hpp>
+
+namespace fmt {
+template<lidl::Struct T>
+struct formatter<T> {
+    using traits = lidl::struct_traits<T>;
+
+    template<typename ParseContext>
+    constexpr auto parse(ParseContext& ctx) {
+        return ctx.begin();
+    }
+
+    struct compile_string_t : fmt::detail::compiled_string {
+        static constexpr auto make_fmt_str() {
+            constexpr auto names = tos::meta::transform(traits::members, [](auto& mem) {
+                return mem.name;
+            });
+            constexpr auto parts = tos::meta::transform(names, [](auto& name) {
+                return name + tos::fixed_string(" = {}, ");
+            });
+            constexpr auto total = tos::meta::accumulate(parts, tos::fixed_string("{")) + tos::fixed_string("}");
+            
+            std::array<char, total.size()> ret{};
+            std::copy(std::begin(total.val), std::end(total.val), ret.begin());
+            return ret;
+        }
+
+        static constexpr auto make_simple_fmt_str() {
+            std::array<char, 3 * traits::arity> res{};
+            for (size_t i = 0; i < traits::arity; ++i) {
+                std::copy_n("{} ", 3, res.begin() + i);
+            }
+            res.back() = 0;
+            return res;
+        }
+
+        static constexpr auto fmt_str = make_fmt_str();
+
+        using char_type = char;
+
+        [[maybe_unused]] constexpr explicit
+        operator fmt::basic_string_view<char>() const {
+            return fmt::basic_string_view<char>(fmt_str.begin(), fmt_str.size());
+        }
+    };
+
+    static constexpr auto format_string() {
+        return compile_string_t{};
+    }
+
+    template<typename FormatContext>
+    constexpr auto format(T const& str, FormatContext& ctx) {
+        using lidl::apply;
+        return apply([&](auto&... mems) {
+            return fmt::format_to(ctx.out(), format_string(), mems...);
+        }, str);
+    }
+};
+} // namespace fmt
