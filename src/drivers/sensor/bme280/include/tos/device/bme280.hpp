9d7d101b0fae04a657f0cbb522559df6d600125c
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-03 14:47:43 -0800

    [bme280] Ambience enabled driver

--- src/drivers/sensor/bme280/include/tos/device/bme280.hpp
+++ src/drivers/sensor/bme280/include/tos/device/bme280.hpp
@@ -4,10 +4,12 @@
 
 #pragma once
 
+#include "tos/debug/log.hpp"
 #include <common/driver_base.hpp>
 #include <common/i2c.hpp>
 #include <tos/device/bme280/bme280.h>
 #include <tos/expected.hpp>
+#include <weather_sensor_generated.hpp>
 
 namespace tos::device::bme280 {
 enum class read_errors : uint8_t
@@ -24,15 +26,62 @@ enum class components : uint8_t
     all = BME280_ALL
 };
 
+inline
 components operator|(const components& a, const components& b) {
     return components(uint8_t(a) | uint8_t(b));
 }
+
+struct base_driver {
+    bme280_dev m_dev;
+    base_driver() = default;
+
+    base_driver(base_driver&& rhs)
+        : m_dev{rhs.m_dev} {
+        m_dev.user = this;
+    }
+
+    void set_config() {
+        uint8_t settings_sel{};
+
+        m_dev.settings.osr_h = BME280_OVERSAMPLING_1X;
+        m_dev.settings.osr_p = BME280_OVERSAMPLING_1X;
+        m_dev.settings.osr_t = BME280_OVERSAMPLING_1X;
+        m_dev.settings.filter = BME280_FILTER_COEFF_OFF;
+        m_dev.settings.standby_time = BME280_STANDBY_TIME_62_5_MS;
+
+        settings_sel = BME280_OSR_PRESS_SEL;
+        settings_sel |= BME280_OSR_TEMP_SEL;
+        settings_sel |= BME280_OSR_HUM_SEL;
+        settings_sel |= BME280_STANDBY_SEL;
+        settings_sel |= BME280_FILTER_SEL;
+
+        [[maybe_unused]] auto rslt = bme280_set_sensor_settings(settings_sel, &m_dev);
+    }
+
+    void sleep() {
+        [[maybe_unused]] auto rslt = bme280_set_sensor_mode(BME280_SLEEP_MODE, &m_dev);
+    }
+
+    void enable() {
+        [[maybe_unused]] auto rslt = bme280_set_sensor_mode(BME280_NORMAL_MODE, &m_dev);
+    }
+
+    tos::expected<bme280_data, read_errors> read(components what = components::all) {
+        bme280_data res;
+        auto rslt = bme280_get_sensor_data(uint8_t(what), &res, &m_dev);
+        if (rslt != BME280_OK) {
+            return unexpected(read_errors(rslt));
+        }
+        return res;
+    }
+};
 
 template<class I2CT, class DelayT>
 class driver
     : public self_pointing<driver<I2CT, DelayT>>
     , public non_copyable
-    , DelayT {
+    , DelayT
+    , public base_driver {
 public:
     driver(twi_addr_t addr, I2CT i2c, DelayT delay)
         : DelayT(std::move(delay))
@@ -75,52 +124,27 @@ public:
         };
 
         m_dev.user = this;
-        bme280_init(&m_dev);
+        auto res = bme280_init(&m_dev);
+        if (res != BME280_OK) {
+            LOG_ERROR("Init failed", res);
+        }
     }
 
     driver(driver&& rhs)
-        : m_dev{rhs.m_dev}
+        : base_driver(std::move(rhs))
         , m_i2c{std::move(rhs.m_i2c)} {
-        m_dev.user = this;
-    }
-
-    void set_config() {
-        uint8_t settings_sel{};
-
-        m_dev.settings.osr_h = BME280_OVERSAMPLING_1X;
-        m_dev.settings.osr_p = BME280_OVERSAMPLING_16X;
-        m_dev.settings.osr_t = BME280_OVERSAMPLING_2X;
-        m_dev.settings.filter = BME280_FILTER_COEFF_16;
-        m_dev.settings.standby_time = BME280_STANDBY_TIME_62_5_MS;
-
-        settings_sel = BME280_OSR_PRESS_SEL;
-        settings_sel |= BME280_OSR_TEMP_SEL;
-        settings_sel |= BME280_OSR_HUM_SEL;
-        settings_sel |= BME280_STANDBY_SEL;
-        settings_sel |= BME280_FILTER_SEL;
-
-        [[maybe_unused]] auto rslt = bme280_set_sensor_settings(settings_sel, &m_dev);
-    }
-
-    void sleep() {
-        [[maybe_unused]] auto rslt = bme280_set_sensor_mode(BME280_SLEEP_MODE, &m_dev);
-    }
-
-    void enable() {
-        [[maybe_unused]] auto rslt = bme280_set_sensor_mode(BME280_NORMAL_MODE, &m_dev);
-    }
-
-    tos::expected<bme280_data, read_errors> read(components what = components::all) {
-        bme280_data res;
-        auto rslt = bme280_get_sensor_data(uint8_t(what), &res, &m_dev);
-        if (rslt != BME280_OK) {
-            return unexpected(read_errors(rslt));
-        }
-        return res;
     }
 
 private:
     I2CT m_i2c;
-    bme280_dev m_dev;
+};
+
+struct sensor_wrapper : public ae::services::weather_sensor::sync_server {
+    sensor_wrapper(base_driver* drv) : m_drv{drv} {}
+    base_driver* m_drv;
+    tos::ae::services::weather_sample sample() override {
+        auto [pres, temp, humid] = force_get(m_drv->read());
+        return ae::services::weather_sample{temp, humid, pres};
+    }
 };
 } // namespace tos::device::bme280
