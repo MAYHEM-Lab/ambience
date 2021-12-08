8d4a0bf5ba1232cf86b2f85bc51136f5ae2190b8
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-07 20:29:22 -0800

    [ambience] interface can be in .bss

--- ambience/ambictl/ambictl/templates/user_group/interface.cpp
+++ ambience/ambictl/ambictl/templates/user_group/interface.cpp
@@ -2,7 +2,7 @@
 
 namespace {
 constexpr auto queue_len = {{queue_size}};
-[[gnu::section(".nozero")]] tos::ae::interface_storage<queue_len> storage;
+tos::ae::interface_storage<queue_len> storage;
 } // namespace
 
 tos::ae::interface iface = storage.make_interface();
