04624124ec154a0ba52a4f6d8d038d622d7a90bc
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-16 11:34:29 -0800

    [libcxx] header

--- third_party/llvm/libcxx/src/vector.cpp
+++ third_party/llvm/libcxx/src/vector.cpp
@@ -1,6 +1,10 @@
+//===------------------------- vector.cpp ---------------------------------===//
 //
-// Created by Mehmet Fatih BAKIR on 24/10/2018.
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
+//===----------------------------------------------------------------------===//
 
 #include "vector"
 
