9107918782a783276f35d2f4a8f1337ec5173f09
Fatih Bakir <mfatihbakir@gmail.com>
2021-12-21 17:23:31 -0800

    [timestamp] arm support

--- ambience/services/impls/timestamp/timestamp.cpp
+++ ambience/services/impls/timestamp/timestamp.cpp
@@ -1,6 +1,8 @@
 #include <agent_generated.hpp>
 #ifdef __x86_64__
 #include <tos/x86_64/assembly.hpp>
+#elif defined(__arm__)
+#include <tos/arm/core.hpp>
 #endif
 
 namespace {
@@ -8,6 +10,8 @@ struct timestamper : tos::ae::nullaryfn::async_server {
     tos::Task<uint64_t> run() override {
 #ifdef __x86_64__
         co_return tos::x86_64::rdtsc();
+#elif defined __arm__
+        co_return tos::arm::DWT::CYCCNT.read();
 #endif
         co_return 0;
     }
