960f47150ab95b2d727a39bdba08285f1e6b212c
Fatih Bakir <mfatihbakir@gmail.com>
2022-01-04 16:00:58 -0800

    [ambience] xbee_exporter can store uart as a ref too

--- ambience/kernel_space/src/xbee_exporter.cpp
+++ ambience/kernel_space/src/xbee_exporter.cpp
@@ -5,13 +5,13 @@
 
 void maybe_init_xbee(tos::any_alarm& alarm) {
     if constexpr (registry.has_service<"XbeeExporter">()) {
-        static auto uart = tos::bsp::board_spec::xbee_uart::open();
+        static decltype(auto) uart = tos::bsp::board_spec::xbee_uart::open();
         auto exporter =
             new tos::ae::xbee_exporter(&uart, &alarm);
         registry.register_service<"XbeeExporter">(exporter);
     }
     if constexpr (registry.has_service<"XbeeImporter">()) {
-        static auto uart = tos::bsp::board_spec::xbee_uart::open();
+        static decltype(auto) uart = tos::bsp::board_spec::xbee_uart::open();
         auto importer = new tos::ae::xbee_importer(&uart, &alarm);
         registry.register_service<"XbeeImporter">(importer);
     }
