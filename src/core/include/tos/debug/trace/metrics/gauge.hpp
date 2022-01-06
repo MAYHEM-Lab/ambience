113c38eec3515a2028cb773b1770eac07236afc4
Fatih Bakir <mfatihbakir@gmail.com>
2022-01-06 14:51:45 -0800

    [ambience] Keep track of max concurrency

--- src/core/include/tos/debug/trace/metrics/gauge.hpp
+++ src/core/include/tos/debug/trace/metrics/gauge.hpp
@@ -1,8 +1,35 @@
 #pragma once
 
 #include <cstdint>
+#include <array>
 
 namespace tos::trace {
+struct basic_min_max_gauge {
+    constexpr void set(uint64_t val) {
+        cur = val;
+        max = std::max(max, cur);
+        min = std::min(min, cur);
+    }
+
+    constexpr void inc(int by = 1) {
+        cur += by;
+        max = std::max(max, cur);
+    }
+
+    constexpr void dec(int by = 1) {
+        cur -= by;
+        min = std::min(min, cur);
+    }
+
+    std::array<uint64_t, 3> get() const {
+        return {min, cur, max};
+    }
+
+private:
+    uint64_t min = 0;
+    uint64_t max = 0;
+    uint64_t cur = 0;
+};
 
 struct basic_gauge {
     constexpr void set(uint64_t val) {
