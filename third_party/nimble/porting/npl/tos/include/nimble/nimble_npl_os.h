ec8143d767d67971eb0f14c4e477edd33aa3947a
Fatih Bakir <mfatihbakir@gmail.com>
2022-01-09 21:40:27 -0800

    [nimble] NPL header

--- third_party/nimble/porting/npl/tos/include/nimble/nimble_npl_os.h
+++ third_party/nimble/porting/npl/tos/include/nimble/nimble_npl_os.h
@@ -4,7 +4,7 @@
 
 #define BLE_NPL_OS_ALIGNMENT    4
 
-#define BLE_NPL_TIME_FOREVER    -1
+#define BLE_NPL_TIME_FOREVER    (unsigned)-1
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,17 +15,25 @@ typedef uint32_t ble_npl_time_t;
 typedef int32_t ble_npl_stime_t;
 
 struct ble_npl_event {
-    _Alignas(_Alignof(void*)) char buffer[4 * sizeof(void*)];
+    #define BLE_NPL_EVENT_SIZE (4 * sizeof(void*))
+    _Alignas(_Alignof(void*)) char buffer[BLE_NPL_EVENT_SIZE];
+    uint32_t initd;
 };
 
 struct ble_npl_eventq {
-    _Alignas(_Alignof(void*)) char buffer[4 * sizeof(void*)];
+    #define BLE_NPL_EVENTQ_SIZE (5 * sizeof(void*))
+    _Alignas(_Alignof(void*)) char buffer[BLE_NPL_EVENTQ_SIZE];
+    uint32_t initd;
 };
 
 struct ble_npl_callout {
-    // TimerHandle_t handle;
-    // struct ble_npl_eventq *evq;
-    // struct ble_npl_event ev;
+    #define BLE_NPL_CALLOUT_SIZE (6 * sizeof(void*))
+    _Alignas(_Alignof(void*)) char buffer[BLE_NPL_CALLOUT_SIZE];
+
+    ble_npl_time_t ticks;
+    struct ble_npl_eventq *evq;
+    struct ble_npl_event ev;
+    uint32_t initd;
 };
 
 struct ble_npl_mutex {
