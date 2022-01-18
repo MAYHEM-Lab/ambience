95f4f4d6a64609ec848c891e5bd6a4ca2226a336
Fatih Bakir <mfatihbakir@gmail.com>
2022-01-17 23:38:45 -0800

    [ambience] DNS agent manages buffers correctly

--- ambience/services/impls/dns/agent.cpp
+++ ambience/services/impls/dns/agent.cpp
@@ -10,64 +10,66 @@
 #include <utility>
 
 namespace tos::ae::services {
+alignas(4096) static std::array<std::array<uint8_t, 64>, 1024 * 10> globbufs;
 struct bench_agent : agent::async_server {
     bench_agent(tos::ae::services::dns::async_server* dns)
         : m_dns{dns} {
     }
     tos::Task<tos::ae::bench_result> start(const int64_t& param) override {
-        std::vector<std::array<uint8_t, 32>> bufs(param);
-        std::vector<lidl::message_builder> mb;
-        mb.reserve(param);
-        for (auto& buf : bufs) {
-            mb.emplace_back(buf);
-        }
+        tos::debug::log("agent", param);
 
+        std::vector<lidl::message_builder> mb(param);
         std::vector<std::string_view> results(param);
+        uint64_t latency_sum = 0;
+        uint64_t latency_sq = 0;
         auto now = tos::ae::timestamp();
-        co_await tos::coro::generate_n(
-            param, results.begin(), [&](int i) {
+        for (auto j = 0; j < 1024 * 10; j += param) {
+            auto bufs = tos::span(globbufs).slice(j, param);
+            std::transform(bufs.begin(), bufs.end(), mb.begin(), [](auto& arr) {
+                return lidl::message_builder(arr);
+            });
+
+            auto now = tos::ae::timestamp();
+            co_await tos::coro::generate_n(param, results.begin(), [&](int i) {
                 return m_dns->resolve(hosts[(head + i) % hosts.size()], "", mb[i]);
             });
-        head++;
+            auto end = tos::ae::timestamp();
+            auto diff = end - now;
+            latency_sum += diff;
+            latency_sq += diff * diff;
+            head++;
+        }
         auto dur = tos::ae::timestamp() - now;
-        tos::debug::log(dur);
+        tos::debug::log(dur, latency_sum, latency_sq);
 
         // auto now = tos::ae::timestamp();
         // auto results = co_await tos::coro::when_all(
         //     m_dns->resolve(hosts[(head + 0) % hosts.size()], "", mb[0]),
-        //     m_dns->resolve(hosts[(head + 1) % hosts.size()], "", mb[1]),
-        //     m_dns->resolve(hosts[(head + 2) % hosts.size()], "", mb[2]),
-        //     m_dns->resolve(hosts[(head + 3) % hosts.size()], "", mb[3]),
-        //     m_dns->resolve(hosts[(head + 4) % hosts.size()], "", mb[4]),
-        //     m_dns->resolve(hosts[(head + 5) % hosts.size()], "", mb[5]),
-        //     m_dns->resolve(hosts[(head + 6) % hosts.size()], "", mb[6]),
-        //     m_dns->resolve(hosts[(head + 7) % hosts.size()], "", mb[7]),
-        //     m_dns->resolve(hosts[(head + 8) % hosts.size()], "", mb[8]),
-        //     m_dns->resolve(hosts[(head + 9) % hosts.size()], "", mb[9]));
+        //     m_dns->resolve(hosts[(head + 1) % hosts.size()], "", mb[1]));
         // head += 1;
         // auto dur = tos::ae::timestamp() - now;
         // [&]<size_t... Is>(std::index_sequence<Is...>) {
         //     tos::debug::log(std::get<Is>(results)..., dur);
         // }
         // (std::make_index_sequence<std::tuple_size_v<decltype(results)>>{});
-        co_return tos::ae::bench_result{dur, 1, 2, 3};
+        co_return tos::ae::bench_result{dur, latency_sum, latency_sq, tos::ae::default_allocator().in_use().value()};
     }
 
     static constexpr std::array<std::string_view, 16> hosts{
         "example.com",
+        "invalid",
+        "google.com",
+        "lidl.dev",
+        "invalid",
+        "invalid",
         "a6e.org",
-        "google.com",
-        "lidl.dev",
         "invalid",
-        "example.com",
-        "a6e.org",
+        "google.com",
         "invalid",
         "google.com",
         "lidl.dev",
-        "google.com",
-        "lidl.dev",
         "invalid",
-        "example.com",
+        "invalid",
         "a6e.org",
         "invalid",
     };
