7e93b1e91f429b33af69d3cc1e42862ccfc2bc17
Fatih Bakir <mfatihbakir@gmail.com>
2022-01-25 20:40:44 -0800

    [util] Flag functions expect an enum

--- src/util/include/tos/flags.hpp
+++ src/util/include/tos/flags.hpp
@@ -1,16 +1,17 @@
 #pragma once
 
 #include <type_traits>
+#include <tos/concepts.hpp>
 
 namespace tos::util {
-template<class EnumT>
+template<Enumeration EnumT>
 constexpr bool is_flag_set(EnumT all, EnumT look_for) {
     return (std::underlying_type_t<EnumT>(all) &
             std::underlying_type_t<EnumT>(look_for)) ==
            std::underlying_type_t<EnumT>(look_for);
 }
 
-template<class EnumT>
+template<Enumeration EnumT>
 constexpr EnumT set_flag(EnumT all, EnumT to_set) {
     return EnumT(std::underlying_type_t<EnumT>(all) |
                  std::underlying_type_t<EnumT>(to_set));
