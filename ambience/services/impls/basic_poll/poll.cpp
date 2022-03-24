1eb07aed3351b4cc73965134c85802a839380ad7
Sierra Wang <sierrawang@ucsb.edu>
2022-03-24 20:20:46 +0000

    Remove synchronous function from poll so that timestamp works

--- ambience/services/impls/basic_poll/poll.cpp
+++ ambience/services/impls/basic_poll/poll.cpp
@@ -4,11 +4,11 @@
 #include <tos/ae/user_space.hpp>
 
 namespace {
-    struct basic_poll : tos::ae::services::poll::sync_server {
-        uint64_t fn() override {
-            return tos::ae::timestamp();
-        }
-    };
+    // struct basic_poll : tos::ae::services::poll::sync_server {
+    //     uint64_t fn() override {
+    //         return tos::ae::timestamp();
+    //     }
+    // };
 
     struct async_basic_poll : tos::ae::services::poll::async_server {
         tos::Task<uint64_t> fn() override {
@@ -17,9 +17,9 @@ namespace {
     };
 }
 
-tos::ae::services::poll::sync_server* init_basic_poll() {
-    return new basic_poll();
-}
+// tos::ae::services::poll::sync_server* init_basic_poll() {
+//     return new basic_poll();
+// }
 
 tos::Task<tos::ae::services::poll::async_server*> init_async_basic_poll() {
     co_return new async_basic_poll();
