8af3ec22ca7b2ae6d905eb8f84b5f7670cd28c85
Fatih Bakir <mfatihbakir@gmail.com>
2022-04-04 15:19:53 -0700

    [stm32f7] Demo uses ethernet

--- examples/stm32f7_disco/main.cpp
+++ examples/stm32f7_disco/main.cpp
@@ -2,14 +2,25 @@
 // Created by fatih on 10/25/18.
 //
 
-#include "arch/gpio.hpp"
+#include "lwip/init.h"
 #include <arch/drivers.hpp>
+#include "arch/gpio.hpp"
+#include "arch/timer.hpp"
+#include "common/clock.hpp"
+#include "common/inet/tcp_ip.hpp"
+#include "lwip/timeouts.h"
+#include "tos/arm/assembly.hpp"
+#include "tos/debug/dynamic_log.hpp"
+#include "tos/debug/sinks/serial_sink.hpp"
+#include "tos/thread.hpp"
 #include <numeric>
 #include <tos/board.hpp>
 #include <tos/fixed_fifo.hpp>
 #include <tos/ft.hpp>
+#include <tos/lwip/common.hpp>
 #include <tos/mem_stream.hpp>
 #include <tos/periph/stm32f7_dma2d.hpp>
+#include <tos/periph/stm32f7_eth/driver.hpp>
 #include <tos/periph/stm32f7_ltdc.hpp>
 #include <tos/periph/stm32f7_sdram.hpp>
 #include <tos/print.hpp>
@@ -20,7 +31,7 @@ using bs = tos::bsp::board_spec;
 #define SDRAM_BANK_ADDR ((uint32_t)0xC0000000)
 
 tos::span<uint16_t> sdram_span() {
-    return {(uint16_t*)SDRAM_BANK_ADDR, (uint16_t*)SDRAM_BANK_ADDR + 480*272};
+    return {(uint16_t*)SDRAM_BANK_ADDR, (uint16_t*)SDRAM_BANK_ADDR + 480 * 272};
 }
 
 void blink_task() {
@@ -35,8 +46,42 @@ void blink_task() {
     auto timer = open(devs::timer<2>);
     tos::alarm alarm(&timer);
 
+
+    auto clk_timer = open(devs::timer<3>);
+    auto clk = tos::clock(&clk_timer);
+    auto erased_clk = tos::erase_clock(&clk);
+    lwip::global::system_clock = &erased_clk;
+
     auto usart = bs::default_com::open();
+
+    tos::debug::serial_sink uart_sink(&usart);
+    tos::debug::detail::any_logger uart_log{&uart_sink};
+    uart_log.set_log_level(tos::debug::log_level::trace);
+    tos::debug::set_default_log(&uart_log);
+
     tos::println(usart, "Hello From Tos!");
+    lwip_init();
+
+    auto eth = tos::periph::stm32f7::ethernet::open(
+        tos::mac_addr_t{{0x00, 0x80, 0xe1, 0x00, 0x00, 0x00}});
+
+    set_name(tos::launch(tos::alloc_stack,
+                         [&] {
+                             while (true) {
+                                 using namespace std::chrono_literals;
+                                 tos::this_thread::sleep_for(alarm, 50ms);
+                                 tos::lock_guard lg{tos::lwip::lwip_lock};
+                                 sys_check_timeouts();
+                             }
+                         }),
+             "LWIP check timeouts");
+
+    while (true) {
+        using namespace std::chrono_literals;
+
+        tos::this_thread::sleep_for(alarm, 1s);
+        tos::cur_arch::nop();
+    }
 
     tos::println(usart, "init display!");
 
@@ -45,7 +90,7 @@ void blink_task() {
     layer.set_blending(LTDC_BLENDING_FACTOR1_CA, LTDC_BLENDING_FACTOR2_CA);
     layer.set_pixel_format(LTDC_PIXEL_FORMAT_RGB565);
     layer.set_framebuffer(const_cast<uint16_t*>(sdram_span().data()));
-//    layer.set_framebuffer(data);
+    //    layer.set_framebuffer(data);
     layer.set_alpha(255);
     layer.set_dimensions(480, 272);
     layer.set_back_color(0, 0, 0, 0);
@@ -79,7 +124,7 @@ void blink_task() {
     }
 }
 
-tos::stack_storage<4096> stack;
+tos::stack_storage<8192> stack;
 void tos_main() {
     tos::launch(stack, blink_task);
 }
