07f882736e03813a47657df9394510f06788dcea
Fatih Bakir <mfatihbakir@gmail.com>
2022-04-28 20:44:28 -0700

    [ambience] handle_req with a union

--- ambience/include/tos/ae/detail/handle_req.hpp
+++ ambience/include/tos/ae/detail/handle_req.hpp
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <lidlrt/concepts.hpp>
+#include <lidlrt/service.hpp>
 #include <tos/ae/service_host.hpp>
 #include <tos/detail/poll.hpp>
+#include <tos/ft.hpp>
 #include <tos/function_ref.hpp>
 #include <tos/semaphore.hpp>
-#include <tos/ft.hpp>
 
 namespace tos::ae {
 inline void sync_run_message(async_service_host& serv,
@@ -40,5 +42,29 @@ inline tos::Task<bool> async_run_message(sync_service_host& serv,
     });
     co_await sem;
     co_return true;
+}
+
+template<lidl::SyncService ServT>
+bool sync_run_union(
+    ServT& serv,
+    typename ServT::service_type::wire_types::call_union& call_union,
+    lidl::message_builder& response_builder) {
+    return lidl::detail::union_caller<ServT>(serv, call_union, response_builder);
+}
+
+template<lidl::AsyncService ServT>
+bool sync_run_union(
+    ServT& serv,
+    typename ServT::service_type::wire_types::call_union& call_union,
+    lidl::message_builder& response_builder) {
+
+    tos::semaphore exec_sem{0};
+
+    tos::coro::make_detached(
+        lidl::detail::async_union_caller<ServT>(serv, call_union, response_builder),
+        tos::make_semaphore_upper(exec_sem));
+
+    exec_sem.down();
+    return true;
 }
 } // namespace tos::ae
